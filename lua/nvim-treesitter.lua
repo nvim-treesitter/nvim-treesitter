5897d72b075991e4de5876c918be186d4551cf12
Thomas Vigouroux <tomvig38@gmail.com>
2020-04-18 19:32:51 +0200

    feat: add empty setup function
    Will be used later to setup everything so that a language works properly
    in each situation.

---
+++ lua/nvim-treesitter.lua
@@ -0,0 +1,10 @@
+local api = vim.api
+
+local M = {}
+
+-- This function sets up everythin needed for a given language
+-- this is the main interface through the plugin
+function M.setup(lang)
+end
+
+return M
