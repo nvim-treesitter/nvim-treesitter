df17a48c85b8e47bc4982b640dbb686e372cd81e
kiyan42 <yazdani.kiyan@protonmail.com>
2020-06-20 12:21:42 +0200

    refactor: parser list and lang->ft/ft->lang
    - move parser list in `parsers.lua`
    - most `ft` variable where changed to `lang`, `ft` is only used on
      autocmd binding, and lang is used for everything else. Functions have
      been defined to make the switch between `ft` and `lang`

--- lua/nvim-treesitter/highlight.lua
+++ lua/nvim-treesitter/highlight.lua
@@ -2,6 +2,7 @@ local api = vim.api
 local ts = vim.treesitter
 
 local queries = require'nvim-treesitter.query'
+local parsers = require'nvim-treesitter.parsers'
 
 local M = {
   highlighters = {}
@@ -49,14 +50,14 @@ hlmap["type.builtin"] = "Type"
 hlmap["structure"] = "Structure"
 hlmap["include"] = "Include"
 
-function M.attach(bufnr, ft)
-  local buf = bufnr or api.nvim_get_current_buf()
-  local ft = ft or api.nvim_buf_get_option(buf, 'ft')
+function M.attach(bufnr, lang)
+  local bufnr = bufnr or api.nvim_get_current_buf()
+  local lang = lang or parsers.ft_to_lang(api.nvim_buf_get_option(bufnr, 'ft'))
 
-  local query = queries.get_query(ft, "highlights")
+  local query = queries.get_query(lang, "highlights")
   if not query then return end
 
-  M.highlighters[buf] = ts.TSHighlighter.new(query, buf, ft)
+  M.highlighters[bufnr] = ts.TSHighlighter.new(query, bufnr, lang)
 end
 
 function M.detach(bufnr)
