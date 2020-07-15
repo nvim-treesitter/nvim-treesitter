0c416eb2a74ed36b5df0da1f8830e72f8ca9c3c4
Stephan Seitz <stephan.seitz@fau.de>
2020-07-15 20:47:26 +0200

    Python textobjects: Include @decorators and improve @conditional.inner

--- queries/python/textobjects.scm
+++ queries/python/textobjects.scm
@@ -2,8 +2,14 @@
 (function_definition
   body: (block)? @function.inner) @function.outer
 
+(decorated_definition
+  (function_definition) @function.outer) @function.outer.start
+
 (class_definition
   body: (block)? @class.inner) @class.outer
+
+(decorated_definition
+  (class_definition) @class.outer) @class.outer.start
 
 (while_statement
   body: (block)? @loop.inner) @loop.outer
@@ -12,8 +18,10 @@
   body: (block)? @loop.inner) @loop.outer
 
 (if_statement
-  consequence: (block)? @conditional.inner
-  alternative: (_ (block) @conditional.inner)?) @conditional.outer
+  alternative: (_ (_) @conditional.inner)?) @conditional.outer
+
+(if_statement
+  consequence: (block)? @conditional.inner)
 
 (if_statement
   condition: (_) @conditional.inner)
