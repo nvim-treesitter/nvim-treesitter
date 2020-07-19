6bdbca9d33d59ed1cc427a8548aeaca9bb6f85e1
Chinmay Dalal <chinmay.dalal.22012001@gmail.com>
2020-07-19 17:41:24 +0000

    Add files via upload

--- queries/java/locals.scm
+++ queries/java/locals.scm
@@ -1,10 +1,23 @@
 ; CREDITS @maxbrunsfeld (maxbrunsfeld@gmail.com)
 
+;(class_declaration
+;  name: (identifier) @name) @class
+;
+;(method_declaration
+;  name: (identifier) @name) @method
+;
+;(method_invocation
+;  name: (identifier) @name) @call
+
+(class_declaration) @scope
+(method_declaration) @scope
+(if_statement) @scope
+(for_statement) @scope
+(try_statement
+  body: (_) @scope)
+(catch_clause) @scope
+
 (class_declaration
-  name: (identifier) @name) @class
-
+  name: (identifier) @definition.type)
 (method_declaration
-  name: (identifier) @name) @method
-
-(method_invocation
-  name: (identifier) @name) @call
+  name: (identifier) @definition.method)
