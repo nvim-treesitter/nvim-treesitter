bd1d1adb357aae4ccb3038efd5365cd53de0e805
Steven Sojka <steelsojka@gmail.com>
2020-07-28 10:55:54 -0500

    rename query parser to query from scheme

---
+++ queries/query/locals.scm
@@ -0,0 +1,13 @@
+(identifier) @reference
+
+(program (named_node) @scope)
+(program (anonymous_node) @scope)
+
+(named_node
+  (capture) @definition.var)
+
+(anonymous_node
+  (capture) @definition.var)
+
+(grouping
+  (capture) @definition.var)
