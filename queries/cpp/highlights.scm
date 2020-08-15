627a1e558f8a534a779e017edbe1d3bb5f7a0768
Stephan Seitz <stephan.seitz@fau.de>
2020-08-15 18:48:21 +0200

    C++ highlights: Add initial support for attributes

--- queries/cpp/highlights.scm
+++ queries/cpp/highlights.scm
@@ -117,3 +117,9 @@
 
 "::" @operator
 "..." @operator
+
+; Annotations (not fully supported by parser)
+
+((ERROR) @annotation 
+         (vim-match? @annotation  "\[?\[.*\]\]?.*$"))
+(attribute) @annotation
