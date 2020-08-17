9c8c01dc0fb49b633c50b69262baccbe7d8f2725
kiyan42 <yazdani.kiyan@protonmail.com>
2020-08-17 18:35:15 +0200

    add jsx fragment and jsx self closing element highlights

--- queries/javascript/jsx.scm
+++ queries/javascript/jsx.scm
@@ -2,8 +2,11 @@
   open_tag: (jsx_opening_element ["<" ">"] @operator))
 (jsx_element
   close_tag: (jsx_closing_element ["<" "/" ">"] @operator))
+(jsx_self_closing_element ["/" ">" "<"] @operator)
+(jsx_fragment [">" "<" "/"] @operator)
 
 (jsx_closing_element name: (identifier) @variable.builtin)
 (jsx_opening_element name: (identifier) @variable.builtin)
+(jsx_self_closing_element name: (identifier) @variable.builtin)
 
 (jsx_text) @none
