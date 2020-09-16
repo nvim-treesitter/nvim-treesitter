82a8b859c6377a03e0c4d33b8b045548568c7aef
Steven Sojka <Steven.Sojka@tdameritrade.com>
2020-09-16 07:03:22 -0500

    chore(modules): remove refactor module

--- lua/nvim-treesitter/configs.lua
+++ lua/nvim-treesitter/configs.lua
@@ -46,41 +46,6 @@ local builtin_modules = {
     },
     is_supported = queries.has_locals
   },
-  refactor = {
-    highlight_definitions = {
-      module_path = 'nvim-treesitter.refactor.highlight_definitions',
-      enable = false,
-      disable = {},
-      is_supported = queries.has_locals
-    },
-    highlight_current_scope = {
-      module_path = 'nvim-treesitter.refactor.highlight_current_scope',
-      enable = false,
-      disable = {},
-      is_supported = queries.has_locals,
-    },
-    smart_rename = {
-      module_path = 'nvim-treesitter.refactor.smart_rename',
-      enable = false,
-      disable = {},
-      is_supported = queries.has_locals,
-      keymaps = {
-        smart_rename = "grr"
-      }
-    },
-    navigation = {
-      module_path = 'nvim-treesitter.refactor.navigation',
-      enable = false,
-      disable = {},
-      is_supported = queries.has_locals,
-      keymaps = {
-        goto_definition = "gnd",
-        list_definitions = "gnD",
-        goto_next_usage = "<a-*>",
-        goto_previous_usage = "<a-#>",
-      }
-    }
-  },
   textobjects = {
     select = {
       module_path = 'nvim-treesitter.textobjects.select',
