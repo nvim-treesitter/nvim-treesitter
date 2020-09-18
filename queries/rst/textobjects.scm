5c85e6beeee7976a512a02980e999c987f4e8d8e
Santos Gallegos <stsewd@protonmail.com>
2020-09-18 11:12:27 -0500

    RST: update queries
    Some nodes were renamed and added more textobjects.

--- queries/rst/textobjects.scm
+++ queries/rst/textobjects.scm
@@ -4,3 +4,29 @@
 (section (title) @class.inner) @class.outer
 
 (transition) @class.outer
+
+[
+  (bullet_list)
+  (enumerated_list)
+  (definition_list)
+  (field_list)
+
+  (literal_block)
+  (line_block)
+  (block_quote)
+  (doctest_block)
+] @block.outer
+
+(footnote
+  body: (body) @block.inner) @block.outer
+
+(citation
+  body: (body) @block.inner) @block.outer
+
+(target
+  link: (link) @block.inner) @block.outer
+
+(substitution_definition
+  body: (body) @block.inner) @block.outer
+
+(comment) @comment.outer
