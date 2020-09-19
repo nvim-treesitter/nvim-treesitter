0e635d0eb48f44e790432a3f6c531ff58c6af737
kiyan42 <yazdani.kiyan@protonmail.com>
2020-09-19 12:21:28 +0200

    use @tag and @type directives for jsx highlights

--- queries/jsx/highlights.scm
+++ queries/jsx/highlights.scm
@@ -1,12 +1,26 @@
 (jsx_element
-  open_tag: (jsx_opening_element ["<" ">"] @operator))
+  open_tag: (jsx_opening_element ["<" ">"] @tag.delimiter))
 (jsx_element
-  close_tag: (jsx_closing_element ["<" "/" ">"] @operator))
-(jsx_self_closing_element ["/" ">" "<"] @operator)
-(jsx_fragment [">" "<" "/"] @operator)
+  close_tag: (jsx_closing_element ["<" "/" ">"] @tag.delimiter))
+(jsx_self_closing_element ["/" ">" "<"] @tag.delimiter)
+(jsx_fragment [">" "<" "/"] @tag.delimiter)
 
-(jsx_closing_element name: (identifier) @variable.builtin)
-(jsx_opening_element name: (identifier) @variable.builtin)
-(jsx_self_closing_element name: (identifier) @variable.builtin)
+(jsx_opening_element
+  name: (identifier) @tag)
+
+(jsx_closing_element
+  name: (identifier) @tag)
+
+(jsx_self_closing_element 
+  name: (identifier) @tag)
+
+(jsx_opening_element ((identifier) @type
+ (#match? @type "^[A-Z]")))
+
+(jsx_closing_element ((identifier) @type
+ (#match? @type "^[A-Z]")))
+
+(jsx_self_closing_element ((identifier) @type
+ (#match? @type "^[A-Z]")))
 
 (jsx_text) @none
