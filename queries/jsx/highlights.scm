a801ed92f4f498ce553b890a226a8ee6f9bba97d
davidroeca <david.roeca@gmail.com>
2020-10-20 12:10:50 -0400

    handle jsx components with dot in them

--- queries/jsx/highlights.scm
+++ queries/jsx/highlights.scm
@@ -11,17 +11,26 @@
 (jsx_closing_element
   name: (identifier) @tag)
 
-(jsx_self_closing_element 
+(jsx_self_closing_element
   name: (identifier) @tag)
 
 (jsx_opening_element ((identifier) @type
  (#match? @type "^[A-Z]")))
 
+; Handle the dot operator effectively - <My.Component>
+(jsx_opening_element ((nested_identifier (identifier) @type (identifier) @type)))
+
 (jsx_closing_element ((identifier) @type
  (#match? @type "^[A-Z]")))
 
+; Handle the dot operator effectively - </My.Component>
+(jsx_closing_element ((nested_identifier (identifier) @type (identifier) @type)))
+
 (jsx_self_closing_element ((identifier) @type
  (#match? @type "^[A-Z]")))
+
+; Handle the dot operator effectively - <My.Component />
+(jsx_self_closing_element ((nested_identifier (identifier) @type (identifier) @type)))
 
 (variable_declarator ((identifier) @type
  (#match? @type "^[A-Z]")))
