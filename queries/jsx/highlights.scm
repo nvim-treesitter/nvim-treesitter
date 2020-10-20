f925408640bd7be24c3c5d1eec1a4a46f3fb438f
davidroeca <david.roeca@gmail.com>
2020-10-20 14:11:19 -0400

    use  for first identifier in jsx nested_identifier

--- queries/jsx/highlights.scm
+++ queries/jsx/highlights.scm
@@ -18,19 +18,19 @@
  (#match? @type "^[A-Z]")))
 
 ; Handle the dot operator effectively - <My.Component>
-(jsx_opening_element ((nested_identifier (identifier) @type (identifier) @type)))
+(jsx_opening_element ((nested_identifier (identifier) @tag (identifier) @type)))
 
 (jsx_closing_element ((identifier) @type
  (#match? @type "^[A-Z]")))
 
 ; Handle the dot operator effectively - </My.Component>
-(jsx_closing_element ((nested_identifier (identifier) @type (identifier) @type)))
+(jsx_closing_element ((nested_identifier (identifier) @tag (identifier) @type)))
 
 (jsx_self_closing_element ((identifier) @type
  (#match? @type "^[A-Z]")))
 
 ; Handle the dot operator effectively - <My.Component />
-(jsx_self_closing_element ((nested_identifier (identifier) @type (identifier) @type)))
+(jsx_self_closing_element ((nested_identifier (identifier) @tag (identifier) @type)))
 
 (variable_declarator ((identifier) @type
  (#match? @type "^[A-Z]")))
