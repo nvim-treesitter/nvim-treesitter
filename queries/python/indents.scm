22e60eb91816172fd89064db34970749bef3ff5d
Evan <esn89@users.noreply.github.com>
2020-12-16 09:42:48 -0800

    Create indents.scm for python

---
+++ queries/python/indents.scm
@@ -0,0 +1,14 @@
+[
+  (function_definition)
+  (if_statement)
+  (with_statement)
+  (try_statement)
+  (for_statement)
+  (for_in_clause)
+  (class_definition)
+] @indent
+
+[
+  "elif"
+  "else"
+] @branch
