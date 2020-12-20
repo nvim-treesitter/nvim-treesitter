58aa24d5e38d1ffbd6d0b9619fe59c1a4a654b08
Jędrzej Boczar <yendreij@gmail.com>
2020-12-20 12:42:11 +0100

    Improve python/indents.scm

--- queries/python/indents.scm
+++ queries/python/indents.scm
@@ -1,16 +1,38 @@
 [
-  (function_definition)
+  (list)
+  (tuple)
+  (dictionary)
+  (set)
+
   (if_statement)
+  (for_statement)
+  (while_statement)
   (with_statement)
   (try_statement)
-  (for_statement)
-  (for_in_clause)
+  (import_from_statement)
+
+  (parenthesized_expression)
+  (generator_expression)
+  (list_comprehension)
+  (set_comprehension)
+  (dictionary_comprehension)
+
+  (tuple_pattern)
+  (list_pattern)
+  (argument_list)
+  (parameters)
+  (binary_operator)
+
+  (function_definition)
   (class_definition)
 ] @indent
 
 [
-  "elif"
-  "else"
-  "finally"
-  "except"
+  ")"
+  "]"
+  "}"
+  (elif_clause)
+  (else_clause)
+  (except_clause)
+  (finally_clause)
 ] @branch
