bedc5c21f54b36c3052b815aa8265454ad17daab
echasnovski <evgeni.chasnovski@gmail.com>
2021-01-31 13:09:25 +0200

    Implement `TSToggleAll`.

--- lua/nvim-treesitter/configs.lua
+++ lua/nvim-treesitter/configs.lua
@@ -151,6 +151,19 @@ local function toggle_module(mod, bufnr, lang)
   end
 end
 
+-- Toggles the module globally and for all current buffers.
+-- @param mod path to module
+local function toggle_all(mod)
+  local config_mod = M.get_module(mod)
+  if not config_mod then return end
+
+  if config_mod.enable then
+    disable_all(mod)
+  else
+    enable_all(mod)
+  end
+end
+
 -- Recurses through all modules including submodules
 -- @param accumulator function called for each module
 -- @param root root configuration table to start at
@@ -212,6 +225,13 @@ M.commands = {
   },
   TSDisableAll = {
     run = disable_all,
+    args = {
+      "-nargs=+",
+      "-complete=custom,nvim_treesitter#available_modules",
+    },
+  },
+  TSToggleAll = {
+    run = toggle_all,
     args = {
       "-nargs=+",
       "-complete=custom,nvim_treesitter#available_modules",
