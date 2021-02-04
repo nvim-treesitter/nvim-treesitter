5441f83c5c2e81a31f3a70a015c0bd3098c0f6f9
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2021-02-04 17:29:18 +0100

    Cleanup

--- queries/supercollider/locals.scm
+++ queries/supercollider/locals.scm
@@ -1,10 +1,3 @@
-; The local variables query is different from the highlights query in that, while the highlights query uses arbitrary capture names which can then be mapped to colors, the locals variable query uses a fixed set of capture names, each of which has a special meaning.
-; The capture names are as follows:
-; @local.scope - indicates that a syntax node introduces a new local scope.
-; @local.definition - indicates that a syntax node contains the name of a definition within the current local scope.
-; @local.reference - indicates that a syntax node contains the name which may refer to an earlier definition within some enclosing scope.
-
-
 ; Scopes
 [
 (function_call)
