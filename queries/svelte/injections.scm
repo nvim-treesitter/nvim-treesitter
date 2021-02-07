c5cd54fb60905d794687693062b0fffa5117d3f8
elianiva <dicha.arkana03@gmail.com>
2021-02-07 13:14:12 +0700

    feat: improved highlight + injections, cc @stsewd

--- queries/svelte/injections.scm
+++ queries/svelte/injections.scm
@@ -1,6 +1,11 @@
 ((style_element
   (raw_text) @css))
 
+((attribute
+   (attribute_name) @_attr
+   (quoted_attribute_value (attribute_value) @css))
+ (#match? @_attr "^style$"))
+
 ((script_element
   (raw_text) @javascript))
 
