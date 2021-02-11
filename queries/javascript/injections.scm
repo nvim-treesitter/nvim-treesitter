0c415fc5c6670372dcf14e7be7d6443ea2bc2f42
Steven Sojka <Steven.Sojka@tdameritrade.com>
2021-02-11 12:41:06 -0600

    feat(javascript): add template string language injection

--- queries/javascript/injections.scm
+++ queries/javascript/injections.scm
@@ -3,6 +3,12 @@
 ((regex_pattern) @regex)
 
 (call_expression
-	function: ((identifier) @_name
-		(#eq? @_name "gql"))
-	arguments: ((template_string) @graphql))
+  function: ((identifier) @language)
+  arguments: ((template_string) @content
+              (#offset! @content 0 1 0 -1)))
+
+(call_expression
+  function: ((identifier) @_name
+             (#eq? @_name "gql"))
+  arguments: ((template_string) @graphql
+              (#offset! @graphql 0 1 0 -1)))
