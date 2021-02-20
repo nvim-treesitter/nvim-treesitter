ee1bd55dd1ea6f592b2839a714469d055ceade9a
elianiva <dicha.arkana03@gmail.com>
2021-02-20 18:28:10 +0700

    feat: add java indent

---
+++ queries/java/indents.scm
@@ -0,0 +1,19 @@
+[
+  (class_declaration)
+  (class_body)
+  (constructor_declaration)
+  (constructor_body)
+  (block)
+  (switch_block)
+  (array_initializer)
+  (argument_list)
+] @indent
+
+[
+  "("
+  ")"
+  "{"
+  "}"
+  "["
+  "]"
+] @branch
