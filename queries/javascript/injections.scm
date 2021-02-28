afcded22e4d6ec6e227657a5e93b37c4a3f6701c
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-02-28 16:43:32 -0500

    Support the glimmer parser when the `hbs` template literal tag is used

--- queries/javascript/injections.scm
+++ queries/javascript/injections.scm
@@ -12,3 +12,9 @@
              (#eq? @_name "gql"))
   arguments: ((template_string) @graphql
               (#offset! @graphql 0 1 0 -1)))
+
+(call_expression
+  function: ((identifier) @_name
+             (#eq? @_name "hbs"))
+  arguments: ((template_string) @glimmer
+              (#offset! @glimmer 0 1 0 -1)))
