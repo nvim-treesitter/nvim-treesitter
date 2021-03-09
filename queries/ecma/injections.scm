14d4d6aa0ca8db4d5ec958df6ebb1d99eed7219e
kiyan <yazdani.kiyan@protonmail.com>
2021-03-09 23:14:27 +0100

    chore: create ecma base for use in js and ts which fixes jsx issues

---
+++ queries/ecma/injections.scm
@@ -0,0 +1,20 @@
+((comment) @jsdoc)
+
+((regex_pattern) @regex)
+
+(call_expression
+  function: ((identifier) @language)
+  arguments: ((template_string) @content
+              (#offset! @content 0 1 0 -1)))
+
+(call_expression
+  function: ((identifier) @_name
+             (#eq? @_name "gql"))
+  arguments: ((template_string) @graphql
+              (#offset! @graphql 0 1 0 -1)))
+
+(call_expression
+  function: ((identifier) @_name
+             (#eq? @_name "hbs"))
+  arguments: ((template_string) @glimmer
+              (#offset! @glimmer 0 1 0 -1)))
