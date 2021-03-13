11ede1c0d2ab30ca1b637e31892a12e042a73965
Ivo Dancet <ivo.dancet@gmail.com>
2021-03-13 18:41:44 +0100

    folds support and somewhat better indents for ruby

--- queries/ruby/indents.scm
+++ queries/ruby/indents.scm
@@ -1,12 +1,26 @@
 [
   (class)
   (method)
+  (singleton_method)
   (module)
   (call)
   (if)
+  (block)
+  (do_block)
+  (hash)
+  (array)
+  (argument_list)
+  (case)
 ] @indent
 
 [
+  "("
+  ")"
+  "{"
+  "}"
+  "["
+  "]"
+  (when)
   (elsif)
   "end"
 ] @branch
