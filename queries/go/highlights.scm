b818ace3150394d3d8429d7062855615c9c534e4
Yuki Ito <mrno110y@gmail.com>
2021-03-19 19:03:57 +0900

    go: add builtin types

--- queries/go/highlights.scm
+++ queries/go/highlights.scm
@@ -103,6 +103,12 @@
 "var" @keyword
 
 
+;; Builtin types
+
+((type_identifier) @type.builtin
+ (#match? @type.builtin "^(bool|byte|complex128|complex64|error|float32|float64|int|int16|int32|int64|int8|rune|string|uint|uint16|uint32|uint64|uint8|uintptr)$"))
+
+
 ;; Builtin functions
 
 ((identifier) @function.builtin
