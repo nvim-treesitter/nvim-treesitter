71e3391a8cbd9152fd517da4189db9ceb7a9dfd8
shirasaka <shirasaka@n-create.co.jp>
2021-03-19 11:16:02 +0900

    Add indents for php

---
+++ queries/php/indents.scm
@@ -0,0 +1,23 @@
+[
+  (array_creation_expression)
+  (arguments)
+  (formal_parameters)
+  (compound_statement)
+  (declaration_list)
+  (binary_expression)
+  (return_statement)
+  (expression_statement)
+] @indent
+
+[
+  "("
+  ")"
+  "{"
+  "}"
+  "["
+  "]"
+] @branch
+
+[
+  (comment)
+] @ignore
