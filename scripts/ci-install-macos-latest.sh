e01a436c29e5a9b2a72598756161af9978e36047
Stephan Seitz <stephan.seitz@fau.de>
2021-03-22 13:04:24 +0100

    Try out llvm --HEAD

--- scripts/ci-install-macos-latest.sh
+++ scripts/ci-install-macos-latest.sh
@@ -1,5 +1,5 @@
 brew update
-brew install llvm
+brew install llvm --HEAD
 brew install neovim --HEAD
 mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
 ln -s $(pwd) ~/.local/share/nvim/site/pack/nvim-treesitter/start
