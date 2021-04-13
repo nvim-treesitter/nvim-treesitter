7c31ad7a1fd5a6d37d6a8a1b510ff73b98e9c32d
Stephan Seitz <stephan.seitz@fau.de>
2021-04-13 21:35:47 +0200

    Fix #1182: fix TSRange:parent

--- lua/nvim-treesitter/tsrange.lua
+++ lua/nvim-treesitter/tsrange.lua
@@ -3,7 +3,6 @@ local TSRange = {}
 TSRange.__index = TSRange
 
 local api = vim.api
-local parsers = require'nvim-treesitter.parsers'
 local ts_utils = require'nvim-treesitter.ts_utils'
 
 local function get_byte_offset(buf, row, col)
@@ -56,9 +55,8 @@ function TSRange.from_table(buf, range)
     TSRange)
 end
 
-function TSRange:parent(range)
-  local parser = parsers.get_parser(self.buf, parsers.get_buf_lang(range))
-  local root = ts_utils.get_root_for_position(range[1], range[2], parser)
+function TSRange:parent()
+  local root = ts_utils.get_root_for_position(self[1], self[2])
 
   return root
     and root:named_descendant_for_range(self.start_pos[1], self.start_pos[2], self.end_pos[1], self.end_pos[2])
