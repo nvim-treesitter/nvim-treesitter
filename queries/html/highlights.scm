f104eb65b98b97486a8447f389a40d8a884ebfaf
Luis M. Lorca <luismlorca@gmail.com>
2021-04-17 14:57:07 -0400

    HTML/JSX: Highlight attributes as attributes

--- queries/html/highlights.scm
+++ queries/html/highlights.scm
@@ -1,7 +1,7 @@
 (tag_name) @tag
 (erroneous_end_tag_name) @error
 (doctype) @constant
-(attribute_name) @property
+(attribute_name) @attribute
 (attribute_value) @string
 (quoted_attribute_value) @string
 (comment) @comment
