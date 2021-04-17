db1b6dc1cb9a1604a15fd2e57fc098cade9913d3
Jędrzej Boczar <yendreij@gmail.com>
2021-04-17 22:41:46 +0200

    scripts: print test results summary with --summary

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,5 +3,14 @@
 HERE="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 cd $HERE/..
 
-nvim --headless --noplugin -u scripts/minimal_init.lua \
-  -c "PlenaryBustedDirectory lua/tests/indent/ { minimal_init = './scripts/minimal_init.lua' }"
+run() {
+    nvim --headless --noplugin -u scripts/minimal_init.lua \
+        -c "PlenaryBustedDirectory lua/tests/indent/ { minimal_init = './scripts/minimal_init.lua' }"
+}
+
+if [[ $1 = '--summary' ]]; then
+    # really simple results summary by filtering plenary busted output
+    run 2> /dev/null | grep -E '^\S*(Success|Fail(ed)?|Errors?)'
+else
+    run
+fi
