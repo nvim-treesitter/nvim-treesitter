6e90ff3006ba0cd309d9eb0d569d655657a5ff69
Jędrzej Boczar <yendreij@gmail.com>
2021-04-23 00:41:44 +0200

    tests/indent: factor out most of the code into common.lua

--- tests/indent/python_spec.lua
+++ tests/indent/python_spec.lua
@@ -1,52 +1,38 @@
-local whole_file = require('tests.indent.common').indent_whole_file
-local new_line = require('tests.indent.common').indent_new_line
-local scan_dir = require('plenary.scandir').scan_dir
+local Runner = require('tests.indent.common').Runner
 
-local opts = {
+local run = Runner:new(it, 'tests/indent/python', {
   tabstop = 4,
   shiftwidth = 4,
   softtabstop = 0,
   expandtab = true,
-}
-
-local run = function(file, spec, title)
-  title = title and title or tostring(spec.on_line)
-  it(string.format('%s[%s]', file, title), function()
-    new_line('tests/indent/python/' .. file, spec, opts)
-  end)
-end
+})
 
 describe('indent Python:', function()
   describe('whole file:', function()
-    local files = scan_dir('tests/indent/python');
-    for _, file in ipairs(files) do
-      it(vim.fn.fnamemodify(file, ':t'), function()
-        whole_file(file, opts)
-      end)
-    end
+    run:whole_file('.')
   end)
 
   describe('new line:', function()
-    run('aligned_indent.py', { on_line = 1, text = 'arg3,', indent = 19 })
-    run('basic_blocks.py', { on_line = 1, text = 'wait,', indent = 4 })
-    run('basic_blocks.py', { on_line = 6, text = 'x += 1', indent = 4 })
-    run('basic_blocks.py', { on_line = 10, text = 'x += 1', indent = 8 })
-    run('basic_blocks.py', { on_line = 7, text = 'x += 1', indent = 4 }, '7, after last line of a block')
-    run('basic_blocks.py', { on_line = 11, text = 'x += 1', indent = 8 }, '11, after last line of a block')
-    run('basic_collections.py', { on_line = 3, text = '4,', indent = 4 })
-    run('comprehensions.py', { on_line = 8, text = 'if x != 2', indent = 4 })
-    run('control_flow.py', { on_line = 23, text = 'x = 4', indent = 4 })
-    run('hanging_indent.py', { on_line = 1, text = 'arg0,', indent = 8 })
-    run('hanging_indent.py', { on_line = 5, text = '0,', indent = 4 })
-    run('join_lines.py', { on_line = 1, text = '+ 1 \\', indent = 4 })
-    run('join_lines.py', { on_line = 4, text = '+ 1 \\', indent = 4 })
-    run('join_lines.py', { on_line = 7, text = '+ 1 \\', indent = 4 })
-    run('nested_collections.py', { on_line = 5, text = '0,', indent = 12 })
-    run('nested_collections.py', { on_line = 6, text = ',0', indent = 12 })
-    run('nested_collections.py', { on_line = 29, text = '[1, 2],', indent = 12 })
-    run('nested_collections.py', { on_line = 39, text = '0,', indent = 5 })
-    run('strings.py', { on_line = 14, text = 'x', indent = 4 })
-    run('strings.py', { on_line = 15, text = 'x', indent = 0 })
-    run('strings.py', { on_line = 16, text = 'x', indent = 8 })
+    run:new_line('aligned_indent.py', { on_line = 1, text = 'arg3,', indent = 19 })
+    run:new_line('basic_blocks.py', { on_line = 1, text = 'wait,', indent = 4 })
+    run:new_line('basic_blocks.py', { on_line = 6, text = 'x += 1', indent = 4 })
+    run:new_line('basic_blocks.py', { on_line = 10, text = 'x += 1', indent = 8 })
+    run:new_line('basic_blocks.py', { on_line = 7, text = 'x += 1', indent = 4 }, '7, after last line of a block')
+    run:new_line('basic_blocks.py', { on_line = 11, text = 'x += 1', indent = 8 }, '11, after last line of a block')
+    run:new_line('basic_collections.py', { on_line = 3, text = '4,', indent = 4 })
+    run:new_line('comprehensions.py', { on_line = 8, text = 'if x != 2', indent = 4 })
+    run:new_line('control_flow.py', { on_line = 23, text = 'x = 4', indent = 4 })
+    run:new_line('hanging_indent.py', { on_line = 1, text = 'arg0,', indent = 8 })
+    run:new_line('hanging_indent.py', { on_line = 5, text = '0,', indent = 4 })
+    run:new_line('join_lines.py', { on_line = 1, text = '+ 1 \\', indent = 4 })
+    run:new_line('join_lines.py', { on_line = 4, text = '+ 1 \\', indent = 4 })
+    run:new_line('join_lines.py', { on_line = 7, text = '+ 1 \\', indent = 4 })
+    run:new_line('nested_collections.py', { on_line = 5, text = '0,', indent = 12 })
+    run:new_line('nested_collections.py', { on_line = 6, text = ',0', indent = 12 })
+    run:new_line('nested_collections.py', { on_line = 29, text = '[1, 2],', indent = 12 })
+    run:new_line('nested_collections.py', { on_line = 39, text = '0,', indent = 5 })
+    run:new_line('strings.py', { on_line = 14, text = 'x', indent = 4 })
+    run:new_line('strings.py', { on_line = 15, text = 'x', indent = 0 })
+    run:new_line('strings.py', { on_line = 16, text = 'x', indent = 8 })
   end)
 end)
