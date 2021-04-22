af3537fbe57a2a37ab2b620c9ecc487e31b4da64
Jędrzej Boczar <yendreij@gmail.com>
2021-04-23 00:41:44 +0200

    tests/indent: factor out most of the code into common.lua

--- tests/indent/lua_spec.lua
+++ tests/indent/lua_spec.lua
@@ -1,48 +1,35 @@
-local whole_file = require('tests.indent.common').indent_whole_file
-local new_line = require('tests.indent.common').indent_new_line
-local scan_dir = require('plenary.scandir').scan_dir
+local Runner = require('tests.indent.common').Runner
 
-local opts = {
+local run = Runner:new(it, 'tests/indent/lua', {
   tabstop = 2,
   shiftwidth = 2,
   softtabstop = 0,
   expandtab = true,
-}
+})
 
-local run = function(file, spec, title)
-  title = title and title or tostring(spec.on_line)
-  it(string.format('%s[%s]', file, title), function()
-    new_line('tests/indent/lua/' .. file, spec, opts)
-  end)
-end
 
 describe('indent Lua:', function()
   describe('whole file:', function()
-    local files = scan_dir('tests/indent/lua');
-    for _, file in ipairs(files) do
-      it(vim.fn.fnamemodify(file, ':t'), function()
-        whole_file(file, opts)
-      end)
-    end
+    run:whole_file('.')
   end)
 
   describe('new line:', function()
-    run('comment.lua', { on_line = 1, text = 'line', indent = '-- ' })
-    run('comment.lua', { on_line = 5, text = 'multiline', indent = '  ' })
-    run('func.lua', { on_line = 1, text = 'x = x + 1', indent = 2 })
-    run('func.lua', { on_line = 2, text = 'y = y + 1', indent = 4 })
-    run('func.lua', { on_line = 5, text = '3,', indent = 4 })
-    run('string.lua', { on_line = 1, text = 'x', indent = 0 })
-    run('string.lua', { on_line = 2, text = 'x', indent = 0 })
-    run('string.lua', { on_line = 3, text = 'x', indent = 2 })
-    run('string.lua', { on_line = 4, text = 'x', indent = 4 })
-    run('table.lua', { on_line = 1, text = 'b = 0,', indent = 2 })
-    run('table.lua', { on_line = 5, text = '4,', indent = 4 })
-    run('table.lua', { on_line = 7, text = '4,', indent = 4 })
-    run('loop.lua', { on_line = 4, text = 'x = x + 1', indent = 2 })
-    run('cond.lua', { on_line = 5, text = 'x = x + 1', indent = 2 })
-    run('cond.lua', { on_line = 7, text = 'x = x + 1', indent = 2 })
-    run('cond.lua', { on_line = 8, text = 'x = x + 1', indent = 4 })
+    run:new_line('comment.lua', { on_line = 1, text = 'line', indent = '-- ' })
+    run:new_line('comment.lua', { on_line = 5, text = 'multiline', indent = '  ' })
+    run:new_line('func.lua', { on_line = 1, text = 'x = x + 1', indent = 2 })
+    run:new_line('func.lua', { on_line = 2, text = 'y = y + 1', indent = 4 })
+    run:new_line('func.lua', { on_line = 5, text = '3,', indent = 4 })
+    run:new_line('string.lua', { on_line = 1, text = 'x', indent = 0 })
+    run:new_line('string.lua', { on_line = 2, text = 'x', indent = 0 })
+    run:new_line('string.lua', { on_line = 3, text = 'x', indent = 2 })
+    run:new_line('string.lua', { on_line = 4, text = 'x', indent = 4 })
+    run:new_line('table.lua', { on_line = 1, text = 'b = 0,', indent = 2 })
+    run:new_line('table.lua', { on_line = 5, text = '4,', indent = 4 })
+    run:new_line('table.lua', { on_line = 7, text = '4,', indent = 4 })
+    run:new_line('loop.lua', { on_line = 4, text = 'x = x + 1', indent = 2 })
+    run:new_line('cond.lua', { on_line = 5, text = 'x = x + 1', indent = 2 })
+    run:new_line('cond.lua', { on_line = 7, text = 'x = x + 1', indent = 2 })
+    run:new_line('cond.lua', { on_line = 8, text = 'x = x + 1', indent = 4 })
   end)
 end)
 
