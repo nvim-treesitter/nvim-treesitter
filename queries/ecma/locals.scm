788d212ab3d249a673160779528fe6b03eff93a4
Stephan Seitz <stephan.seitz@fau.de>
2021-04-28 14:39:14 +0200

    locals(js,ts): Add parameters again (#1231)

--- queries/ecma/locals.scm
+++ queries/ecma/locals.scm
@@ -13,34 +13,6 @@
 ; Definitions
 ;------------
 
-;(formal_parameters
-  ;(identifier) @definition.parameter)
-
-;(formal_parameters
-  ;(object_pattern
-    ;(identifier) @definition.parameter))
-
-;; function(arg = []) {
-;(formal_parameters
-  ;(assignment_pattern
-    ;(shorthand_property_identifier) @definition.parameter))
-
-;; x => x
-;(arrow_function
-  ;parameter: (identifier) @definition.parameter)
-
-;(formal_parameters
-  ;(object_pattern
-    ;(shorthand_property_identifier) @definition.parameter))
-
-;(formal_parameters
-  ;(array_pattern
-    ;(identifier) @definition.parameter))
-
-;(formal_parameters
-  ;(rest_parameter
-    ;(identifier) @definition.parameter))
-
 (variable_declarator
   name: (identifier) @definition.var)
 
