8730da56ab98311e9bacb22fe65f71bcbb10d2af
Stephan Seitz <stephan.seitz@fau.de>
2021-05-16 23:20:12 +0200

    parsers: fortran no longer requires_generate_from_grammar

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -571,7 +571,6 @@ list.fortran = {
   install_info = {
     url = "https://github.com/stadelmanma/tree-sitter-fortran",
     files = { "src/parser.c", "src/scanner.cc", },
-    requires_generate_from_grammar = true,
   },
 }
 
