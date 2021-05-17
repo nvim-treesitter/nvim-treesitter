0fcfad782eb00019580790d093ed1d194bd4b72e
Daniel Kempkens <daniel+git@kempkens.io>
2021-05-17 23:12:29 +0200

    CI: Install tree-sitter

--- scripts/ci-install-macos-latest.sh
+++ scripts/ci-install-macos-latest.sh
@@ -1,6 +1,7 @@
 brew update
 brew install llvm --HEAD
 brew install neovim --HEAD
+brew install tree-sitter
 mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
 ln -s $(pwd) ~/.local/share/nvim/site/pack/nvim-treesitter/start
 
