f3b20f68e6ab2eb6b4f308f46e907459212cb78d
Luxed <devildead13@gmail.com>
2021-06-02 19:42:20 -0400

    folds(c_sharp): Add basic folding

---
+++ queries/c_sharp/folds.scm
@@ -0,0 +1,18 @@
+[
+ (class_declaration)
+ (constructor_declaration)
+ (enum_declaration)
+ (interface_declaration)
+ (method_declaration)
+ (namespace_declaration)
+ (struct_declaration)
+
+ (do_statement)
+ (for_each_statement)
+ (for_statement)
+ (if_statement)
+ (switch_statement)
+ (try_statement)
+ (using_statement)
+ (while_statement)
+] @fold
