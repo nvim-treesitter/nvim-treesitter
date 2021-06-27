a8cc3982f26cc5897b32e2452d20c65ad939f373
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-06-27 14:29:16 +0100

    Add common Haskell quasiquote injections

---
+++ queries/haskell/injections.scm
@@ -0,0 +1,63 @@
+;; ----------------------------------------------------------------------------- 
+;; General language injection
+
+(quasiquote 
+ ((quoter) @language)
+ ((quasiquote_body) @content)
+)
+
+
+;; ----------------------------------------------------------------------------- 
+;; shakespeare library
+;; NOTE: doesn't support templating
+
+; CoffeeScript: Text.Coffee
+(quasiquote 
+ (quoter) @_name
+ (#eq? @_name "coffee")
+ ((quasiquote_body) @content)
+ (#set! language "coffeescript")
+)
+
+; CSS: Text.Cassius, Text.Lucius
+(quasiquote 
+ (quoter) @_name
+ (#any-of? @_name "cassius" "lucius")
+ ((quasiquote_body) @content)
+ (#set! language "css")
+)
+
+; HTML: Text.Hamlet
+(quasiquote 
+ (quoter) @_name
+ (#any-of? @_name "shamlet" "xshamlet")
+ ((quasiquote_body) @content)
+ (#set! language "html")
+)
+
+; JS: Text.Julius
+(quasiquote 
+ (quoter) @_name
+ (#eq? @_name "julius")
+ ((quasiquote_body) @content)
+ (#set! language "javascript")
+)
+
+; TS: Text.TypeScript
+(quasiquote 
+ (quoter) @_name
+ (#eq? @_name "tsc" "tscJSX")
+ ((quasiquote_body) @content)
+ (#set! language "typescript")
+)
+
+
+;; ----------------------------------------------------------------------------- 
+;; HSX
+
+(quasiquote 
+ (quoter) @_name
+ (#eq? @_name "hsx")
+ ((quasiquote_body) @content)
+ (#set! language "html")
+)
