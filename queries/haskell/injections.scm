7e82b7811e48f34ea44973706ac15f6d591ff328
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-06-28 18:25:11 +0100

    Remove trailing whitespaces

--- queries/haskell/injections.scm
+++ queries/haskell/injections.scm
@@ -1,18 +1,18 @@
-;; ----------------------------------------------------------------------------- 
+;; -----------------------------------------------------------------------------
 ;; General language injection
 
-(quasiquote 
+(quasiquote
  ((quoter) @language)
  ((quasiquote_body) @content)
 )
 
 
-;; ----------------------------------------------------------------------------- 
+;; -----------------------------------------------------------------------------
 ;; shakespeare library
 ;; NOTE: doesn't support templating
 
 ; CoffeeScript: Text.Coffee
-(quasiquote 
+(quasiquote
  (quoter) @_name
  (#eq? @_name "coffee")
  ((quasiquote_body) @content)
@@ -20,7 +20,7 @@
 )
 
 ; CSS: Text.Cassius, Text.Lucius
-(quasiquote 
+(quasiquote
  (quoter) @_name
  (#any-of? @_name "cassius" "lucius")
  ((quasiquote_body) @content)
@@ -28,7 +28,7 @@
 )
 
 ; HTML: Text.Hamlet
-(quasiquote 
+(quasiquote
  (quoter) @_name
  (#any-of? @_name "shamlet" "xshamlet" "hamlet" "xhamlet" "ihamlet")
  ((quasiquote_body) @content)
@@ -36,7 +36,7 @@
 )
 
 ; JS: Text.Julius
-(quasiquote 
+(quasiquote
  (quoter) @_name
  (#any-of? @_name "js" "julius")
  ((quasiquote_body) @content)
@@ -44,7 +44,7 @@
 )
 
 ; TS: Text.TypeScript
-(quasiquote 
+(quasiquote
  (quoter) @_name
  (#any-of? @_name "tsc" "tscJSX")
  ((quasiquote_body) @content)
@@ -52,10 +52,10 @@
 )
 
 
-;; ----------------------------------------------------------------------------- 
+;; -----------------------------------------------------------------------------
 ;; HSX
 
-(quasiquote 
+(quasiquote
  (quoter) @_name
  (#eq? @_name "hsx")
  ((quasiquote_body) @content)
