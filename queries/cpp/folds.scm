cf5d494ede872183ec7f6d6d9d3ffeed34b343d8
Stephan Seitz <sseitz@nvidia.com>
2021-07-02 18:51:43 +0200

    folds(cpp): highlight scoped enums

--- queries/cpp/folds.scm
+++ queries/cpp/folds.scm
@@ -3,6 +3,9 @@
 [
  (for_range_loop)
  (class_specifier)
+ (field_declaration
+   type: (enum_specifier)
+   default_value: (initializer_list))
  (template_declaration)
  (namespace_definition)
  (try_statement)
