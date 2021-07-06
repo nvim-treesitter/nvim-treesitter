b7364d923b209b9ed6845d4548b9846226769cd8
Stephan Seitz <stephan.seitz@fau.de>
2021-07-06 23:00:10 +0200

    CI: remove LLVM compilation from Mac CI

--- scripts/ci-install-macos-latest.sh
+++ scripts/ci-install-macos-latest.sh
@@ -1,5 +1,4 @@
 brew update
-brew install llvm --HEAD
 brew install neovim --HEAD
 mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
 ln -s $(pwd) ~/.local/share/nvim/site/pack/nvim-treesitter/start
