db6b8fc6f04a360c8cb25579e747649644cd8f43
Connor Lay (Clay) <connorlay@pm.me>
2021-07-24 11:18:26 -0600

    Add initial surface highlights & injections

---
+++ queries/surface/highlights.scm
@@ -0,0 +1,16 @@
+(tag_name) @type
+(public_comment) @comment
+(private_comment) @comment
+
+(attribute_name) @symbol
+(attribute_value) @string
+
+[
+  "{"
+  "}"
+  "<"
+  ">"
+  "/>"
+] @punctuation.bracket
+
+"=" @operator
