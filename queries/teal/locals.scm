f96c409e3d2b5c4f8fe76f3e049958a293f13167
Stephan Seitz <stephan.seitz@fau.de>
2021-07-25 15:27:51 +0200

    Update teal highlights

--- queries/teal/locals.scm
+++ queries/teal/locals.scm
@@ -1,23 +1,23 @@
-
 (var_declaration
-   (var (identifier)) @definition.var)
+  declarators: (var_declarators
+  (var (identifier)) @definition.var))
 
 (var_assignment
-   (var (identifier) @definition.var) @definition.associated)
+  variables: (assignment_variables
+    (var (identifier) @definition.var) @definition.associated))
 
 (arg name: (identifier) @definition.parameter)
 
 (anon_function) @scope
 ((function_statement
-  (function_name) @definition.function) @scope
-  (#set! definition.function.scope "parent"))
+  (function_name) @definition.function) @scope)
 
 (program) @scope
 (if_statement) @scope
-(for_statement (for_body) @scope)
+(generic_for_statement (for_body) @scope)
+(numeric_for_statement (for_body) @scope)
 (repeat_statement) @scope
 (while_statement (while_body) @scope)
 (do_statement) @scope
 
 (identifier) @reference
-
