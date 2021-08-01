e6deae30cb66a08fbcc38565f52d08e42a18c0fe
Santos Gallegos <stsewd@protonmail.com>
2021-08-01 12:20:33 -0500

    CI: install tree-sitter-cli form npm

--- scripts/ci-install-macos-latest.sh
+++ scripts/ci-install-macos-latest.sh
@@ -2,4 +2,3 @@ curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.ta
 sudo ln -s $(pwd)/nvim-osx64/bin/nvim /usr/local/bin
 mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
 ln -s $(pwd) ~/.local/share/nvim/site/pack/nvim-treesitter/start
-
--- scripts/ci-install-windows-latest.sh
+++ scripts/ci-install-windows-latest.sh
@@ -2,4 +2,3 @@ choco install neovim --pre
 mkdir -p ~/AppData/Local/nvim/pack/nvim-treesitter/start
 mkdir -p ~/AppData/Local/nvim-data
 cp -r $(pwd) ~/AppData/Local/nvim/pack/nvim-treesitter/start
-
