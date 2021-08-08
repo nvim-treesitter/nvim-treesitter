3f9e7061e751df8d3cf2d65f00dbfba150e36216
Santos Gallegos <stsewd@protonmail.com>
2021-08-07 21:12:22 -0500

    Use xcopy

--- scripts/ci-install-windows-latest.cmd
+++ scripts/ci-install-windows-latest.cmd
@@ -1,4 +1,4 @@
 choco install neovim --pre
 mkdir %homedrive%%homepath%\\AppData\\Local\\nvim\\pack\\nvim-treesitter\\start
 mkdir %homedrive%%homepath%\\AppData\\Local\\nvim-data
-copy * %homedrive%%homepath%\\AppData\\Local\\nvim\\pack\\nvim-treesitter\\start
+xcopy * %homedrive%%homepath%\\AppData\\Local\\nvim\\pack\\nvim-treesitter\\start /E/H
