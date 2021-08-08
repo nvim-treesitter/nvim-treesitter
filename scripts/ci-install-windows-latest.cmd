1610cb65d0986daf258bb04ed3c5d58b0ca68ff0
Santos Gallegos <stsewd@protonmail.com>
2021-08-07 21:33:02 -0500

    debug

--- scripts/ci-install-windows-latest.cmd
+++ scripts/ci-install-windows-latest.cmd
@@ -2,3 +2,4 @@ choco install neovim --pre
 mkdir %homedrive%%homepath%\\AppData\\Local\\nvim\\pack\\nvim-treesitter\\start
 mkdir %homedrive%%homepath%\\AppData\\Local\\nvim-data
 xcopy . %homedrive%%homepath%\\AppData\\Local\\nvim\\pack\\nvim-treesitter\\start /E/H
+dir %homedrive%%homepath%\\AppData\\Local\\nvim\\pack\\nvim-treesitter\\start
