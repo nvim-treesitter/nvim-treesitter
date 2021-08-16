4c8a2cd0af18ee2b648f6a6367ebe55a9eb6ef66
Clay <connorlay@pm.me>
2021-08-16 10:42:56 -0700

    Add doctype highlights

--- queries/heex/highlights.scm
+++ queries/heex/highlights.scm
@@ -1,29 +1,29 @@
-; HEEx text is highlighted as such
 (text) @text
-
-; HEEx has two types of comments, both are highlighted as such
 (comment) @comment
+(doctype) @constant
 
 ; HEEx attributes are highlighted as HTML attributes
 (attribute_name) @tag.attribute
-
-; Attributes are highlighted as strings
 (quoted_attribute_value) @string
 
-; HEEx supports HTML tags and are highlighted as such
 [
-  "<"
-  ">"
-  "</"
-  "/>"
-  "<%"
-  "<%="
-  "<%%="
-  "<%#"
   "%>"
+  "/>"
+  "<!"
+  "<"
+  "<%"
+  "<%#"
+  "<%%="
+  "<%="
+  "</"
+  ">"
   "{"
   "}"
 ] @tag.delimiter
+
+[
+  "="
+] @operator
 
 ; HEEx tags are highlighted as HTML
 (tag_name) @tag
@@ -31,5 +31,3 @@
 ; HEEx components are highlighted as types (Elixir modules)
 (component_name) @type
 
-; HEEx operators
-["="] @operator
