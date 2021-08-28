78b95626b3255b6b61cd904754b70666f5000dd0
leo60228 <leo@60228.dev>
2021-08-28 12:24:04 -0400

    address review

--- queries/pioasm/injections.scm
+++ queries/pioasm/injections.scm
@@ -1,9 +1,11 @@
+ [ (line_comment) (block_comment) ] @comment
+
 ((code_block
   (code_block_language) @_language
   (code_block_body) @c)
  (#eq? @_language "c-sdk"))
 
 ((code_block
-  (code_block_language) @_language
-  (code_block_body) @python)
- (#eq? @_language "python"))
+  (code_block_language) @language
+  (code_block_body) @content)
+ (#eq? @language "python"))
