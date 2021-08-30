cfccc352c9a30c6e6c26afb3840cc8818baac1e1
partizan <serg.partizan@gmail.com>
2021-08-30 21:18:04 +0300

    test: Add tests for is_in_node_range (#1756)
    * fix: is_in_node_range now includes end line and col
    
    This fixes no indents at the end of python files
    
    Refs #1136
    
    * update scala highlights (#1760)
    
    * add type highlights
    
    * add call expression highlights
    
    * add function definition highlights
    
    * add expression highlights
    
    * add literals highlights
    
    * add operator highlights
    
    * add punctuation highlights
    
    * add comment highlights
    
    Co-authored-by: Stevan Milic <stevan.milic@tradecore.com>
    
    * test: Add unit tests
    
    * Revert "fix: is_in_node_range now includes end line and col"
    
    This reverts commit 5a721fef5620eb2fae6d9cebe09bf7b230f2606f.
    
    * refactor test
    
    * apply stylua
    
    * fix luacheck
    
    * update `describe` text
    
    * smallfix
    
    Co-authored-by: Stevan Milic <stevan.milic@yahoo.com>
    Co-authored-by: Stevan Milic <stevan.milic@tradecore.com>

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,12 +5,14 @@ cd $HERE/..
 
 run() {
     nvim --headless --noplugin -u scripts/minimal_init.lua \
-        -c "PlenaryBustedDirectory tests/indent/ { minimal_init = './scripts/minimal_init.lua' }"
+        -c "PlenaryBustedDirectory $1 { minimal_init = './scripts/minimal_init.lua' }"
 }
 
 if [[ $1 = '--summary' ]]; then
     # really simple results summary by filtering plenary busted output
-    run 2> /dev/null | grep -E '^\S*(Success|Fail(ed)?|Errors?)'
+    run tests/indent/ 2> /dev/null | grep -E '^\S*(Success|Fail(ed)?|Errors?)'
+elif [[ $1 = '--unit' ]]; then
+    run tests/unit
 else
-    run
+    run tests/indent/
 fi
