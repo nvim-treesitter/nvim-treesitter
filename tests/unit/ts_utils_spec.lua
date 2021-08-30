ed6e5fbb1702d4d1f53a3014aad277e169ed29d8
Serg Tereshchenko <serg.partizan@gmail.com>
2021-08-30 18:12:41 +0300

    apply stylua

--- tests/unit/ts_utils_spec.lua
+++ tests/unit/ts_utils_spec.lua
@@ -1,43 +1,41 @@
 local tsutils = require "nvim-treesitter.ts_utils"
 
 describe("is_in_node_range", function()
-
   function test_is_in_node_range(line, col, expected)
     local node = {
-      range = function ()
-        return unpack({0, 3, 2, 5})
-      end
+      range = function()
+        return unpack { 0, 3, 2, 5 }
+      end,
     }
     return tsutils.is_in_node_range(node, line, col)
   end
 
-  it("false before node start", function ()
+  it("false before node start", function()
     assert.is_false(test_is_in_node_range(0, 0))
     assert.is_false(test_is_in_node_range(0, 1))
     assert.is_false(test_is_in_node_range(0, 2))
   end)
 
-  it("true at node start", function ()
+  it("true at node start", function()
     assert.is_true(test_is_in_node_range(0, 3))
   end)
 
-  it("true on first line of the node", function ()
+  it("true on first line of the node", function()
     assert.is_true(test_is_in_node_range(0, 4))
   end)
 
-  it("true between node lines", function ()
+  it("true between node lines", function()
     assert.is_true(test_is_in_node_range(1, 2))
     assert.is_true(test_is_in_node_range(1, 20))
   end)
 
-  it("false on node end", function ()
+  it("false on node end", function()
     -- Ranges are end-exclusive
     assert.is_false(test_is_in_node_range(2, 5))
   end)
 
-  it("false after node end", function ()
+  it("false after node end", function()
     assert.is_false(test_is_in_node_range(2, 6))
     assert.is_false(test_is_in_node_range(3, 0))
   end)
-
 end)
