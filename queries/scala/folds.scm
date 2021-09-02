d9850a6049213c17729c3eaa29e4422b024f5f18
Stevan Milic <stevan.milic@tradecore.com>
2021-09-02 13:24:37 +0200

    Add scala folds

---
+++ queries/scala/folds.scm
@@ -0,0 +1,14 @@
+(call_expression (block) @fold)
+
+[(class_definition)
+ (trait_definition)
+ (object_definition)
+ (function_definition)
+ (import_declaration)
+
+ (while_expression)
+ (do_while_expression)
+ (for_expression)
+ (try_expression)
+ (match_expression)
+] @fold
