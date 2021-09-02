9bea9b878da6148d7c6d4269d95edd39bec73487
Anon Ymous <73802848+ancientstraits@users.noreply.github.com>
2021-09-01 20:39:58 -0400

    vim.cmd vimscript syntax highlighting
    This pull request adds a query that performs whenever there is a function called "cmd" that accepts one string. It will change the syntax highlighting of that string into vimscript to make writing vimscript in lua easier.

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -9,4 +9,16 @@
   (#eq? @_cdef_identifier "cdef")
 )
 
+(
+  (function_call
+    (field_expression
+      (property_identifier) @_cmd_identifier)
+    (arguments
+      (string) @vim)
+  )
+
+  (#eq? @_cmd_identifier "cmd")
+)
+
+
 (comment) @comment
