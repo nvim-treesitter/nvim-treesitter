016c757d114a2535546415a24eb2a5c9e82cd3d9
Ronnie Magatti <ronniemagatti@gmail.com>
2021-09-13 23:32:40 -0600

    Address highlight comments about cwd, path, module

--- queries/hcl/highlights.scm
+++ queries/hcl/highlights.scm
@@ -86,9 +86,10 @@
 ; highlight identifier keys as though they were block attributes
 (object_elem key: (expression (variable_expr (identifier) @field)))
 
-(variable_expr (identifier) @variable.builtin (#any-of? @variable.builtin "var" "local"))
-((identifier) @keyword (#any-of? @keyword "module" "resource" "variable" "data" "locals" "terraform" "provider" "output"))
+((identifier) @keyword (#any-of? @keyword "module" "root" "cwd" "resource" "variable" "data" "locals" "terraform" "provider" "output"))
 ((identifier) @type.builtin (#any-of? @type.builtin "bool" "string" "number" "object" "tuple" "list" "map" "set" "any"))
+(variable_expr (identifier) @variable.builtin (#any-of? @variable.builtin "var" "local" "path"))
+(get_attr (identifier) @variable.builtin (#any-of? @variable.builtin  "root" "cwd" "module"))
 
 (object_elem val: (expression
   (variable_expr
