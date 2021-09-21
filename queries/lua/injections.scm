c6dfcc6a3d922b29f87050a879d41c06847e343e
Stephan Seitz <stephan.seitz@fau.de>
2021-09-21 20:38:45 +0200

    injections(lua): fix cdef injection
    Fixes #1840

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -7,6 +7,21 @@
   )
 
   (#eq? @_cdef_identifier "cdef")
+  (#match? @c "^[\"']")
+  (#offset! @c 0 1 0 -1)
+)
+
+(
+  (function_call
+    (field_expression
+      (property_identifier) @_cdef_identifier)
+    (arguments
+      (string) @c)
+  )
+
+  (#eq? @_cdef_identifier "cdef")
+  (#match? @c "^\\[\\[")
+  (#offset! @c 0 2 0 -2)
 )
 
 (comment) @comment
