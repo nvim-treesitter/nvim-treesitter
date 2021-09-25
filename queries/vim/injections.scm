b1387877472d28e15474aba80a4ea7f0fc805a0b
Santos Gallegos <stsewd@protonmail.com>
2021-09-25 00:17:46 -0500

    Vim: update highlights and injections

--- queries/vim/injections.scm
+++ queries/vim/injections.scm
@@ -6,4 +6,18 @@
 
 (autocmd_statement (pattern) @regex)
 
+((set_item
+   option: (option_name) @_option
+   value: (set_value) @vim)
+  (#any-of? @_option
+    "includeexpr" "inex"
+    "printexpr" "pexpr"
+    "formatexpr" "fex"
+    "indentexpr" "inde"
+    "foldtext" "fdt"
+    "foldexpr" "fde"
+    "diffexpr" "dex"
+    "patchexpr" "pex"
+    "charconvert" "ccv"))
+
 (comment) @comment
