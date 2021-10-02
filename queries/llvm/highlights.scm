6188437150d7cb28aa09e9c1788f7f6e67b72782
Benjamin Graham <benwilliamgraham@gmail.com>
2021-10-02 17:09:40 -0400

    LLVM parser/highlighter added

---
+++ queries/llvm/highlights.scm
@@ -0,0 +1,12 @@
+(type) @type
+(statement) @statement
+(number) @number
+(comment) @comment
+(string) @string
+(label) @label
+(keyword) @keyword
+(boolean) @boolean
+(float) @number
+(constant) @identifier
+(identifier) @constant
+(punctuation) @punctuation
