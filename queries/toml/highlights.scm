ecd9efd48611c42d7e51b50028788bf2b74b5c91
oxalica <oxalicc@pm.me>
2021-10-04 02:08:19 +0800

    Update toml highlight queries

--- queries/toml/highlights.scm
+++ queries/toml/highlights.scm
@@ -1,12 +1,35 @@
-(bare_key) @type.builtin
+; Properties
+;-----------
 
-(pair
-  (bare_key) @property)
+(bare_key) @property
+(quoted_key) @string
 
+; Literals
+;---------
+
+(boolean) @boolean
+(comment) @comment
 (string) @string
-(boolean) @constant.builtin
 (integer) @number
 (float) @float
-(comment) @comment
+(offset_date_time) @string.special
+(local_date_time) @string.special
+(local_date) @string.special
+(local_time) @string.special
+
+; Punctuation
+;------------
+
+"." @punctuation.delimiter
+"," @punctuation.delimiter
+
+"=" @operator
+
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"[[" @punctuation.bracket
+"]]" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
 
 (ERROR) @error
