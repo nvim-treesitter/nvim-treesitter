d5093bb0c2f26ccd5fa04c2058675de16c3ab793
Connor Lay (Clay) <connorlay@pm.me>
2021-10-06 12:30:40 -0700

    Update injections & highlights, add folds

--- queries/elixir/injections.scm
+++ queries/elixir/injections.scm
@@ -12,3 +12,10 @@
   (sigil_name) @_sigil_name 
   (quoted_content) @zig 
 (#eq? @_sigil_name "Z"))
+
+(sigil 
+  (sigil_name) @_sigil_name 
+  (quoted_content) @regex 
+(#any-of? @_sigil_name "r" "R"))
+
+(comment) @comment
