7135321cc254fd2a543f596a3648fdc481e54eef
Stephan Seitz <sseitz@nvidia.com>
2021-10-10 11:52:16 +0200

    ci: use nvim v0.5.1 on CI

--- scripts/ci-install-ubuntu-latest.sh
+++ scripts/ci-install-ubuntu-latest.sh
@@ -1,9 +1,9 @@
 sudo apt-get update
 sudo add-apt-repository universe
-wget -O - https://github.com/tree-sitter/tree-sitter/releases/download/v0.19.4/tree-sitter-linux-x64.gz | gunzip -c > tree-sitter
+wget -O - https://github.com/tree-sitter/tree-sitter/releases/download/${TREE_SITTER_CLI_TAG}/tree-sitter-linux-x64.gz | gunzip -c > tree-sitter
 sudo cp ./tree-sitter /usr/bin/tree-sitter
 sudo chmod uog+rwx /usr/bin/tree-sitter
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+wget https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim.appimage
 chmod u+x nvim.appimage
 mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
 ln -s $(pwd) ~/.local/share/nvim/site/pack/nvim-treesitter/start
