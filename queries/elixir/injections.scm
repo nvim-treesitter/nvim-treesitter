0e6c8a4e792cbea4c371ddd55abc6c97023f1cd7
Łukasz Niemier <lukasz@niemier.pl>
2021-10-20 13:20:05 +0200

    fix: Elixir non-doc sigils will no longer be flickering
    It also adds support for colouring documentation metadata attributes
    (ex. `@doc foo: :bar`).

--- queries/elixir/injections.scm
+++ queries/elixir/injections.scm
@@ -1,21 +1,21 @@
-(sigil 
-  (sigil_name) @_sigil_name 
-  (quoted_content) @surface 
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @surface
 (#eq? @_sigil_name "F"))
 
-(sigil 
-  (sigil_name) @_sigil_name 
-  (quoted_content) @heex 
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @heex
 (#eq? @_sigil_name "H"))
 
-(sigil 
-  (sigil_name) @_sigil_name 
-  (quoted_content) @zig 
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @zig
 (#eq? @_sigil_name "Z"))
 
-(sigil 
-  (sigil_name) @_sigil_name 
-  (quoted_content) @regex 
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @regex
 (#any-of? @_sigil_name "r" "R"))
 
 (comment) @comment
