07ab3d1fffd0460cae13809aefad262ad43c4c7f
Cezary Drożak <czarek@drozak.net>
2021-10-24 13:32:12 +0200

    fixup! feat(tests): test highlights

--- tests/highlight/common.lua
+++ tests/highlight/common.lua
@@ -2,19 +2,35 @@ local M = {}
 
 local assert = require "luassert"
 local Path = require "plenary.path"
+local ft_to_lang = require("nvim-treesitter.parsers").ft_to_lang
 
--- Get table of nodes from file
--- @param file  a tested file
-local function get_nodes(file)
+-- Get table with highlight query captures
+-- @param file A path to file to parse
+local function get_highlight_captures(file)
   assert(vim.fn.filereadable(file) == 1, string.format('File "%s" not readable', file))
 
   -- load reference file
   vim.cmd(string.format("edit %s", file))
 
-  local parser = vim.treesitter.get_parser(0)
-  local tstree = parser:parse()
+  local ft = vim.bo.filetype
+  local lang = ft_to_lang(ft)
 
-  return tstree:root()
+  local parser = vim.treesitter.get_parser(0, lang)
+  local tree = parser:parse()[1]
+
+  local query = vim.treesitter.query.get_query(lang, "highlights")
+  local captures = {}
+
+  for id, node, _ in query:iter_captures(tree:root(), 0, 0, -1) do
+    local capture = {}
+
+    capture.name = query.captures[id]
+    capture.begin_row, capture.begin_column, capture.end_row, capture.end_column = node:range()
+
+    table.insert(captures, capture)
+  end
+
+  return captures
 end
 
 local Runner = {}
@@ -30,12 +46,10 @@ function Runner:new(it, base_dir)
   return setmetatable(runner, self)
 end
 
-function Runner:expect_nodes(file, spec, title)
-  title = title and title or tostring(spec.on_line)
-
-  self.it(string.format("%s[%s]", file, title), function()
+function Runner:expect_nodes(file, spec)
+  self.it(string.format("%s", file), function()
     local path = self.base_dir / file
-    local nodes = get_nodes(path.filename)
+    local nodes = get_highlight_captures(path.filename)
     assert.are.same(nodes, spec)
   end)
 end
--- tests/highlight/d_spec.lua
+++ tests/highlight/d_spec.lua
@@ -2,12 +2,12 @@ local Runner = require("tests.highlight.common").Runner
 
 local runner = Runner:new(it, "tests/highlight/d")
 
-describe("D highlights", function()
+describe("D highlights:", function()
   runner:expect_nodes("import.d", {
     {
-      begin_line = 1,
+      begin_row = 1,
       begin_column = 1,
-      end_line = 1,
+      end_row = 1,
       end_column = 8,
       name = "@include",
     },
