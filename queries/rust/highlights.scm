bd1399105026c7b067dc197b31ccdb2602700a86
Santos Gallegos <stsewd@protonmail.com>
2021-10-30 12:09:54 -0500

    Rust: highlight uppercase identifiers in match arms as constant (#1940)
    * Rust: highlight uppercase identifiers in match arms as constant
    
    Closes https://github.com/nvim-treesitter/nvim-treesitter/issues/1928
    
    * Highlight builtins

--- queries/rust/highlights.scm
+++ queries/rust/highlights.scm
@@ -5,6 +5,8 @@
 ; Identifier conventions
 
 (identifier) @variable
+((identifier) @type
+ (#match? @type "^[A-Z]"))
 (const_item
   name: (identifier) @constant)
 ; Assume all-caps names are constants
@@ -81,6 +83,19 @@
     "::"
     name: (identifier) @constant)
   (#match? @constant "^[A-Z]"))
+
+; Assume uppercase names in a match arm are constants.
+((match_arm
+   pattern: (match_pattern (identifier) @constant))
+ (#match? @constant "^[A-Z]"))
+((match_arm
+   pattern: (match_pattern
+     (scoped_identifier
+       name: (identifier) @constant)))
+ (#match? @constant "^[A-Z]"))
+
+((identifier) @constant.builtin
+ (#any-of? @constant.builtin "Some" "None" "Ok" "Err"))
 
 ;; Macro definitions
 "$" @function.macro
