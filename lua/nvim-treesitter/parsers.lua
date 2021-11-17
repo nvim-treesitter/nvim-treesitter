1e5e028cb7b0c4ed20b05d4d71b5142392c5e7d0
Santos Gallegos <stsewd@protonmail.com>
2021-11-16 21:19:51 -0500

    Format with stylua
    CI is failing for stylua due to this.

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -792,9 +792,7 @@ function M.available_parsers()
   else
     return vim.tbl_filter(function(p)
       return not M.list[p].install_info.requires_generate_from_grammar
-    end, vim.tbl_keys(
-      M.list
-    ))
+    end, vim.tbl_keys(M.list))
   end
 end
 
