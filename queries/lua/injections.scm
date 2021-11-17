d78fb79ed6f3e5260db48f352d8dcbd3e82935a3
vhyrro <76052559+vhyrro@users.noreply.github.com>
2021-11-17 03:14:22 +0100

    Add basic vimscript injection in `vim.cmd`/`nvim_command`/`nvim_exec` (#1922)

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -1,3 +1,4 @@
+; C Injections
 (
   (function_call
     (field_expression
@@ -22,6 +23,31 @@
   (#eq? @_cdef_identifier "cdef")
   (#match? @c "^\\[\\[")
   (#offset! @c 0 2 0 -2)
+)
+
+; Vimscript Injections
+(
+  (function_call
+    (field_expression) @_vimcmd_identifier
+    (arguments
+      (string) @vim)
+  )
+
+  (#any-of? @_vimcmd_identifier "vim.cmd" "vim.api.nvim_command" "vim.api.nvim_exec")
+  (#match? @vim "^[\"']")
+  (#offset! @vim 0 1 0 -1)
+)
+
+(
+  (function_call
+    (field_expression) @_vimcmd_identifier
+    (arguments
+      (string) @vim)
+  )
+
+  (#any-of? @_vimcmd_identifier "vim.cmd" "vim.api.nvim_command" "vim.api.nvim_exec")
+  (#match? @vim "^\\[\\[")
+  (#offset! @vim 0 2 0 -2)
 )
 
 (comment) @comment
