3131b75c3e74a36ab80b073715f804d4bb27c9f5
Stephan Seitz <stephan.seitz@fau.de>
2021-11-20 15:14:09 +0100

    Update run_tests to be more scalable with more test folders added

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -8,11 +8,9 @@ run() {
         -c "PlenaryBustedDirectory $1 { minimal_init = './scripts/minimal_init.lua' }"
 }
 
-if [[ $1 = '--summary' ]]; then
-    # really simple results summary by filtering plenary busted output
-    run tests/indent/ 2> /dev/null | grep -E '^\S*(Success|Fail(ed)?|Errors?)'
-elif [[ $1 = '--unit' ]]; then
-    run tests/unit
+if [[ $2 = '--summary' ]]; then
+    ## really simple results summary by filtering plenary busted output
+    run tests/$1  2> /dev/null | grep -E '^\S*(Success|Fail(ed)?|Errors?)\s*:'
 else
-    run tests/indent/
+    run tests/$1
 fi
