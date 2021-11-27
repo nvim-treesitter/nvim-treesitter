ad615ffc4c829564adb2a183c78f334b42c4a959
Stephan Seitz <stephan.seitz@fau.de>
2021-11-27 12:52:45 +0100

    injections(yaml): inject bash on key "run"

--- queries/yaml/injections.scm
+++ queries/yaml/injections.scm
@@ -1,1 +1,10 @@
 (comment) @comment
+
+(block_mapping_pair
+  key: (flow_node) @_run (#eq? @_run "run")
+  value: (flow_node
+           (plain_scalar) @bash))
+
+(block_mapping_pair
+  key: (flow_node) @_run (#eq? @_run "run")
+  value: (block_node) @bash (#offset! @bash 0 0 0 0))
