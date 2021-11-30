675ecbb0bd676a4e0e9e2ab89fc85825f08759a4
Stephan Seitz <stephan.seitz@fau.de>
2021-11-30 23:27:08 +0100

    Fix pug injection

--- queries/vue/injections.scm
+++ queries/vue/injections.scm
@@ -30,8 +30,8 @@
     (start_tag
       (attribute
         (quoted_attribute_value (attribute_value) @_lang)))
-    (raw_text) @pug)
-  (#match? @_lang "pug")
+    (text) @pug)
+  (#eq? @_lang "pug")
 )
 
 (comment) @comment
