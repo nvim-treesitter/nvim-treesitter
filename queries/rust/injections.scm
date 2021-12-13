225cac5e6b762bf5223d96df20d4b2ff658f5f34
Stephan Seitz <stephan.seitz@fau.de>
2021-12-13 20:59:19 +0100

    injections(rust): inject regex into `Regex::new`

--- queries/rust/injections.scm
+++ queries/rust/injections.scm
@@ -19,3 +19,16 @@
     (#eq? @_html_def "html")
 )
 
+(call_expression
+  function: (scoped_identifier
+    path: (identifier) @_regex (#eq? @_regex "Regex")
+    name: (identifier) @_new (#eq? @_new "new"))
+  arguments: (arguments
+    (raw_string_literal) @regex))
+
+(call_expression
+  function: (scoped_identifier
+    path: (scoped_identifier (identifier) @_regex (#eq? @_regex "Regex").)
+    name: (identifier) @_new (#eq? @_new "new"))
+  arguments: (arguments
+    (raw_string_literal) @regex))
