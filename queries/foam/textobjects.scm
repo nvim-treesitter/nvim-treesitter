efaf985b2481cc8368c711efd9fd0f674f76e716
Elwardi <elwardifadeli@gmail.com>
2021-12-14 19:55:43 +0100

    Remove OpenFOAM textobjects

--- queries/foam/textobjects.scm
+++
@@ -1,8 +0,0 @@
-(dict) @class.outer
-((dict_core) @class.inner)
-((key_value value: _? @_start (_)* _? @parameter.inner)
-    (#make-range! "function.inner" @_start @parameter.inner)) @function.outer
-(code (_)* @class.inner) @class.outer
-((comment) @_start ((comment)+) @_end
- (#make-range! "comment.outer" @_start @_end))
-(comment) @comment.inner
