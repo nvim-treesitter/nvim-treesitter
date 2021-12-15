c7f4b7c935b67cafc0dfd940bbafb3483be07fb2
Elwardi <elwardifadeli@gmail.com>
2021-12-15 17:32:35 +0100

    Fix stylua linting

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -850,12 +850,12 @@ list.foam = {
   install_info = {
     url = "https://github.com/FoamScience/tree-sitter-foam",
     branch = "master",
-    files = {"src/parser.c", "src/scanner.cc"},
+    files = { "src/parser.c", "src/scanner.cc" },
     generate_requires_npm = true,
   },
   maintainers = { "@FoamScience" },
   filetype = "foam",
-  used_by = {"OpenFOAM"},
+  used_by = { "OpenFOAM" },
   -- Queries might change over time on the grammar's side
   -- Otherwise everything runs fine
   experimental = true,
