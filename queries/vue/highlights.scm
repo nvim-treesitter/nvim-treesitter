70c9805e92ebed7de603b5f4a4840eb5fd9fc90a
Michael Chris Lopez <hello@michaelchris.space>
2021-12-18 15:31:09 +0800

    highlights(vue): improve vue highlights
    - remove template_name and element and make more specific targeting

--- queries/vue/highlights.scm
+++ queries/vue/highlights.scm
@@ -1,20 +1,23 @@
 ; inherits: html_tags
 
 [
-  (template_element)
-  (directive_attribute)
   (directive_dynamic_argument)
   (directive_dynamic_argument_value)
-  (component)
 ] @tag
 
-(element) @string
 (interpolation) @punctuation.special
 (interpolation
   (raw_text) @none)
 
+(directive_name) @tag.attribute
+
+(directive_attribute
+  (quoted_attribute_value) @punctuation.special)
+
+(directive_attribute
+  (quoted_attribute_value (attribute_value) @none))
+
 [
   (directive_modifier)
-  (directive_name)
   (directive_argument)
 ] @method
