273472cc92b311db947b7062067191213289dba9
Michael Chris Lopez <hello@michaelchris.space>
2021-12-18 15:27:12 +0800

    highlights(html_tags): more specific attribute_value target

--- queries/html_tags/highlights.scm
+++ queries/html_tags/highlights.scm
@@ -2,8 +2,8 @@
 (erroneous_end_tag_name) @error
 (comment) @comment
 (attribute_name) @tag.attribute
-(attribute_value) @string
-(quoted_attribute_value) @string
+(attribute
+  (quoted_attribute_value) @string)
 (text) @text
 
 ((element (start_tag (tag_name) @_tag) (text) @text.title)
