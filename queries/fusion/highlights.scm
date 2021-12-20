e494a62c027b0b22be86767f23c92c59ef3aae4b
Jirgn <jirgn76@googlemail.com>
2021-12-20 22:53:45 +0100

    fix(fusion): update to (highlight breaking) version 1.1.0
    * update grammer to introduce afx support
    * update tests and queries

--- queries/fusion/highlights.scm
+++ queries/fusion/highlights.scm
@@ -1,6 +1,45 @@
 (comment) @comment
+(afx_comment) @comment
+
+; identifiers afx
+(afx_opening_element
+  (afx_identifier) @tag)
+(afx_closing_element
+  (afx_identifier) @tag)
+(afx_element_self_closing
+  (afx_identifier) @tag)
+
+(afx_attribute
+  (afx_property_identifier) @attribute)
+
+(afx_opening_element
+  [
+   "<"
+   ">"
+   "/"
+   ] @punctation.bracket
+ )
+ (afx_closing_element
+  [
+   "<"
+   ">"
+   "/"
+   ] @punctation.bracket
+ )
+(afx_element_self_closing
+  [
+   "<"
+   ">"
+   "/"
+   ] @punctation.bracket
+ )
 
 ; identifiers eel
+
+(eel_object_path
+  (eel_path_identifier) @variable.builtin
+  (#match? @variable.builtin "^(this|props)$")
+)
 
 (eel_object_path
   (eel_path_identifier) @variable)
@@ -11,7 +50,6 @@
 (eel_method_name) @function
 
 (eel_parameter) @variable
-
 
 ; identifiers fusion
 ; -----------
@@ -84,5 +122,4 @@
  "."
  "?"
 ] @punctuation.delimiter
-
 
