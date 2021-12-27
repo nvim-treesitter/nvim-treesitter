a64f509beb02f3dce691daed44ca32b430fa10c5
Jirgn <jirgn76@googlemail.com>
2021-12-27 23:40:37 +0100

    feat: add indent for fusion and afx

---
+++ queries/fusion/indents.scm
@@ -0,0 +1,15 @@
+[
+  (block)
+  (value_dsl)
+  (afx_element)
+  (afx_element_self_closing)
+] @indent
+
+
+(block end: _ @branch)
+(value_dsl end: _ @branch)
+[
+  (afx_closing_element)
+] @branch
+
+(comment) @ignore
