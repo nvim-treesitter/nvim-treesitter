3990ecd164645a7cd0b2675435dd871046df28de
antosha417 <anton.kavalkou@gmail.com>
2022-01-16 15:26:01 +0200

    remove ftdetect. add filetype hocon to minimal_init.lua

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -2,6 +2,8 @@ vim.cmd [[set runtimepath+=.]]
 vim.cmd [[runtime! plugin/plenary.vim]]
 vim.cmd [[runtime! plugin/nvim-treesitter.vim]]
 
+vim.cmd [[au BufRead,BufNewFile *.conf set filetype=hocon]]
+
 vim.o.swapfile = false
 vim.bo.swapfile = false
 
