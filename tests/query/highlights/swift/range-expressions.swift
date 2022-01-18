c6821cac7ae21d683877f554b94e8a8500ac025a
Stephan Seitz <stephan.seitz@fau.de>
2022-01-18 22:17:17 +0100

    ci: add swift highlight tests

---
+++ tests/query/highlights/swift/range-expressions.swift
@@ -0,0 +1,8 @@
+  for i in 0..<5 {
+    //       ^ operator
+    print(i)
+  }
+  for i in 0...5 {
+    //       ^ operator
+    print(i)
+  }
---
+++ tests/query/highlights/swift/string-interpolation.swift
@@ -0,0 +1,7 @@
+var one = 1
+var two = 2
+var name = "Let's do some math: \(one) + \(one) = \(one + one) = \(two)"
+//                              ^ punctuation.bracket
+//                                 ^ variable
+//                                   ^ punctuation.bracket
+//                                                 ^ punctuation.bracket
