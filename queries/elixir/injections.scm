fa3074eab7f32c5fd48cc0e08b3e2069fd733000
Clay <connorlay@pm.me>
2022-01-23 20:40:21 -0800

    Add new Elixir language injections

--- queries/elixir/injections.scm
+++ queries/elixir/injections.scm
@@ -1,21 +1,43 @@
-(sigil
-  (sigil_name) @_sigil_name
-  (quoted_content) @surface
-(#eq? @_sigil_name "F"))
+; Comments
+(comment) @comment
 
+; Documentation
+(unary_operator
+  operator: "@"
+  operand: (call
+    target: ((identifier) @_identifier (#any-of? @_identifier "moduledoc" "typedoc" "shortdoc" "doc"))
+    (arguments [
+      (string (quoted_content) @markdown)
+      (sigil (quoted_content) @markdown)
+    ])))
+
+; HEEx
 (sigil
   (sigil_name) @_sigil_name
   (quoted_content) @heex
 (#eq? @_sigil_name "H"))
 
+; Surface
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @surface
+(#eq? @_sigil_name "F"))
+
+; Zigler
 (sigil
   (sigil_name) @_sigil_name
   (quoted_content) @zig
-(#eq? @_sigil_name "Z"))
+(#any-of? @_sigil_name "z" "Z"))
 
+; Regex
 (sigil
   (sigil_name) @_sigil_name
   (quoted_content) @regex
 (#any-of? @_sigil_name "r" "R"))
 
-(comment) @comment
+; Jason
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @json
+(#any-of? @_sigil_name "j" "J"))
+
