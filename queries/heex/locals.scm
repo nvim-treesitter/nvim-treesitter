9e90866bcea3e392126c4eb90fde56b1c0b56b69
Clay <connorlay@pm.me>
2022-01-23 18:09:29 -0800

    Upgrade to HEEx parser 0.3.0

---
+++ queries/heex/locals.scm
@@ -0,0 +1,11 @@
+; HEEx tags and components are references
+[
+  (tag_name)
+  (component_name)
+] @reference
+
+; Create a new scope within each HEEx tag or component
+[
+  (tag)
+  (component)
+] @scope
