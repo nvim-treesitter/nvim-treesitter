519de4c974e629ba07459567c6aa6d3ccd324a1d
Stephan Seitz <stephan.seitz@fau.de>
2022-01-25 22:19:10 +0100

    indents(go): add tests

---
+++ tests/indent/go/issue-2369.go
@@ -0,0 +1,14 @@
+// https://github.com/nvim-treesitter/nvim-treesitter/issues/2369
+package main
+
+import "fmt"
+
+func goodIndent(param string) {
+	fmt.Println("typing o here works as expected")
+}
+
+func badIndent(
+	param string, // this is the difference
+) {
+	fmt.Println("typing o here triggers bad indent")
+}
