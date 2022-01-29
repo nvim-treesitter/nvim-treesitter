e94f6b2bc405961fadc82ec2b749d9d3fbf9b2a6
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2022-01-29 11:04:13 +0100

    supercollider: update indent query to match upstream

--- queries/supercollider/indents.scm
+++ queries/supercollider/indents.scm
@@ -1,8 +1,18 @@
 [
-  (collection)
-  (parameter_call_list)
   (function_block)
-  (code_block)
+  (binary_expression)
+  (collection)
+  (indexed_collection)
+  (parameter_call_list)
+  (function_call)
+  (class_def)
+  (classvar)
+  (const)
+  (instance_var)
+  (variable_definition)
+  (variable_definition_sequence (variable_definition))
+  (control_structure)
+  (return_statement)
 ] @indent
 
 [
@@ -14,3 +24,8 @@
   "["
   "]"
 ] @branch
+
+[
+  (block_comment)
+  (line_comment)
+] @ignore
