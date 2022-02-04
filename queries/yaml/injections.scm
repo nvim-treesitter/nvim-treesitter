71cfcff51097011eb1cb0c3048c8a949d983b79e
Stephan Seitz <stephan.seitz@fau.de>
2022-02-04 21:12:44 +0100

    injections(yaml): injection on Gitlab's before_script/after_script

--- queries/yaml/injections.scm
+++ queries/yaml/injections.scm
@@ -2,17 +2,17 @@
 
 ;; Github actions ("run") / Gitlab CI ("scripts")
 (block_mapping_pair
-  key: (flow_node) @_run (#any-of? @_run "run" "script")
+  key: (flow_node) @_run (#any-of? @_run "run" "script" "before_script" "after_script")
   value: (flow_node
            (plain_scalar) @bash))
 
 (block_mapping_pair
-  key: (flow_node) @_run (#any-of? @_run "run" "script")
+  key: (flow_node) @_run (#any-of? @_run "run" "script" "before_script" "after_script")
   value: (block_node
            (block_scalar) @bash (#offset! @bash 0 0 0 0)))
 
 (block_mapping_pair
-  key: (flow_node) @_run (#any-of? @_run "run" "script")
+  key: (flow_node) @_run (#any-of? @_run "run" "script" "before_script" "after_script")
   value: (block_node
            (block_sequence
              (block_sequence_item
