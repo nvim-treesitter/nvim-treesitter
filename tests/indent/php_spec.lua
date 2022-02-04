b9069a81b9c42c4d6d1ee4ebc4cd5feb35aa590d
Stephan Seitz <stephan.seitz@fau.de>
2022-02-04 23:36:36 +0100

    tests: only compare indents not contents in indent tests

--- tests/indent/php_spec.lua
+++ tests/indent/php_spec.lua
@@ -18,7 +18,7 @@ describe("indent PHP:", function()
     run:new_line("example.php", { on_line = 11, text = "// new line starts 1 indentation to far", indent = 4 })
     run:new_line(
       "example2.php",
-      { on_line = 5, text = "// indendation with `enter` in insert mode is not correct", indent = 4 }
+      { on_line = 5, text = "indendation with `enter` in insert mode is not correct", indent = 4 }
     )
   end)
 end)
