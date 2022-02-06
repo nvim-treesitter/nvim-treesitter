da6132ebbe39424c14e73fe1a7d2e3a9c028c645
Connor Lay (Clay) <connorlay@pm.me>
2022-02-06 12:48:38 -0800

    Add Gleam indents

---
+++ queries/gleam/indents.scm
@@ -0,0 +1,21 @@
+; Gleam indents similar to Rust and JavaScript
+[
+  (case)
+  (case_clause)
+  (constant)
+  (expression_group)
+  (function)
+  (let)
+  (list)
+  (public_function)
+  (public_opaque_type_definition)
+  (public_type_definition)
+  (tuple)
+  (type_definition)
+] @indent
+
+[
+  ")"
+  "]"
+  "}"
+] @indent_end @branch
