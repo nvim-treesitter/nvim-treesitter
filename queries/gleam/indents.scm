30941ea0b34ac9be725c2d9cfd60bbde8a441e7e
Connor Lay (Clay) <connorlay@pm.me>
2022-02-12 15:12:05 -0800

    Gleam indentation tests

--- queries/gleam/indents.scm
+++ queries/gleam/indents.scm
@@ -1,16 +1,25 @@
 ; Gleam indents similar to Rust and JavaScript
 [
+  (assert)
   (case)
   (case_clause)
   (constant)
   (expression_group)
+  (external_function)
   (function)
+  (import)
   (let)
   (list)
+  (public_constant)
+  (public_external_function)
   (public_function)
   (public_opaque_type_definition)
+  (public_type_alias)
   (public_type_definition)
+  (todo)
+  (try)
   (tuple)
+  (type_alias)
   (type_definition)
 ] @indent
 
@@ -19,3 +28,6 @@
   "]"
   "}"
 ] @indent_end @branch
+
+; Gleam pipelines are not indented, but other binary expression chains are
+((binary_expression operator: _ @_operator) @indent (#not-eq? @_operator "|>"))
