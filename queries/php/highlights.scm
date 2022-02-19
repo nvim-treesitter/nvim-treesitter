81d1cbc6d6c1586bdacb6b80d2aa36a71b70d235
Wilman Barrios <wilmanbarrios@gmail.com>
2022-02-19 13:36:23 -0400

    highlights(php): add namespace aliasing name as type

--- queries/php/highlights.scm
+++ queries/php/highlights.scm
@@ -20,6 +20,7 @@
   name: (name) @type)
 (namespace_use_clause
   [(name) (qualified_name)] @type)
+(namespace_aliasing_clause (name)) @type
 (class_interface_clause
   [(name) (qualified_name)] @type)
 (scoped_call_expression
