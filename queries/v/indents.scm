1bb30abe17058b39d4e681e9bbfe8c5f2a448a97
tami5 <kkharji@protonmail.com>
2022-02-22 17:36:44 +0300

    feat(vlang): initial support
    Add support for vlang filetypes.
    
    - [ ] Highlight `C` as builtin variable. This is FFI in vlang land,
      where C act like extern and access c functions. The vlang parser does
      some extension between C function calls and arguments but I believe
      highlighting C as builtin variable is sufficient indicator for now. I
      tried to use offset! but failed. Any suggestions?
    - [ ] Set up parser url. the vlang parser is located within [vls] repo. Is
      installing from nested repo supported? `tree_sitter_v/src/parser.c`?
    
    [vls]: https://github.com/vlang/vls/tree/master/tree_sitter_v
    
    cc @elianiva @theHamsta

---
+++ queries/v/indents.scm
@@ -0,0 +1,18 @@
+[(import_declaration
+  (const_declaration)
+  (var_declaration)
+  (type_declaration)
+  (literal_value)
+  (type_initializer)
+  (block)
+  (map)
+  (call_expression)
+  (parameter_list))]
+@indent
+
+[ "}"]
+@branch
+
+(parameter_list ")" @branch)
+
+(comment) @ignore
