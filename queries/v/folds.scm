c19d14f39593ff9bc028db6db35f9fc60966543b
Stephan Seitz <stephan.seitz@fau.de>
2022-02-22 18:48:05 +0100

    parser(vlang): fix location of remote repo

--- queries/v/folds.scm
+++ queries/v/folds.scm
@@ -1,8 +1,6 @@
-;; WARNING: Doesn't seems to work ..
 [(function_declaration)
  (const_declaration)
  (type_declaration)
- (var_declaration)
  (import_declaration)
  (if_expression)
  (struct_declaration)] @fold
