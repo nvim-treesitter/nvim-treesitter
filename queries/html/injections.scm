1e3e33cab8f54aaaee39a2493f13cbcea0d48403
Benny Powers <web@bennypowers.com>
2022-02-25 07:42:31 -0500

    fix: script mimetypes, unquoted regexp attrs

--- queries/html/injections.scm
+++ queries/html/injections.scm
@@ -8,33 +8,42 @@
 
 (comment) @comment
 
+; <input pattern="[0-9]">
 (element
-   (self_closing_tag
+   (_
       (tag_name) @_tagname (#eq? @_tagname "input")
       ((attribute
           (attribute_name) @_attr
-          (quoted_attribute_value (attribute_value) @regex)))
-       (#eq? @_attr "pattern")))
+          (quoted_attribute_value (attribute_value) @regex)
+       (#eq? @_attr "pattern")))))
+(element
+   (_
+      (tag_name) @_tagname (#eq? @_tagname "input")
+      ((attribute
+          (attribute_name) @_attr
+          (attribute_value) @regex
+       (#eq? @_attr "pattern")))))
 
-(element
+; <script async defer>
+(script_element
    (start_tag
-      (tag_name) @_tagname (#eq? @_tagname "input")
-      ((attribute
-          (attribute_name) @_attr
-          (quoted_attribute_value (attribute_value) @regex)))
-       (#eq? @_attr "pattern")))
+      ((attribute (attribute_name) @_attr (#not-eq? @_attr "type"))))
+   (raw_text) @javascript)
+
+; should work but chokes on the negation...
+; (script_element !attribute (raw_text) @javascript)
 
 (script_element
    (start_tag
       ((attribute
            (attribute_name) @_attr (#eq? @_attr "type")
            (quoted_attribute_value (attribute_value) @_type))
-        (#any-of? @_type "module" "application/javascript")))
-   (raw_text) @javascript)
-
-(script_element
-   (start_tag
-      ((attribute (attribute_name) @_attr (#not-eq? @_attr "type"))))
+        (#any-of? @_type "module"
+                         "application/ecmascript"
+                         "application/javascript"
+                         "text/ecmascript"
+                         "text/javascript"
+        )))
    (raw_text) @javascript)
 
 (script_element
@@ -52,4 +61,5 @@
            (quoted_attribute_value (attribute_value) @_type))
      (#eq? @_type "text/markdown")))
    (raw_text) @markdown)
+
 
