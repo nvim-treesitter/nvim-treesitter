71a4a13ed1bd7499cfb5de1d14af2cdcc4293fdf
Tomas Sandven <tomas@sandven.email>
2022-04-05 12:16:14 +0200

    Add aligned indents for multi-line strings

--- queries/yang/indents.scm
+++ queries/yang/indents.scm
@@ -1,5 +1,8 @@
-(block) @indent
-(block "}" @branch)
-
+(module) @indent
+(submodule) @indent
 (statement) @indent
 (statement ";" @indent_end)
+(block "}" @indent_end @branch)
+
+((string) @aligned_indent
+ (#set! "delimiter" "\"\""))
