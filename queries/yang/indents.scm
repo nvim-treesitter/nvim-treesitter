6290e3ec1ad9c4cbd15fc0621a3449844026ba4b
Tomas Sandven <tomas@sandven.email>
2022-04-07 10:35:18 +0200

    Fix YANG indentation for extension statements

--- queries/yang/indents.scm
+++ queries/yang/indents.scm
@@ -1,7 +1,9 @@
 (module) @indent
 (submodule) @indent
 (statement) @indent
+(extension_statement) @indent
 (statement ";" @indent_end)
+(extension_statement ";" @indent_end)
 (block "}" @indent_end @branch)
 
 ((string) @aligned_indent
