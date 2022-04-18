d0a34b972bbd7671f85bd6dd95ece9a287f0d9b4
Stephan Seitz <stephan.seitz@fau.de>
2022-04-19 01:03:36 +0200

    highlights(markdown): conceal check boxes

--- queries/markdown/highlights.scm
+++ queries/markdown/highlights.scm
@@ -88,3 +88,6 @@
 (inline_link 
   "["  @conceal
   (#set! conceal ""))
+
+((shortcut_link) @conceal (#set! conceal "") (eq? @conceal "[ ]"))
+((shortcut_link) @conceal (#set! conceal "☒") (eq? @conceal "[x]"))
