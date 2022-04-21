986cc390d51a9261af7c48a27c960cae113b02f6
Lewis Russell <lewis6991@gmail.com>
2022-04-21 10:48:47 +0100

    fix(fold): don't include whitespace end regions
    Some languages that are difficult to parse via Treesitter may
    incorrectly include whitespace lines at the end of regions. This can
    makes the calculated folds sub-optimal.
    
    To recitfy, use a custom directive (trim), to calculate the range with
    the trailing whitespace lines removed. Note this only works if the
    region end column is 0.
    
    Also added folds for Make.

--- queries/markdown/folds.scm
+++ queries/markdown/folds.scm
@@ -1,6 +1,9 @@
-[
-  (fenced_code_block)
-  (indented_code_block)
-  (list)
-  (section)
-] @fold
+(
+  [
+    (fenced_code_block)
+    (indented_code_block)
+    (list)
+    (section)
+  ] @fold
+  (#trim! @fold)
+)
