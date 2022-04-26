b6239543e686d8690d7e42fc80f34b6ce9d9507a
Graham Bates <info@grahambates.com>
2022-04-26 17:53:51 +0100

    feat: add m68k parser and queries

---
+++ queries/m68k/highlights.scm
@@ -0,0 +1,73 @@
+(symbol) @variable
+
+(label name: (symbol) @constant)
+
+[
+  (instruction_mnemonic)
+  (directive_mnemonic)
+] @function.builtin
+
+(include (directive_mnemonic) @include)
+(include_bin (directive_mnemonic) @include)
+(include_dir (directive_mnemonic) @include)
+
+
+(size) @attribute
+
+(macro_definition name: (symbol) @function.macro)
+(macro_call name: (symbol) @function.macro)
+
+[
+  (path)
+  (string_literal)
+] @string
+
+[
+  (decimal_literal)
+  (hexadecimal_literal)
+  (octal_literal)
+  (binary_literal)
+] @number
+
+[
+  (reptn)
+  (carg)
+  (narg) 
+  (macro_arg)
+] @variable.builtin
+
+[
+  (control_mnemonic)
+  (address_register)
+  (data_register)
+  (float_register)
+  (named_register)
+] @keyword
+
+(repeat (control_mnemonic) @repeat)
+(conditional (control_mnemonic) @conditional)
+
+(interpolated (macro_arg) @embedded) 
+
+(comment) @comment
+
+[
+  (operator)
+  "="
+  "#"
+] @operator
+
+[
+  "."
+  ","
+  "/"
+  "-"
+] @punctuation.delimiter
+
+[
+  "("
+  ")"
+  ")+"
+] @punctuation.bracket
+
+(section) @namespace
