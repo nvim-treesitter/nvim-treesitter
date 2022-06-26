002084b1bea4311fdac184f080206bf890937d80
Matthias Deiml <matthias@deiml.net>
2022-06-26 18:02:29 +0200

    feat(markdown)!: switch to split parser (#3048)
    * switch to split markdown parser with separate block and inline parsers to improve performance
    * add exclude_children! directive (useful for something like Injected markdown incorrectly highlights indented docstrings #2212)
    * split markdown queries into block and inline ones and add the injection for inline into block grammar
    * add include_dir option to parser configs (needed because the two grammars don't live in the repos root directory)
    
    BREAKING CHANGE: downstream queries need to be adapted to new parser

--- queries/markdown/injections.scm
+++ queries/markdown/injections.scm
@@ -1,9 +1,9 @@
 (fenced_code_block
   (info_string
     (language) @language)
-  (code_fence_content) @content)
+  (code_fence_content) @content (#exclude_children! @content))
 
 ((html_block) @html)
-((html_tag) @html)
 
-(document . (thematic_break) (_) @yaml @combined (thematic_break))
+(document . (section . (thematic_break) (_) @yaml @combined (thematic_break)))
+((inline) @markdown_inline (#exclude_children! @markdown_inline))
