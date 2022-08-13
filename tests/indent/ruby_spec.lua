48d88ddbff301d69703f0942c75603d545670782
Zheng Piaodan <zhengpiaodan@gmail.com>
2022-08-13 16:56:02 +0800

    fix(ruby): indent of brackets & parens

--- tests/indent/ruby_spec.lua
+++ tests/indent/ruby_spec.lua
@@ -6,6 +6,12 @@ local run = Runner:new(it, "tests/indent/ruby", {
 })
 
 describe("indent Ruby:", function()
+  describe("whole file:", function()
+    run:whole_file(".", {
+      expected_failures = {},
+    })
+  end)
+
   describe("new line:", function()
     run:new_line("indent-unless.rb", { on_line = 1, text = "stmt", indent = 2 })
     run:new_line("indent-assignment.rb", { on_line = 1, text = "1 +", indent = 2 })
