2a9fa9d0810f1b2a12366426cccf44fb56dacef5
Stephan Seitz <stephan.seitz@fau.de>
2022-08-29 23:40:33 +0200

    highlights(fish): use `@function.call`

--- queries/fish/highlights.scm
+++ queries/fish/highlights.scm
@@ -106,7 +106,7 @@
 (command_substitution_dollar "$" @punctuation.bracket)
 
 ; non-bultin command names
-(command name: (word) @function)
+(command name: (word) @function.call)
 
 ; derived from builtin -n (fish 3.2.2)
 (command
