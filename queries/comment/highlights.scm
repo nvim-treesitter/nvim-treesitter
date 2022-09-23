dd2afc7ea64f87be97b33427417ddc05609f0b25
Erik Westrup <erik.westrup@gmail.com>
2022-09-23 19:47:33 +0200

    Use @text.note instead of @text.warning

--- queries/comment/highlights.scm
+++ queries/comment/highlights.scm
@@ -7,11 +7,17 @@
 
 (tag (name) @text.note (user)? @constant)
 
+((tag ((name) @text.note))
+ (#any-of? @text.note "NOTE"))
+
+("text" @text.note
+ (#any-of? @text.note "NOTE"))
+
 ((tag ((name) @text.warning))
- (#any-of? @text.warning "TODO" "HACK" "WARNING" "NOTE"))
+ (#any-of? @text.warning "TODO" "HACK" "WARNING"))
 
 ("text" @text.warning
- (#any-of? @text.warning "TODO" "HACK" "WARNING" "NOTE"))
+ (#any-of? @text.warning "TODO" "HACK" "WARNING"))
 
 ((tag ((name) @text.danger))
  (#any-of? @text.danger "FIXME" "XXX" "BUG"))
