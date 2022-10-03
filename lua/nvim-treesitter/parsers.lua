179a06bc8b4b028960dc105feceb5a4b1cbcb41d
gbprod <contact@gb-prod.fr>
2022-10-03 09:53:27 +0200

    style: fix code styling according to Stylua

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -1259,7 +1259,7 @@ function M.ft_to_lang(ft)
 end
 
 function M.available_parsers()
-  if vim.fn.executable("tree-sitter") == 1 and vim.fn.executable("node") == 1 then
+  if vim.fn.executable "tree-sitter" == 1 and vim.fn.executable "node" == 1 then
     return vim.tbl_keys(M.list)
   else
     return vim.tbl_filter(function(p)
