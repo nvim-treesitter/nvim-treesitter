8efef81d01fd87b28963d7523039a2ecee46e402
gbprod <contact@gb-prod.fr>
2022-10-06 09:36:29 +0200

    Add regex injections for php
    This commit introduce regex language injection for php preg_* functions.

--- queries/php/injections.scm
+++ queries/php/injections.scm
@@ -1,3 +1,10 @@
 (text) @html
 
 (comment) @phpdoc
+
+;; regex
+
+((function_call_expression
+  function: (_) @_preg_func_identifier
+  arguments: (arguments . (argument (_ (string_value) @regex))))
+    (#lua-match? @_preg_func_identifier "^preg_"))
