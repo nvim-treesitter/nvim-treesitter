82767f3f33c903e92f059dc9a2b27ec38dcc28d7
gbprod <contact@gb-prod.fr>
2022-10-06 16:29:27 +0200

    feat(php): add queries for bash injections
    This commit allows to inject bash syntax into relevant function arguments
    and shell expression.

--- queries/php/injections.scm
+++ queries/php/injections.scm
@@ -1,3 +1,14 @@
 (text) @html
 
 (comment) @phpdoc
+
+;; bash
+
+((function_call_expression
+  function: (_) @_shell_func_identifier
+  arguments: (arguments . (argument (_ (string_value) @bash))))
+  (#any-of? @_shell_func_identifier "shell_exec" "escapeshellarg" 
+   "escapeshellcmd" "exec" "passthru" "proc_open" "shell_exec" "system"))
+
+((expression_statement (shell_command_expression (string_value) @bash)))
+
