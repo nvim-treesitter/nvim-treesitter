b22c9e7e32f0c8d2c1a2a4bfe44047f1f7458e5b
Benny Powers <bennypowers@users.noreply.github.com>
2022-10-13 18:04:04 +0300

    feat(ecma): add innerHTML injection

--- queries/ecma/injections.scm
+++ queries/ecma/injections.scm
@@ -67,3 +67,18 @@
 
 (((template_string) @_template_string
  (#match? @_template_string "^`#graphql")) @graphql)
+
+; el.innerHTML = `<html>`
+(assignment_expression
+  left: (member_expression
+          property: (property_identifier) @_prop
+          (#eq? @_prop "innerHTML"))
+  right: (template_string) @html
+    (#offset! @html 0 1 0 -1))
+; el.innerHTML = '<html>'
+(assignment_expression
+   left: (member_expression
+           property: (property_identifier) @_prop
+           (#eq? @_prop "innerHTML"))
+   right: (string) @html
+            (#offset! @html 0 1 0 -1))
