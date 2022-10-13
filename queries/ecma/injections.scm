2fcde70feea1f61a0b66e07456ab3a6fc3d9524f
Benny Powers <bennypowers@users.noreply.github.com>
2022-10-13 18:08:56 +0300

    feat(ecma): include outerHTML and innerHTML

--- queries/ecma/injections.scm
+++ queries/ecma/injections.scm
@@ -72,13 +72,14 @@
 (assignment_expression
   left: (member_expression
           property: (property_identifier) @_prop
-          (#eq? @_prop "innerHTML"))
+          (#match? @_prop "(out|inn)erHTML"))
   right: (template_string) @html
     (#offset! @html 0 1 0 -1))
+
 ; el.innerHTML = '<html>'
 (assignment_expression
    left: (member_expression
            property: (property_identifier) @_prop
-           (#eq? @_prop "innerHTML"))
+           (#match? @_prop "(out|inn)erHTML"))
    right: (string) @html
             (#offset! @html 0 1 0 -1))
