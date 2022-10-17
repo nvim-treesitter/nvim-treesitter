bece6d8327f4658997ab0cc853ccf5e036e6682c
ObserverOfTime <chronobserver@disroot.org>
2022-10-17 20:09:56 +0300

    highlights(comment): improve note tags

--- queries/comment/highlights.scm
+++ queries/comment/highlights.scm
@@ -7,27 +7,38 @@
 
 ":" @punctuation.delimiter
 
-(tag (name) @text.note (user)? @constant)
+(tag
+  (name) @text.todo
+  (user)? @constant)
+
+((tag ((name) @text.todo))
+ (#eq? @text.todo "TODO"))
+
+("text" @text.todo
+ (#eq? @text.todo "TODO"))
 
 ((tag ((name) @text.note))
- (#any-of? @text.note "NOTE"))
+ (#any-of? @text.note "NOTE" "XXX"))
 
 ("text" @text.note
- (#any-of? @text.note "NOTE"))
+ (#any-of? @text.note "NOTE" "XXX"))
 
 ((tag ((name) @text.warning))
- (#any-of? @text.warning "TODO" "HACK" "WARNING"))
+ (#any-of? @text.warning "HACK" "WARNING"))
 
 ("text" @text.warning
- (#any-of? @text.warning "TODO" "HACK" "WARNING"))
+ (#any-of? @text.warning "HACK" "WARNING"))
 
 ((tag ((name) @text.danger))
- (#any-of? @text.danger "FIXME" "XXX" "BUG"))
+ (#any-of? @text.danger "FIXME" "BUG"))
 
 ("text" @text.danger
- (#any-of? @text.danger "FIXME" "XXX" "BUG"))
+ (#any-of? @text.danger "FIXME" "BUG"))
 
 ; Issue number (#123)
-("text" @number (#lua-match? @number "^#[0-9]+$"))
+("text" @number
+ (#lua-match? @number "^#[0-9]+$"))
+
 ; User mention (@user)
-("text" @constant (#lua-match? @constant "^[@][a-zA-Z0-9_-]+$"))
+("text" @constant
+ (#lua-match? @constant "^[@][a-zA-Z0-9_-]+$"))
