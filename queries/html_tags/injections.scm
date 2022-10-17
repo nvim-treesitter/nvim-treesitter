e8d2ff0b3d307841c1dfb207cecc520d4caa621f
Benny Powers <bennypowers@users.noreply.github.com>
2022-10-17 21:13:17 +0300

    feat(html): lit attr binding interpolation

--- queries/html_tags/injections.scm
+++ queries/html_tags/injections.scm
@@ -4,4 +4,14 @@
 ((script_element
   (raw_text) @javascript))
 
+; lit-html style template interpolation
+((attribute
+  (quoted_attribute_value (attribute_value) @javascript))
+  (#match? @javascript "\\$\\{")
+  (#offset! @javascript 0 2 0 -1))
+((attribute
+  (attribute_value) @javascript)
+  (#match? @javascript "\\$\\{")
+  (#offset! @javascript 0 2 0 -2))
+
 (comment) @comment
