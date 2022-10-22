b9729e42c0ba4bf40c055b497e4d5c4d24e71814
ObserverOfTime <chronobserver@disroot.org>
2022-10-22 11:10:21 +0300

    highlights(cuda): use more specific groups

--- queries/cuda/highlights.scm
+++ queries/cuda/highlights.scm
@@ -3,13 +3,9 @@
 [ "<<<" ">>>" ] @punctuation.bracket
 
 [
-  "__local__"
-  "__shared__"
-  "__global__"
   "__host__"
   "__device__"
   "__forceinline__"
-  "__restrict__"
-  "__launch_bounds__"
-  "__grid_constant__"
-] @keyword
+] @storageclass
+
+"__launch_bounds__" @type.qualifier
