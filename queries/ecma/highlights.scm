67c10f3fe33fd824579eed79536b2aac8274c3a7
ObserverOfTime <chronobserver@disroot.org>
2022-10-22 16:45:59 +0300

    highlights(ecma): fix captures
    Co-Authored-By: Stephan Seitz <stephan.seitz@fau.de>

--- queries/ecma/highlights.scm
+++ queries/ecma/highlights.scm
@@ -186,7 +186,8 @@
 
 (binary_expression "/" @operator)
 (ternary_expression ["?" ":"] @conditional)
-(unary_expression ["!" "~" "-" "+" "delete" "void" "typeof"]  @operator)
+(unary_expression ["!" "~" "-" "+"] @operator)
+(unary_expression ["delete" "void" "typeof"] @keyword.operator)
 
 [
   "("
@@ -245,7 +246,6 @@
 "target"
 "typeof"
 "var"
-"void"
 "with"
 ] @keyword
 
