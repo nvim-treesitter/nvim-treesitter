287ffdccc1dd7ed017d844a4fad069fd3340fa94
gbprod <contact@gb-prod.fr>
2022-10-28 14:22:16 +0200

    Add regex injections for php (#3592)

--- queries/php/injections.scm
+++ queries/php/injections.scm
@@ -2,6 +2,13 @@
 
 (comment) @phpdoc
 
+;; regex
+
+((function_call_expression
+  function: (_) @_preg_func_identifier
+  arguments: (arguments . (argument (_ (string_value) @regex))))
+    (#lua-match? @_preg_func_identifier "^preg_"))
+
 ;; bash
 
 ((function_call_expression
@@ -11,4 +18,3 @@
    "escapeshellcmd" "exec" "passthru" "proc_open" "shell_exec" "system"))
 
 ((expression_statement (shell_command_expression (string_value) @bash)))
-
