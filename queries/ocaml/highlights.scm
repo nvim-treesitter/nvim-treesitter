c723260f049aa21f044204baccfa22599fecf523
ObserverOfTime <chronobserver@disroot.org>
2022-11-15 18:56:25 +0200

    highlights(ocaml): use more specific groups

--- queries/ocaml/highlights.scm
+++ queries/ocaml/highlights.scm
@@ -95,12 +95,17 @@
 ;---------
 
 [
-  "and" "as" "assert" "begin" "class" "constraint"
-  "end" "external" "in"
-  "inherit" "initializer" "lazy" "let" "match" "method" "module"
-  "mutable" "new" "nonrec" "object" "of" "private" "rec" "sig" "struct"
-  "type" "val" "virtual" "when" "with"
+  "and" "as" "assert" "begin" "class"
+  "constraint" "end" "external" "in"
+  "inherit" "initializer" "let" "match"
+  "method" "module" "new" "object" "of"
+  "sig" "struct" "type" "val" "when" "with"
 ] @keyword
+
+[
+  "lazy" "mutable" "nonrec"
+  "rec" "private" "virtual"
+] @type.qualifier
 
 ["fun" "function" "functor"] @keyword.function
 
