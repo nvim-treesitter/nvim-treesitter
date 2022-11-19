f568f12de5c4ddc6f495166c9f32fa676fd6b44d
Benedikt Reinartz <filmor@gmail.com>
2022-11-19 15:31:38 +0100

    Initial attempt at a new highlights file

--- queries/erlang/highlights.scm
+++ queries/erlang/highlights.scm
@@ -3,6 +3,7 @@
   "fun"
   "div"
 ] @keyword
+
 ;; bracket
 [
   "("
@@ -13,6 +14,41 @@
   "]"
   "#"
 ] @punctuation.bracket
+
+;;; operator
+[
+  ":"
+  ":="
+  "!"
+  "-"
+  "+"
+  "="
+  "->"
+  "=>"
+  "|"
+ ] @operator
+
+;;; Comparisons
+[
+  "=="
+  "=:="
+  "=/="
+  "=<"
+  ">="
+  "<"
+  ">"
+] @operator ;; .comparison
+
+[
+  ","
+  "."
+  ";"
+] @punctuation
+
+[
+  "record"
+] @attribute
+
 ;; conditional
 [
   "receive"
@@ -27,79 +63,67 @@
 [
   "catch"
   "try"
-  "throw"
 ] @exception
+((atom) @exception (#any-of? @exception "error" "throw" "exit"))
+
+((atom) @boolean (#any-of? @boolean "true" "false"))
+
+;; Macros
+(macro_call_expr
+  "?" @macro
+  name: (_) @macro
+) @macro
+
+;; Preprocessor
+(pp_define) @define
+[(pp_include) (pp_include_lib)] @include
+(_preprocessor_directive) @preprocessor
+
 ;;; module define
 [
   "module"
   "export"
+  "export_type"
+  "include"
+  "include_lib"
+  "behaviour"
+  "behavior"
 ] @include
-;;; operator
-[
-  ":"
-  ":="
-  "?"
-  "!"
-  "-"
-  "+"
-  "="
-  "->"
-  "=>"
-  "|"
-  ;;;TODO
-  "$"
- ] @operator
+
+;; Records
+(record_field_expr _ @structure) @structure
+(record_field_name _ @structure) @structure
+(record_name name: _ @structure) @structure
+(record_decl name: _ @structure)
+(record_field name: _ @structure ty: _ @type)
+
+;; Type alias
+(type_alias "type" @type.definition)
+(spec "spec" @type.definition)
 
 (comment) @comment
-(string) @string
-(var) @variable
+[(string) (binary)] @string
 
 (module_attribute
   (atom) @namespace
 )
 ;;; expr_function_call
-(call
-  expr: [(atom) (remote) (var)] @function.call 
-)
+(call expr: [(atom) (remote) (var)] @function)
 
 ;;; Parenthesized expression: (SomeFunc)(), only highlight the parens
 (call
-  expr: (paren_expr ("(") @function.call (_) (")") @function.call)
-)
-
-;;; map
-(map_expr
- (map_field [
-   (atom)
-   (var)
- ] @variable)
-)
-
-(map_expr_update
- (map_field [
-   (atom)
-   (variable)
- ] @variable)
-)
-
-(tuple (atom) @variable)
-(pat_tuple ( pattern (atom) @variable))
-
-;;; case
-(cr_clause
-  pat: (atom) @variable
-)
-(cr_clause
-  body: (atom) @variable
+  expr: (paren_expr "(" @function ")" @function)
 )
 
 ;;; function
-(external_fun
-  module: (atom) @attribute
-)
-(external_fun
-  fun: (atom) @function
-)
-(internal_fun
-  fun: (atom) @function
-)
+(external_fun) @function
+(internal_fun fun: (atom) @function)
+(function_clause name: (atom) @function)
+
+;;; attributes
+(attr_name name: (atom) @keyword)
+
+(var) @variable
+(atom) @constant
+(integer) @number
+(float) @float
