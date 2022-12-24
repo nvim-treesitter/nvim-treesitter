9a533425ec6daf88849511d8c4e7e7eb8ea74b3e
Lucario387 <hoangtun0810@gmail.com>
2022-12-24 16:30:19 +0900

    add test file

---
+++ tests/query/injections/vue/test-vue-injections.vue
@@ -0,0 +1,41 @@
+<template>
+  <span>{{"Some text"}}</span>
+  <!--      ^ javascript 
+-->
+
+  <template lang="pug">
+    ul
+      li(v-for="item in items")
+        a(v-if="item.type == 'link'" :href="item.url") some link title: 
+<!--                                                    ^ pug
+-->
+  </template>
+
+  <template v-if="true"></template>
+<!--              ^ javascript 
+-->
+</template>
+<script>
+const foo = "1"
+//    ^ javascript
+</script>
+<script defer lang="js">
+const foo = "1"
+//    ^ javascript
+</script>
+<script lang="ts">
+const foo = "1"
+//    ^ typescript
+</script>
+<style>
+.bar {
+/* ^ css*/
+}
+</style>
+<style lang="scss">
+.bar {
+  &-baz {
+//  ^ scss
+  }
+}
+</style>
