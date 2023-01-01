8b0b57235a6f6d646dc44fd4acdc513c8a07b480
Stephan Seitz <stephan.seitz@fau.de>
2023-01-01 15:16:04 +0100

    highlights(rust): use `@namespace` for `[(crate) (self) (super)]`
    Fixes https://github.com/nvim-treesitter/nvim-treesitter/issues/3992

--- queries/rust/highlights.scm
+++ queries/rust/highlights.scm
@@ -209,10 +209,10 @@
 (type_cast_expression "as" @keyword.operator)
 (qualified_type "as" @keyword.operator)
 
-(use_list (self) @keyword)
-(scoped_use_list (self) @keyword)
-(scoped_identifier [(crate) (super) (self)] @keyword)
-(visibility_modifier [(crate) (super) (self)] @keyword)
+(use_list (self) @namespace)
+(scoped_use_list (self) @namespace)
+(scoped_identifier [(crate) (super) (self)] @namespace)
+(visibility_modifier [(crate) (super) (self)] @namespace)
 
 [
   "else"
