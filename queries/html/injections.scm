a2df00313d0433970d58dfd867a40441e0eaed29
Lucario387 <hoangtun0810@gmail.com>
2023-01-02 02:38:54 +0900

    refactor(vue, html): Update injections Update html_tags injections so that html/vue/svelte parsers can use it without having a script/style tag being captured twice
    Signed-off-by: Pham Huy Hoang <hoangtun0810@gmail.com>

--- queries/html/injections.scm
+++ queries/html/injections.scm
@@ -1,12 +1,1 @@
-((style_element
-  (raw_text) @css))
-
-((attribute
-   (attribute_name) @_attr
-   (quoted_attribute_value (attribute_value) @css))
- (#eq? @_attr "style"))
-
-((script_element
-  (raw_text) @javascript))
-
-(comment) @comment
+; inherits html_tags
