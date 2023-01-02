28baed769815c54b243f0df606ccb4114287e772
Lucario387 <hoangtun0810@gmail.com>
2023-01-02 10:47:46 +0900

    refactor(svelte): Remove duplicated queries These injection queries are defined already in html_tags, remove them
    Signed-off-by: Pham Huy Hoang <hoangtun0810@gmail.com>

---
+++ tests/query/injections/svelte/test-svelte-injections.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Button from "./Button.svelte";
+//              ^ javascript
+</script>
+<script lang="ts">
+  const foo: number = 1
+//            ^ typescript
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+/*           ^ css
+*/
+  }
+</style>
+<style lang="scss">
+  main {
+    font-family: sans-serif;
+    text-align: center;
+    &:hover {
+//  ^ scss
+    }
+  }
+</style>
+
+<main>
+	<h1>Test file</h1>
+  {#each someItems as someItem}
+<!--          ^ javascript
+-->
+    <div>{someItem}</div>
+<!--         ^ javascript 
+-->
+  {/each}
+	<Button />
+  <button on:click={() => foo++}></button>
+</main>
