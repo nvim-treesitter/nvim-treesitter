9a6988c7f82ee3855f2f788766f1482dc42e66e3
figsoda <figsoda@pm.me>
2023-01-05 17:11:53 -0500

    injections(nix): remove `@combined`

--- queries/nix/injections.scm
+++ queries/nix/injections.scm
@@ -16,7 +16,6 @@
     (indented_string_expression (string_fragment) @bash)
   ]
   (#match? @_path "(^\\w+(Phase|Hook)|(pre|post)[A-Z]\\w+|script)$"))
-  @combined
 
 (apply_expression
   function: (_) @_func
