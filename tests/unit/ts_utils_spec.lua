eaf6e9659c7c27722379bd92e796bde5cad74d3c
Michael Lan <michaellan202@gmail.com>
2023-01-06 13:00:13 -0800

    test(swap_nodes): check cursor
    * add a regression test (for multiline node swap)
    * add a test with adjacent swaps that fails when char_delta is not
      calculated correctly

--- tests/unit/ts_utils_spec.lua
+++ tests/unit/ts_utils_spec.lua
@@ -106,3 +106,35 @@ describe("update_selection", function()
     )
   end)
 end)
+
+describe("swap_nodes", function()
+  local function swap_and_return_cursor(case)
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, case.lines)
+    local a = vim.treesitter.get_node_at_position(0, case.a[1], case.a[2], {})
+    local b = vim.treesitter.get_node_at_position(0, case.b[1], case.b[2], {})
+    tsutils.swap_nodes(a, b, 0, true)
+    return vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win())
+  end
+
+  it("moves cursor correctly with adjacent nodes", function()
+    assert.equal(
+      swap_and_return_cursor {
+        lines = { "print(1)" },
+        a = { 0, 0 },
+        b = { 0, 5 },
+      },
+      { 1, 3 }
+    )
+  end)
+
+  it("moves cursor correctly with multiline nodes", function()
+    assert.equal(
+      swap_and_return_cursor {
+        lines = { "x = { [[", "]], [[", ".....]]}" },
+        a = { 0, 6 },
+        b = { 1, 4 },
+      },
+      { 2, 9 }
+    )
+  end)
+end)
