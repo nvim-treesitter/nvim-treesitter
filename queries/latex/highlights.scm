034556938fb875eda8571591642ca72982b0d47b
Jia Jia <35106942+mathjiajia@users.noreply.github.com>
2023-01-08 10:29:57 +0800

    fix(latex): highlights for math_environment

--- queries/latex/highlights.scm
+++ queries/latex/highlights.scm
@@ -112,16 +112,16 @@
 
 (math_environment
   (begin
-   command: _ @text.math
-   name: (curly_group_text (text) @text.math)))
+   command: _ @text.environment
+   name: (curly_group_text (text) @text.environment.name)))
 
 (math_environment
   (text) @text.math)
 
 (math_environment
   (end
-   command: _ @text.math
-   name: (curly_group_text (text) @text.math)))
+   command: _ @text.environment
+   name: (curly_group_text (text) @text.environment.name)))
 
 ;; Sectioning
 (title_declaration
