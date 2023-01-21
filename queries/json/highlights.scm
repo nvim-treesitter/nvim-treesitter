da6dc214ddde3fac867bd4a6f4ea51a794b01e18
ObserverOfTime <chronobserver@disroot.org>
2023-01-21 23:10:32 +0200

    highlights(json): conceal escaped quote

--- queries/json/highlights.scm
+++ queries/json/highlights.scm
@@ -1,17 +1,33 @@
-(true) @boolean
-(false) @boolean
-(null) @constant.builtin
-(number) @number
-(pair key: (string) @label)
-(pair value: (string) @string)
-(array (string) @string)
-(string_content (escape_sequence) @string.escape)
-(string_content) @spell
-(ERROR) @error
-["," ":"] @punctuation.delimiter
-"[" @punctuation.bracket
-"]" @punctuation.bracket
-"{" @punctuation.bracket
-"}" @punctuation.bracket
+[
+ (true)
+ (false)
+] @boolean
 
-(("\"" @conceal) (#set! conceal ""))
+(null) @constant.builtin
+
+(number) @number
+
+(pair
+  key: (string) @label
+  value: (string) @string)
+
+(array (string) @string)
+
+(string_content) @spell
+
+(ERROR) @error
+
+["," ":"] @punctuation.delimiter
+
+[
+ "[" "]"
+ "{" "}"
+] @punctuation.bracket
+
+(("\"" @conceal)
+ (#set! conceal ""))
+
+(escape_sequence) @string.escape
+((escape_sequence) @conceal
+ (#eq? @conceal "\\\"")
+ (#set! conceal "\""))
