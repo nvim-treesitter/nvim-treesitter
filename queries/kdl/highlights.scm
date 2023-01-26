862001cbc873a0276a54ce0c4ec7850d3bdda704
Amaan Qureshi <amaanq12@gmail.com>
2023-01-26 13:31:14 -0500

    feat: comment queries

--- queries/kdl/highlights.scm
+++ queries/kdl/highlights.scm
@@ -27,8 +27,7 @@
 
 (escape) @string.escape
 
-((number) @number
- (#is-not? float))
+(number) @number
 
 (number (decimal) @float)
 (number (exponent) @float)
@@ -50,3 +49,8 @@
   (single_line_comment)
   (multi_line_comment)
 ] @comment
+
+(node (node_slash_dash)) @comment
+(node (node_prop_or_arg_slash_dash) @comment (prop) @comment)
+(node (node_prop_or_arg_slash_dash) @comment (value) @comment)
+(node_children (node_children_slash_dash)) @comment
