9bffdeafd13d44d11204b24c0c4800b143f46679
Vir Chaudhury <virchau13@hexular.net>
2023-02-09 21:14:02 +0800

    astro: add injections for scss

--- queries/astro/injections.scm
+++ queries/astro/injections.scm
@@ -8,3 +8,12 @@
 
 ((script_element
     (raw_text) @typescript))
+
+((style_element
+   (start_tag
+     (attribute
+       (attribute_name) @_lang_attr
+       (quoted_attribute_value (attribute_value) @_lang_value)))
+   (raw_text) @scss)
+ (#eq? @_lang_attr "lang")
+ (#eq? @_lang_value "scss"))
