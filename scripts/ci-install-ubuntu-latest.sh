d1c09f0d7a197d7b6dc7b36a124faa9e80a70f33
Kiyoon Kim <yoonkr33@gmail.com>
2023-02-14 17:16:33 +0000

    Add treesitter CLI in CI with Windows and mac

--- scripts/ci-install-ubuntu-latest.sh
+++ scripts/ci-install-ubuntu-latest.sh
@@ -1,6 +1,3 @@
-wget -O - https://github.com/tree-sitter/tree-sitter/releases/download/${TREE_SITTER_CLI_TAG}/tree-sitter-linux-x64.gz | gunzip -c > tree-sitter
-sudo cp ./tree-sitter /usr/bin/tree-sitter
-sudo chmod uog+rwx /usr/bin/tree-sitter
 wget https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-linux64.tar.gz
 tar -zxf nvim-linux64.tar.gz
 sudo ln -s $(pwd)/nvim-linux64/bin/nvim /usr/local/bin
