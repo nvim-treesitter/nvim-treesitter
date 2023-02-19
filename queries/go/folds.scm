ce0dba96f47cd8bbd46b4c3ac8fd1b9502f1002a
Laytan Laats <laytanlaats@hotmail.com>
2023-02-19 01:07:36 +0100

    feat(go): add folds for type switch and switch cases

--- queries/go/folds.scm
+++ queries/go/folds.scm
@@ -1,6 +1,10 @@
 [
  (const_declaration)
  (expression_switch_statement)
+ (expression_case)
+ (default_case)
+ (type_switch_statement)
+ (type_case)
  (for_statement)
  (func_literal)
  (function_declaration)
