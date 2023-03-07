6ce7cd1345625f2f83f97463d1404cb77a95b4da
Daniel Woznicki <danwoz@nettoolkit.com>
2023-03-06 17:10:55 -0800

    Added a helper call to activate plenary when running tests with a vim-plug setup

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -12,3 +12,16 @@ require("nvim-treesitter.configs").setup {
   indent = { enable = true },
   highlight = { enable = true },
 }
+
+-- Load the plenary.nvim module with vim-plug if present.
+-- Note that since vim-plug is an autoload module, which means we need to call
+-- `runtime autoload/plug.vim` load load it before checking if the `plug#begin` function exists.
+-- See: https://vi.stackexchange.com/questions/38490/how-to-check-if-an-autoload-function-exists-without-calling-it
+vim.cmd [[
+runtime autoload/plug.vim
+if exists('*plug#begin')
+  call plug#begin()
+  Plug 'nvim-lua/plenary.nvim'
+  call plug#end()
+endif
+]]
