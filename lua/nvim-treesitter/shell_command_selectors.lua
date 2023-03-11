b7383cc8b38a298d63ef1894ee32209cdd03aa8e
Adam Wolski <adam@wolski.email>
2023-03-11 10:42:17 +0100

    fix(windows): fix invalid powershell separator during install

--- lua/nvim-treesitter/shell_command_selectors.lua
+++ lua/nvim-treesitter/shell_command_selectors.lua
@@ -301,7 +301,11 @@ end
 ---@return string command
 function M.make_directory_change_for_command(dir, command)
   if fn.has "win32" == 1 then
-    return string.format("pushd %s & %s & popd", cmdpath(dir), command)
+    if vim.o.shell == "powershell" or vim.o.shell == "pwsh" then
+      return string.format("pushd %s ; %s ; popd", cmdpath(dir), command)
+    else
+      return string.format("pushd %s & %s & popd", cmdpath(dir), command)
+    end
   else
     return string.format("cd %s;\n %s", dir, command)
   end
