01e8a5145160620140c3c57d285ccb2cfe067540
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-03-16 19:04:17 +0900

    feat(python): add "return" patterns to @indent_end
    `return ...` is usually the last line of a
    function/statement, so mark some possible patterns of `return` with
    `@indent_end` to dedent the line after it

--- queries/python/indents.scm
+++ queries/python/indents.scm
@@ -72,6 +72,15 @@
 (list_pattern "]" @indent_end)
 
 
+(return_statement
+  [
+    (_) @indent_end 
+    (_ (_) @indent_end .)
+    (attribute 
+      attribute: (_) @indent_end)
+    "return" @indent_end
+  ] .)
+
 [
   ")"
   "]"
---
+++ tests/indent/python/return_dedent.py
@@ -0,0 +1,16 @@
+def a():
+    return
+
+def a():
+    return True
+
+def a():
+    return (1, 2, 3)
+
+def a():
+    return x.y.z
+
+def a():
+    return (
+        1, 2, 3
+    )
