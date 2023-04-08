a4cf1d025c000e4967e7ebbe0edb5fa7786ef8a4
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-04-09 01:18:44 +0900

    refactor(html_inject): Use lua-match instead of match

--- queries/html_tags/injections.scm
+++ queries/html_tags/injections.scm
@@ -5,7 +5,8 @@
 (
   (style_element
     (start_tag) @_no_type_lang
-      (#not-match? @_no_type_lang "\\s(lang|type)\\s*\\=")
+      (#not-lua-match? @_no_type_lang "%slang%s*=")
+      (#not-lua-match? @_no_type_lang "%stype%s*=")
     (raw_text) @css))
 
 (
