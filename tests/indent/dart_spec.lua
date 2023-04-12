3dcd2f0905822bf5bbf05175716b1bb22a8e18d4
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-04-13 01:42:53 +0900

    fix(dart_indent): Fix dart case/default indent
    add tests
    
    add tests for fallthrough case
    
    add more tests
    
    keep functional tests

--- tests/indent/dart_spec.lua
+++ tests/indent/dart_spec.lua
@@ -1,9 +1,9 @@
 local Runner = require("tests.indent.common").Runner
 
 local run = Runner:new(it, "tests/indent/dart", {
-  tabstop = 4,
+  tabstop = 2,
   shiftwidth = 2,
-  softtabstop = 0,
+  softtabstop = 2,
   expandtab = true,
 })
 
@@ -18,6 +18,13 @@ end)
 describe("new line:", function()
   run:new_line("class.dart", { on_line = 2, text = "var x;", indent = 0 })
   run:new_line("try.dart", { on_line = 2, text = "var x;", indent = 4 })
+  run:new_line("switch.dart", { on_line = 3, text = "x = 1;", indent = 6 })
+  run:new_line("switch.dart", { on_line = 9, text = "x = 1;", indent = 6 })
+  run:new_line("switch.dart", { on_line = 3, text = "case 2:", indent = 4 })
+  run:new_line("switch.dart", { on_line = 16, text = "abc;", indent = 4 })
+  run:new_line("switch.dart", { on_line = 20, text = "abc;", indent = 4 })
+  run:new_line("switch.dart", { on_line = 28, text = "y++;", indent = 6 })
+
   run:new_line("multiple_arguments.dart", { on_line = 10, text = "var x;", indent = 4 })
   run:new_line("multiple_arguments.dart", { on_line = 11, text = "var x;", indent = 4 })
 end)
