36d4deb294fb0cd4385d58d8c9f603975bf355ad
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-04-13 01:42:53 +0900

    fix(dart_indent): Fix dart case/default indent
    add tests
    
    add tests for fallthrough case
    
    add more tests
    
    keep functional tests

--- queries/dart/indents.scm
+++ queries/dart/indents.scm
@@ -13,6 +13,13 @@
   (try_statement)
 ] @indent.begin
 
+(switch_block
+  (_) @indent.begin
+  (#set! indent.immediate 1)
+  (#set! indent.start_at_same_line 1))
+
+(switch_label) @indent.branch
+
 [
   "("
   ")"
@@ -25,6 +32,9 @@
 [
  "}"
 ] @indent.end
+
+(return_statement ";" @indent.end)
+(break_statement ";" @indent.end)
 
 ; this one is for dedenting the else block
 (if_statement (block) @indent.branch)
