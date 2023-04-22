51e547c84473b8f84b42a4b447d5da69d87d579d
ObserverOfTime <chronobserver@disroot.org>
2023-04-22 23:29:36 +0300

    python: inject rst in docstrings

--- queries/python/injections.scm
+++ queries/python/injections.scm
@@ -7,5 +7,14 @@
   (#lua-match? @_string "^r.*")
   (#set! injection.language "regex"))
 
+((expression_statement
+   (string (string_content) @injection.content))
+ (#set! injection.language "rst"))
+
+((comment) @injection.content
+ (#lua-match? @injection.content "^#: ")
+ (#offset! @injection.content 0 3 0 0)
+ (#set! injection.language "rst"))
+
 ((comment) @injection.content
  (#set! injection.language "comment"))
