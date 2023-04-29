1ca9b938c1c3d8b2c64a168f6a5419b8f3870410
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-04-29 17:35:35 +0900

    Add Python else elif dedent (#4618)
    * feat(python): add interactive else/elif dedent
    
    * fixup
    
    * add test

--- queries/python/indents.scm
+++ queries/python/indents.scm
@@ -83,6 +83,14 @@
   (continue_statement)
 ] @indent.dedent
 
+(ERROR
+  (_) @indent.branch ":" .
+  (#lua-match? @indent.branch "^else"))
+
+(ERROR
+  (_) @indent.branch @indent.dedent ":" .
+  (#lua-match? @indent.branch "^elif"))
+
 (parenthesized_expression ")" @indent.end)
 (generator_expression ")" @indent.end)
 (list_comprehension "]" @indent.end)
