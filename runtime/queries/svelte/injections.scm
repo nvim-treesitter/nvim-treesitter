80a27299ee8e7395a76da121b772e62bebdc5e92
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-05-03 19:07:59 +0900

    chore(injections)!: update injection syntax to 0.9
    Since 0.9, @lang syntax is still available as fallback but will soon be deprecated.
    Because of that, new syntax should be adopted once 0.9 becomes the
    baseline requirements for nvim-treesitter
    
    - update health check
    - update doc

--- runtime/queries/svelte/injections.scm
+++ runtime/queries/svelte/injections.scm
@@ -1,40 +1,41 @@
 ; inherits: html_tags
 
-(
-  (style_element
+((style_element
     (start_tag
       (attribute
         (attribute_name) @_attr
         (quoted_attribute_value (attribute_value) @_lang)))
-    (raw_text) @scss)
+    (raw_text) @injection.content)
   (#eq? @_attr "lang")
   (#any-of? @_lang "scss" "postcss" "less")
-)
+  (#set! injection.language "scss")
+  (#set! injection.include-children))
 
-[
+([
   (raw_text_expr)
   (raw_text_each)
-] @javascript
+] @injection.content
+ (#set! injection.language "javascript")
+ (#set! injection.include-children))
 
-(
-  (script_element
+((script_element
     (start_tag
       (attribute
         (attribute_name) @_attr
         (quoted_attribute_value (attribute_value) @_lang)))
-    (raw_text) @typescript)
-  (#eq? @_attr "lang") 
-  (#any-of? @_lang "ts" "typescript")
-)
+    (raw_text) @injection.content)
+ (#eq? @_attr "lang") 
+ (#any-of? @_lang "ts" "typescript")
+ (#set! injection.language "typescript")
+ (#set! injection.include-children))
 
-(
-  (element
-    (start_tag
-      (attribute
-        (attribute_name) @_attr
-        (quoted_attribute_value
-          (attribute_value) @_lang)))
-    (text) @pug)
-  (#eq? @_attr "lang") 
-  (#eq? @_lang "pug")
-)
+((element
+   (start_tag
+     (attribute
+       (attribute_name) @_attr
+       (quoted_attribute_value
+         (attribute_value) @injection.language)))
+   (text) @injection.content)
+ (#eq? @_attr "lang") 
+ (#eq? @injection.language "pug")
+ (#set! injection.include-children))
