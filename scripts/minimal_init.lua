040dd69c6f3ec448d27b1800d11e49cd86559046
Christian Clason <c.clason@uni-graz.at>
2023-05-07 11:44:51 +0200

    feat!: remove module framework, general cleanup/refactor
    work towards https://github.com/nvim-treesitter/nvim-treesitter/issues/4767
    
    - remove module framework
    - remove extra utilities
    - refactor `indent.lua` into standalone
    - refactor commands, predicates, filetypes registration to plugin/
    - support installing tiers of parsers
    - install parsers to standard directory by default
    - remove bundled queries from runtimepath; copy on parser install
    - general refactor and cleanup
    - remove locals from highlighting

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -1,20 +1,24 @@
-vim.opt.runtimepath:append "."
-vim.cmd.runtime { "plugin/plenary.vim", bang = true }
-vim.cmd.runtime { "plugin/nvim-treesitter.lua", bang = true }
+vim.opt.runtimepath:append('.')
+vim.cmd.runtime({ 'plugin/plenary.vim', bang = true })
+vim.cmd.runtime({ 'plugin/nvim-treesitter.lua', bang = true })
+vim.cmd.runtime({ 'plugin/query_predicates.lua', bang = true })
 
-vim.filetype.add {
+vim.filetype.add({
   extension = {
-    conf = "hocon",
-    ncl = "nickel",
-    usd = "usd",
-    usda = "usd",
+    conf = 'hocon',
+    ncl = 'nickel',
+    usd = 'usd',
+    usda = 'usd',
   },
-}
+})
 
 vim.o.swapfile = false
 vim.bo.swapfile = false
 
-require("nvim-treesitter.configs").setup {
-  indent = { enable = true },
-  highlight = { enable = true },
-}
+require('nvim-treesitter').setup()
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    pcall(vim.treesitter.start)
+    vim.bo[args.buffer].indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
+  end,
+})
