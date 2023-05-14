1a82ce0b4a344d77ea3e8eb147a1ee4d7189afe9
Christian Clason <c.clason@uni-graz.at>
2023-05-14 13:30:15 +0200

    feat: remove obsolete query cache autocommand

--- plugin/nvim-treesitter.lua
+++ plugin/nvim-treesitter.lua
@@ -5,29 +5,6 @@ vim.g.loaded_nvim_treesitter = true
 
 local api = vim.api
 
--- define autocommands
-local augroup = api.nvim_create_augroup('nvim-treesitter', {})
-
-api.nvim_create_autocmd('Filetype', {
-  pattern = 'query',
-  group = augroup,
-  callback = function()
-    api.nvim_clear_autocmds({
-      group = augroup,
-      event = 'BufWritePost',
-    })
-    api.nvim_create_autocmd('BufWritePost', {
-      group = augroup,
-      buffer = 0,
-      callback = function(opts)
-        require('nvim-treesitter.query').invalidate_query_file(opts.file)
-      end,
-      desc = 'Invalidate query file',
-    })
-  end,
-  desc = 'Reload query',
-})
-
 -- create user commands
 api.nvim_create_user_command('TSInstallInfo', function()
   require('nvim-treesitter.install').info()
