a90007551c28d63e63aeed1a0a29a0bdf4801486
Alex Manning <alex41290@gmail.com>
2023-05-30 23:08:18 -0400

    small gleam indent fixes

--- queries/gleam/indents.scm
+++ queries/gleam/indents.scm
@@ -1,5 +1,6 @@
 ; Gleam indents similar to Rust and JavaScript
 [
+  (anonymous_function)
   (assert)
   (case)
   (case_clause)
@@ -11,7 +12,6 @@
   (let)
   (list)
   (constant)
-  (external_function)
   (function)
   (type_definition)
   (type_alias)
