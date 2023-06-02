c2daa02537f0d63ca93be646ea438859d19ce0eb
lnay <luke@grahamnaylor.net>
2023-06-02 11:37:24 +0100

    Add extra language injections for latex (#4896)
    Add python injections to contents of `sagetex` defined environments, similarly to how `pycode` currently treated.

--- runtime/queries/latex/injections.scm
+++ runtime/queries/latex/injections.scm
@@ -10,6 +10,16 @@
   (source_code) @injection.content 
   (#set! injection.language "python"))
 
+(sagesilent_environment
+  code: 
+  (source_code) @injection.content 
+  (#set! injection.language "python"))
+
+(sageblock_environment
+  code: 
+  (source_code) @injection.content 
+  (#set! injection.language "python"))
+
 (minted_environment
   (begin
     language: (curly_group_text
