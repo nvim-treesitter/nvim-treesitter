ce14fc8650b9f4f3e51e06eec63970cf10475cdb
Lewis Russell <lewis6991@gmail.com>
2023-06-07 22:30:26 +0100

    refactor: use vim.system (#4923)

--- scripts/update-lockfile.lua
+++ scripts/update-lockfile.lua
@@ -5,14 +5,14 @@ local util = require('nvim-treesitter.util')
 
 -- Load previous lockfile
 local filename = require('nvim-treesitter.install').get_package_path('lockfile.json')
-local lockfile = vim.json.decode(util.read_file(filename))
+local lockfile = vim.json.decode(util.read_file(filename)) --[[@as table<string,{revision:string}>]]
 
 ---@type string?
 local skip_lang_string = os.getenv('LOCKFILE_SKIP')
 local skip_langs = skip_lang_string and vim.split(skip_lang_string, ',') or {}
 vim.print('Skipping languages: ', skip_langs)
 
-local sorted_parsers = {}
+local sorted_parsers = {} --- @type {name:string,parser:ParserInfo}[]
 for k, v in pairs(require('nvim-treesitter.parsers').configs) do
   table.insert(sorted_parsers, { name = k, parser = v })
 end
@@ -20,23 +20,53 @@ table.sort(sorted_parsers, function(a, b)
   return a.name < b.name
 end)
 
+local jobs = {} --- @type table<{name:string,parser:ParserInfo},SystemObj>
+
 -- check for new revisions
-for _, v in ipairs(sorted_parsers) do
-  if not vim.list_contains(skip_langs, v.name) and v.parser.install_info then
-    local cmd = 'git ls-remote ' .. v.parser.install_info.url
-    if v.parser.install_info.branch then
-      cmd = cmd .. ' | grep refs/heads/' .. v.parser.install_info.branch
+for i, v in ipairs(sorted_parsers) do
+  -- keep spawning jobs until we hit EMFILE
+  local ok, err = pcall(function()
+    if vim.list_contains(skip_langs, v.name) or not v.parser.install_info then
+      print('Skipping ' .. v.name)
+      return
     end
-    local sha = vim.split(vim.fn.systemlist(cmd)[1], '\t')[1]
-    lockfile[v.name] = { revision = sha }
-    print(v.name .. ': ' .. sha)
-  else
-    print('Skipping ' .. v.name)
+
+    jobs[v] = vim.system({ 'git', 'ls-remote', v.parser.install_info.url })
+  end)
+
+  if not ok or i == #sorted_parsers then
+    if err and not err:match('EMFILE') then
+      error(err)
+    end
+
+    for vi, job in pairs(jobs) do
+      local stdout = vim.split(job:wait().stdout, '\n')
+      jobs[vi] = nil
+
+      local branch = vi.parser.install_info.branch
+
+      local line = 1
+      if branch then
+        for j, l in ipairs(stdout) do
+          if l:find(vim.pesc(branch)) then
+            line = j
+            break
+          end
+        end
+      end
+
+      local sha = vim.split(stdout[line], '\t')[1]
+      lockfile[vi.name] = { revision = sha }
+      print(vi.name .. ': ' .. sha)
+    end
   end
 end
 
-lockfile = vim.json.encode(lockfile)
+assert(#vim.tbl_keys(jobs) == 0)
+
+local lockfile_json = vim.json.encode(lockfile) --[[@as string]]
 if vim.fn.executable('jq') == 1 then
-  lockfile = vim.fn.system('jq --sort-keys', lockfile)
+  lockfile_json =
+    assert(vim.system({ 'jq', '--sort-keys' }, { stdin = lockfile_json }):wait().stdout)
 end
-util.write_file(filename, lockfile)
+util.write_file(filename, lockfile_json)
