20c077f3746a8644291dc7b746ccb08c673872ef
Christian Clason <c.clason@uni-graz.at>
2023-06-12 09:54:30 -0600

    feat!: drop modules, general refactor and cleanup

---
+++ runtime/queries/cpp/injections.scm
@@ -0,0 +1,9 @@
+; inherits: c
+
+((comment) @injection.content
+  (#lua-match? @injection.content "/[*\/][!*\/]<?[^a-zA-Z]")
+  (#set! injection.language "doxygen"))
+
+(raw_string_literal
+  delimiter: (raw_string_delimiter) @injection.language
+  (raw_string_content) @injection.content)
