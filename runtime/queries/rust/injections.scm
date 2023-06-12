81554c6fb12c52988bd9a08494e02138555989d6
Christian Clason <c.clason@uni-graz.at>
2023-06-12 09:54:30 -0600

    feat!: drop modules, general refactor and cleanup

--- runtime/queries/rust/injections.scm
+++ runtime/queries/rust/injections.scm
@@ -11,6 +11,7 @@
 (macro_definition
   (macro_rule
     right: (token_tree) @injection.content
+    (#set! injection.combined)
     (#set! injection.language "rust")))
 
 ([
@@ -22,6 +23,8 @@
 ((macro_invocation
   macro: (identifier) @injection.language
   (token_tree) @injection.content)
+  (#set! injection.combined)
+  (#set! injection.include-children)
   (#any-of? @injection.language "html" "json"))
 
 (call_expression
