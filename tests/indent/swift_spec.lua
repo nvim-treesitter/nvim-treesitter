889ef63eb6be013f8640baa3d7bdb73da8ab8ab4
Christian Clason <c.clason@uni-graz.at>
2023-06-12 09:54:30 -0600

    feat!: drop modules, general refactor and cleanup

--- tests/indent/swift_spec.lua
+++ tests/indent/swift_spec.lua
@@ -1,22 +1,37 @@
-local Runner = require("tests.indent.common").Runner
+local Runner = require('tests.indent.common').Runner
 
-local run = Runner:new(it, "tests/indent/swift", {
+local run = Runner:new(it, 'tests/indent/swift', {
   tabstop = 2,
   shiftwidth = 2,
   softtabstop = 2,
   expandtab = true,
 })
 
-describe("indent Swift:", function()
-  describe("whole file:", function()
-    run:whole_file(".", {})
+describe('indent Swift:', function()
+  describe('whole file:', function()
+    run:whole_file('.', {})
   end)
-  describe("new line:", function()
-    run:new_line("declarations.swift", { on_line = 6, text = "var x = 1", indent = 2 })
-    run:new_line("declarations.swift", { on_line = 12, text = "var textInsideInit = true", indent = 4 })
-    run:new_line("declarations.swift", { on_line = 19, text = "var textInsideWillSet = 1", indent = 6 })
-    run:new_line("declarations.swift", { on_line = 22, text = "var textInsideDidSet = 1", indent = 6 })
-    run:new_line("declarations.swift", { on_line = 27, text = "var textInsideOverrideFunc", indent = 4 })
-    run:new_line("declarations.swift", { on_line = 33, text = "var InsideProtocol: String { get }", indent = 2 })
+  describe('new line:', function()
+    run:new_line('declarations.swift', { on_line = 6, text = 'var x = 1', indent = 2 })
+    run:new_line(
+      'declarations.swift',
+      { on_line = 12, text = 'var textInsideInit = true', indent = 4 }
+    )
+    run:new_line(
+      'declarations.swift',
+      { on_line = 19, text = 'var textInsideWillSet = 1', indent = 6 }
+    )
+    run:new_line(
+      'declarations.swift',
+      { on_line = 22, text = 'var textInsideDidSet = 1', indent = 6 }
+    )
+    run:new_line(
+      'declarations.swift',
+      { on_line = 27, text = 'var textInsideOverrideFunc', indent = 4 }
+    )
+    run:new_line(
+      'declarations.swift',
+      { on_line = 33, text = 'var InsideProtocol: String { get }', indent = 2 }
+    )
   end)
 end)
