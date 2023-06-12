e7afa5d14e7708bf853a1ed6acc2843f8da2c716
Christian Clason <c.clason@uni-graz.at>
2023-06-12 09:54:30 -0600

    feat!: drop modules, general refactor and cleanup

--- scripts/update-lockfile.sh
+++ scripts/update-lockfile.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 make_ignored() {
-  if [[ -n $1 ]]; then
+  if [ -n "$1" ]
+  then
     while read -r lang; do
-      if [[ $lang != "$1" ]]; then
-        printf '%s,' "$lang"
+      if [ "$lang" != "$1" ]
+      then
+        printf "%s," "$lang"
       fi
-    done < <(jq -r 'keys[]' lockfile.json)
+    done < <(jq 'keys|@sh' -c lockfile.json)
   fi
 }
 
-SKIP_LOCKFILE_UPDATE_FOR_LANGS="$(make_ignored "$1")" \
-  nvim --headless -c 'luafile ./scripts/write-lockfile.lua' +q
+TO_IGNORE=$(make_ignored $1)
 
+SKIP_LOCKFILE_UPDATE_FOR_LANGS="$TO_IGNORE" nvim -l ./scripts/write-lockfile.lua
 # Pretty print
 cp lockfile.json /tmp/lockfile.json
-jq --sort-keys > lockfile.json < /tmp/lockfile.json
+cat /tmp/lockfile.json | jq --sort-keys > lockfile.json
--- tests/indent/wgsl_spec.lua
+++ tests/indent/wgsl_spec.lua
@@ -1,43 +1,43 @@
-local Runner = require("tests.indent.common").Runner
+local Runner = require('tests.indent.common').Runner
 --local XFAIL = require("tests.indent.common").XFAIL
 
-local run = Runner:new(it, "tests/indent/wgsl", {
+local run = Runner:new(it, 'tests/indent/wgsl', {
   tabstop = 2,
   shiftwidth = 2,
   softtabstop = 0,
   expandtab = true,
 })
 
-describe("indent WGSL:", function()
-  describe("whole file:", function()
-    run:whole_file(".", {
+describe('indent WGSL:', function()
+  describe('whole file:', function()
+    run:whole_file('.', {
       expected_failures = {},
     })
   end)
 
-  describe("new line:", function()
-    run:new_line("basic.wgsl", { on_line = 47, text = "c: u32,", indent = 2 })
-    run:new_line("basic.wgsl", { on_line = 52, text = "c: u32,", indent = 2 })
-    run:new_line("basic.wgsl", { on_line = 56, text = "c: u32,", indent = 2 })
-    run:new_line("basic.wgsl", { on_line = 59, text = "c: u32,", indent = 2 })
-    run:new_line("type_constructor_or_function_call_expression.wgsl", {
+  describe('new line:', function()
+    run:new_line('basic.wgsl', { on_line = 47, text = 'c: u32,', indent = 2 })
+    run:new_line('basic.wgsl', { on_line = 52, text = 'c: u32,', indent = 2 })
+    run:new_line('basic.wgsl', { on_line = 56, text = 'c: u32,', indent = 2 })
+    run:new_line('basic.wgsl', { on_line = 59, text = 'c: u32,', indent = 2 })
+    run:new_line('type_constructor_or_function_call_expression.wgsl', {
       on_line = 3,
-      text = "b",
+      text = 'b',
       indent = 4,
     })
-    run:new_line("type_constructor_or_function_call_expression.wgsl", {
+    run:new_line('type_constructor_or_function_call_expression.wgsl', {
       on_line = 4,
-      text = "c",
+      text = 'c',
       indent = 4,
     })
-    run:new_line("type_constructor_or_function_call_expression.wgsl", {
+    run:new_line('type_constructor_or_function_call_expression.wgsl', {
       on_line = 8,
-      text = "b",
+      text = 'b',
       indent = 4,
     })
-    run:new_line("type_constructor_or_function_call_expression.wgsl", {
+    run:new_line('type_constructor_or_function_call_expression.wgsl', {
       on_line = 9,
-      text = "c",
+      text = 'c',
       indent = 4,
     })
   end)
