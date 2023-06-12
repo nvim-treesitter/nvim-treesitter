7b2c79f7fb24e22f3851c0ecb4673dd99255761a
Christian Clason <c.clason@uni-graz.at>
2023-06-12 09:54:30 -0600

    feat!: drop modules, general refactor and cleanup

--- queries/typst/injections.scm
+++
@@ -1,6 +0,0 @@
-((comment) @injection.content
-  (#set! injection.language "comment"))
-
-(raw_blck
-  (ident) @injection.language
-  (blob) @injection.content)
--- tests/indent/nu_spec.lua
+++ tests/indent/nu_spec.lua
@@ -1,16 +1,16 @@
-local Runner = require("tests.indent.common").Runner
+local Runner = require('tests.indent.common').Runner
 --local XFAIL = require("tests.indent.common").XFAIL
 
-local run = Runner:new(it, "tests/indent/nu", {
+local run = Runner:new(it, 'tests/indent/nu', {
   tabstop = 2,
   shiftwidth = 2,
   softtabstop = -1,
   expandtab = true,
 })
 
-describe("indent Nu:", function()
-  describe("whole file:", function()
-    run:whole_file(".", {
+describe('indent Nu:', function()
+  describe('whole file:', function()
+    run:whole_file('.', {
       expected_failures = {},
     })
   end)
