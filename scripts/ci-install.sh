ce973df77621b638156a72f473e6236e9c95aea1
Christian Clason <c.clason@uni-graz.at>
2023-06-12 09:54:30 -0600

    feat!: drop modules, general refactor and cleanup

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -2,22 +2,25 @@
 
 set -e
 
+NVIM_TAG=${NVIM_TAG-nightly}
+
 os=$(uname -s)
 if [[ $os == Linux ]]; then
-  wget https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-linux64.tar.gz
+  wget "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-linux64.tar.gz"
   tar -zxf nvim-linux64.tar.gz
   sudo ln -s "$PWD"/nvim-linux64/bin/nvim /usr/local/bin
   rm -rf "$PWD"/nvim-linux64/lib/nvim/parser
   mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
   ln -s "$PWD" ~/.local/share/nvim/site/pack/nvim-treesitter/start
 elif [[ $os == Darwin ]]; then
-  curl -L https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-macos.tar.gz | tar -xz
-  sudo ln -s "$PWD"/nvim-macos/bin/nvim /usr/local/bin
-  rm -rf "$PWD"/nvim-macos/lib/nvim/parser
+  RELEASE_NAME="nvim-macos-$(uname -m)"
+  curl -L "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/$RELEASE_NAME.tar.gz" | tar -xz
+  sudo ln -s "$PWD/$RELEASE_NAME/bin/nvim" /usr/local/bin
+  rm -rf "$PWD/$RELEASE_NAME/lib/nvim/parser"
   mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
   ln -s "$PWD" ~/.local/share/nvim/site/pack/nvim-treesitter/start
 else
-  curl -L https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-win64.zip -o nvim-win64.zip
+  curl -L "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-win64.zip" -o nvim-win64.zip
   unzip nvim-win64
   mkdir -p ~/AppData/Local/nvim/pack/nvim-treesitter/start
   mkdir -p ~/AppData/Local/nvim-data
