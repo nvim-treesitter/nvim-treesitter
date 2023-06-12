ff43015e9284ebbe2d83217abf0478800c8c9c5c
Christian Clason <c.clason@uni-graz.at>
2023-06-12 09:54:30 -0600

    feat!: drop modules, general refactor and cleanup

--- plugin/nvim-treesitter.lua
+++ plugin/nvim-treesitter.lua
@@ -1,34 +1,86 @@
--- Last Change: 2022 Apr 16
-
 if vim.g.loaded_nvim_treesitter then
   return
 end
 vim.g.loaded_nvim_treesitter = true
 
--- setup modules
-require("nvim-treesitter").setup()
-
 local api = vim.api
 
--- define autocommands
-local augroup = api.nvim_create_augroup("NvimTreesitter", {})
+local function complete_available_parsers(arglead)
+  return vim.iter.filter(function(v)
+    return v:find(arglead)
+  end, require('nvim-treesitter.parsers').get_available())
+end
 
-api.nvim_create_autocmd("Filetype", {
-  pattern = "query",
-  group = augroup,
-  callback = function()
-    api.nvim_clear_autocmds {
-      group = augroup,
-      event = "BufWritePost",
-    }
-    api.nvim_create_autocmd("BufWritePost", {
-      group = augroup,
-      buffer = 0,
-      callback = function(opts)
-        require("nvim-treesitter.query").invalidate_query_file(opts.file)
-      end,
-      desc = "Invalidate query file",
-    })
-  end,
-  desc = "Reload query",
+local function complete_installed_parsers(arglead)
+  return vim.iter.filter(function(v)
+    return v:find(arglead)
+  end, require('nvim-treesitter.config').installed_parsers())
+end
+
+-- create user commands
+api.nvim_create_user_command('TSInstallInfo', function()
+  require('nvim-treesitter.install').info()
+end, { nargs = 0, desc = 'List available treesitter parsers' })
+
+api.nvim_create_user_command('TSInstall', function(args)
+  require('nvim-treesitter.install').install(args.fargs, { force = args.bang })
+end, {
+  nargs = '+',
+  bang = true,
+  bar = true,
+  complete = complete_available_parsers,
+  desc = 'Install treesitter parsers',
+})
+
+api.nvim_create_user_command('TSInstallFromGrammar', function(args)
+  require('nvim-treesitter.install').install(args.fargs, {
+    generate_from_grammar = true,
+    force = args.bang,
+  })
+end, {
+  nargs = '+',
+  bang = true,
+  bar = true,
+  complete = complete_available_parsers,
+  desc = 'Install treesitter parsers from grammar',
+})
+
+api.nvim_create_user_command('TSInstallSync', function(args)
+  require('nvim-treesitter.install').install(args.fargs, {
+    with_sync = true,
+    force = args.bang,
+  })
+end, {
+  nargs = '+',
+  bang = true,
+  bar = true,
+  complete = complete_available_parsers,
+  desc = 'Install treesitter parsers synchronously',
+})
+
+api.nvim_create_user_command('TSUpdate', function(args)
+  require('nvim-treesitter.install').update(args.fargs)
+end, {
+  nargs = '*',
+  bar = true,
+  complete = complete_installed_parsers,
+  desc = 'Update installed treesitter parsers',
+})
+
+api.nvim_create_user_command('TSUpdateSync', function(args)
+  require('nvim-treesitter.install').update(args.fargs, { with_sync = true })
+end, {
+  nargs = '*',
+  bar = true,
+  complete = complete_installed_parsers,
+  desc = 'Update installed treesitter parsers synchronously',
+})
+
+api.nvim_create_user_command('TSUninstall', function(args)
+  require('nvim-treesitter.install').uninstall(args.fargs)
+end, {
+  nargs = '+',
+  bar = true,
+  complete = complete_installed_parsers,
+  desc = 'Uninstall treesitter parsers',
 })
--- tests/indent/angular_spec.lua
+++ tests/indent/angular_spec.lua
@@ -1,18 +1,18 @@
-local Runner = require("tests.indent.common").Runner
-local runner = Runner:new(it, "tests/indent/angular", {
+local Runner = require('tests.indent.common').Runner
+local runner = Runner:new(it, 'tests/indent/angular', {
   tabstop = 2,
   shiftwidth = 2,
   expandtab = true,
-  filetype = "htmlangular",
+  filetype = 'htmlangular',
 })
 
-describe("indent HTML Angular:", function()
-  describe("whole file:", function()
-    runner:whole_file "."
+describe('indent HTML Angular:', function()
+  describe('whole file:', function()
+    runner:whole_file('.')
   end)
 
-  describe("new line:", function()
-    for _, info in ipairs {
+  describe('new line:', function()
+    for _, info in ipairs({
       { 1, 2 },
       { 2, 2 },
       { 3, 2 },
@@ -25,11 +25,11 @@ describe("indent HTML Angular:", function()
       { 12, 6 },
       { 13, 4 },
       { 14, 2 },
-    } do
-      runner:new_line("if-else.html", { on_line = info[1], text = "//", indent = info[2] })
+    }) do
+      runner:new_line('if-else.html', { on_line = info[1], text = '//', indent = info[2] })
     end
 
-    for _, info in ipairs {
+    for _, info in ipairs({
       { 1, 2 },
       { 2, 4 },
       { 3, 6 },
@@ -39,21 +39,21 @@ describe("indent HTML Angular:", function()
       { 9, 6 },
       { 10, 6 },
       { 12, 2 },
-    } do
-      runner:new_line("switch-case.html", { on_line = info[1], text = "//", indent = info[2] })
+    }) do
+      runner:new_line('switch-case.html', { on_line = info[1], text = '//', indent = info[2] })
     end
 
-    for _, info in ipairs {
+    for _, info in ipairs({
       { 1, 2 },
       { 2, 2 },
       { 3, 2 },
       { 4, 2 },
       { 5, 0 },
-    } do
-      runner:new_line("for.html", { on_line = info[1], text = "//", indent = info[2] })
+    }) do
+      runner:new_line('for.html', { on_line = info[1], text = '//', indent = info[2] })
     end
 
-    for _, info in ipairs {
+    for _, info in ipairs({
       { 1, 2 },
       { 2, 2 },
       { 3, 2 },
@@ -63,8 +63,8 @@ describe("indent HTML Angular:", function()
       { 7, 2 },
       { 8, 2 },
       { 9, 0 },
-    } do
-      runner:new_line("defer.html", { on_line = info[1], text = "//", indent = info[2] })
+    }) do
+      runner:new_line('defer.html', { on_line = info[1], text = '//', indent = info[2] })
     end
   end)
 end)
