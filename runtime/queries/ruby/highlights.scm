70f92637d42004a58910413c2d9f7c37c191dbb1
Christian Clason <c.clason@uni-graz.at>
2023-06-12 09:54:30 -0600

    feat!: drop modules, general refactor and cleanup

--- runtime/queries/ruby/highlights.scm
+++ runtime/queries/ruby/highlights.scm
@@ -170,9 +170,6 @@
 (keyword_parameter
   (identifier) @variable.parameter)
 
-; TODO: Re-enable this once it is supported
-; ((identifier) @function
-;  (#is-not? local))
 ; Literals
 [
   (string_content)
