ca5ec2fef8614e79d5616e7ecc34e6eac2423f6f
Christian Clason <c.clason@uni-graz.at>
2023-06-24 14:33:45 +0200

    fix: remove upstreamed directives
    `#inject-lang!` and `#trim!`; fix `set-lang-from-mimetype`

--- runtime/queries/authzed/injections.scm
+++ runtime/queries/authzed/injections.scm
@@ -1,26 +1,2 @@
 ((comment) @injection.content
   (#set! injection.language "comment"))
-
-((function_call
-  name: (ident) @_name
-  .
-  (simplexpr)
-  .
-  (simplexpr
-    (string
-      (string_fragment) @injection.content)+))
-  (#any-of? @_name "replace" "search" "matches" "captures")
-  (#set! injection.language "regex")
-  (#set! injection.combined))
-
-((function_call
-  name: (ident) @_name
-  .
-  (simplexpr)
-  .
-  (simplexpr
-    (string
-      (string_fragment) @injection.content)+))
-  (#eq? @_name "jq")
-  (#set! injection.language "jq")
-  (#set! injection.combined))
