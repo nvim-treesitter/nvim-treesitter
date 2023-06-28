1af383d7b41f052d183b976950d3a4fba1440c9f
Marc Jakobi <marc.jakobi@tiko.energy>
2023-06-28 16:34:13 +0200

    injections(nix): add nixosTest.testScript + home-manager nvim config
    (+ add Check to mkDerivation bash matches)
    
    (+ add Check to mkDerivation bash matches)

--- queries/nix/injections.scm
+++ queries/nix/injections.scm
@@ -204,3 +204,32 @@
     ])
   (#match? @_func "(^|\\.)writeRust(Bin)?$")
   (#set! injection.combined))
+
+; (nixosTest) testScript
+((binding
+  attrpath: (attrpath) @_attr_name
+  (#eq? @_attr_name "nodes"))
+  (binding
+    attrpath: (attrpath) @_func_name
+    (#eq? @_func_name "testScript")
+    (_
+      (string_fragment) @injection.content
+      (#set! injection.language "python")))
+  (#set! injection.combined))
+
+; home-manager Neovim plugin config
+(attrset_expression
+  (binding_set
+    (binding
+      attrpath: (attrpath) @_ty_attr
+      (_
+        (string_fragment) @_ty)
+      (#eq? @_ty_attr "type")
+      (#eq? @_ty "lua"))
+    (binding
+      attrpath: (attrpath) @_cfg_attr
+      (_
+        (string_fragment) @injection.content
+        (#set! injection.language "lua"))
+      (#eq? @_cfg_attr "config")))
+  (#set! injection.combined))
