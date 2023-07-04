e26ffb53c705ec3cc0b59ad160bb59ed643190f0
Amaan Qureshi <amaanq12@gmail.com>
2023-07-04 04:12:21 -0400

    refactor(v)!: update upstream repository

--- queries/v/injections.scm
+++ queries/v/injections.scm
@@ -6,8 +6,8 @@
 
 ;; regex for the methods defined in `re` module
 ((call_expression
-    function: (selector_expression
-      field: (identifier) @_re)
+    name: (selector_expression
+      field: (reference_expression (identifier) @_re))
     arguments: (argument_list
-  (raw_string_literal) @regex (#offset! @regex 0 2 0 -1)))
+  (argument (literal (raw_string_literal) @regex (#offset! @regex 0 2 0 -1)))))
   (#any-of? @_re "regex_base" "regex_opt" "compile_opt"))
