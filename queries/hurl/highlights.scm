4b435ebd4ab371bc353c646d939d595f6bf4299a
Jacob Pfeifer <jacob@pfeifer.dev>
2023-07-04 16:41:06 -0400

    additional grouping of highlight queries

--- queries/hurl/highlights.scm
+++ queries/hurl/highlights.scm
@@ -9,20 +9,9 @@
   "[Asserts]"
   "[Options]"
   "[BasicAuth]"
+  (key_string)
+  (json_key_string)
 ] @property
-
-(comment) @comment @spell
-
-(key_string) @property
-(json_key_string) @property
-
-[ 
-  (value_string)
-  (quoted_string)
-  (json_string)
-] @string
-(file_value) @string.special
-(regex) @string.regex
 
 [ 
   "\\"
@@ -35,9 +24,6 @@
   (filename_escaped_char)
   (json_string_escaped_char)
 ] @string.escape
-
-(method) @type.builtin
-(multiline_string_type) @type
 
 [
   "status"
@@ -55,24 +41,22 @@
   "bytes"
 ] @function.builtin
 
-(filter) @attribute
-
-(version) @string.special
-
-"null" @constant.builtin
-"cacert" @constant.builtin
-"compressed" @constant.builtin
-"location" @constant.builtin
-"insecure" @constant.builtin
-"path_as_is" @constant.builtin
-"proxy" @constant.builtin
-"max-redirs" @constant.builtin
-"retry" @constant.builtin
-"retry-interval" @constant.builtin
-"retry-max-count" @constant.builtin
-(variable_option "variable") @constant.builtin
-"verbose" @constant.builtin
-"very-verbose" @constant.builtin
+[
+  "null"
+  "cacert"
+  "compressed"
+  "location"
+  "insecure"
+  "path_as_is"
+  "proxy"
+  "max-redirs"
+  "retry"
+  "retry-interval"
+  "retry-max-count"
+  (variable_option "variable")
+  "verbose"
+  "very-verbose"
+]
 
 (boolean) @boolean
 
@@ -122,6 +106,28 @@
 
 [ "[" "]" "{" "}" "{{" "}}" ] @punctuation.bracket
 
-"base64," @string.special
-"file," @string.special
-"hex," @string.special
+[ 
+  (value_string)
+  (quoted_string)
+  (json_string)
+] @string
+
+
+[
+  "base64,"
+  "file,"
+  "hex,"
+  (file_value)
+  (version)
+] @string.special
+
+(regex) @string.regex
+
+(multiline_string_type) @type
+
+(comment) @comment @spell
+
+(filter) @attribute
+
+(method) @type.builtin
+
