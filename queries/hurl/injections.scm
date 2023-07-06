987ce249452f6f383e2d92f8b924495f6502bc5a
Jacob Pfeifer <jacob@pfeifer.dev>
2023-07-06 11:15:20 -0400

    Update queries/hurl/injections.scm
    Co-authored-by: Pham Huy Hoang <hoangtun0810@gmail.com>

--- queries/hurl/injections.scm
+++ queries/hurl/injections.scm
@@ -3,5 +3,5 @@
 (xml) @html
 
 (multiline_string
-  (multiline_string_type) @injection.language
-  (multiline_string_content) @injection.content)
+  (multiline_string_type) @language
+  (multiline_string_content) @content)
