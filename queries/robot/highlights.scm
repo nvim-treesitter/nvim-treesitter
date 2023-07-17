ad5b1121a40e97871b4c1868b26ffd1f3816276e
Emmanuel Bustos Torres <ema2159@gmail.com>
2023-07-17 08:06:29 +0200

    feat:  Reverse the order of the captures

--- queries/robot/highlights.scm
+++ queries/robot/highlights.scm
@@ -1,22 +1,21 @@
-(comment) @comment
-(ellipses) @punctuation.delimiter
-
-(section_header) @keyword
-(extra_text) @comment
-
-(setting_statement) @keyword
-
-(variable_definition (variable_name) @variable)
-
-(keyword_definition (name) @function)
-(keyword_definition (body (keyword_setting) @keyword))
-
-(test_case_definition (name) @property)
+(argument (dictionary_variable) @string.special)
+(argument (list_variable) @string.special)
+(argument (scalar_variable) @string.special)
+(argument (text_chunk) @string)
 
 (keyword_invocation (keyword) @function)
 
-(argument (text_chunk) @string)
-(argument (scalar_variable) @string.special)
-(argument (list_variable) @string.special)
-(argument (dictionary_variable) @string.special)
+(test_case_definition (name) @property)
 
+(keyword_definition (body (keyword_setting) @keyword))
+(keyword_definition (name) @function)
+
+(variable_definition (variable_name) @variable)
+
+(setting_statement) @keyword
+
+(extra_text) @comment
+(section_header) @keyword
+
+(ellipses) @punctuation.delimiter
+(comment) @comment
