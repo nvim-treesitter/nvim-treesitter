bb07d43f478c98f884f182949388a042bce23df2
Christian Clason <c.clason@uni-graz.at>
2023-08-11 17:07:47 +0200

    feat: remove obsolete directives

--- runtime/queries/bash/injections.scm
+++ runtime/queries/bash/injections.scm
@@ -8,5 +8,4 @@
    (heredoc_redirect
      (heredoc_start) @injection.language))
  (heredoc_body) @injection.content
- (#offset! @injection.content 0 0 -1 0)
- (#downcase! @injection.language))
+ (#offset! @injection.content 0 0 -1 0))
