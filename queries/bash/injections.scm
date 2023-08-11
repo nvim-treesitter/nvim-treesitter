71532e7776578fc56355bfdac874afd1e908001e
ObserverOfTime <chronobserver@disroot.org>
2023-08-11 15:24:48 +0300

    feat(bash): add heredoc injection

--- queries/bash/injections.scm
+++ queries/bash/injections.scm
@@ -2,4 +2,10 @@
  (#set! injection.language "comment"))
 
 ((regex) @injection.content 
- (#set! injection.language "regex"))
+
+((redirected_statement
+   (heredoc_redirect
+     (heredoc_start) @injection.language))
+ (heredoc_body) @injection.content
+ (#offset! @injection.content 0 0 -1 0)
+ (#downcase! @injection.language))(#set! injection.language "regex"))
