8bad61083f3c78b7c821e52e108c70a5b2bf9d0d
Christian Clason <c.clason@uni-graz.at>
2023-08-11 17:07:47 +0200

    feat: remove obsolete directives

--- runtime/queries/php/injections.scm
+++ runtime/queries/php/injections.scm
@@ -5,17 +5,15 @@
 ((comment) @injection.content
   (#set! injection.language "phpdoc"))
 
-((heredoc
+(heredoc
   (heredoc_body) @injection.content
   (heredoc_end) @injection.language
- (#set! injection.include-children)
- (#downcase! @injection.language)))
+  (#set! injection.include-children))
 
-((nowdoc
+(nowdoc
   (nowdoc_body) @injection.content
   (heredoc_end) @injection.language
- (#set! injection.include-children)
- (#downcase! @injection.language)))
+ (#set! injection.include-children))
 
 ;; regex
 
