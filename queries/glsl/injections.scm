b5f203031282a6e9e025080fe71b58dbb43f7509
Amaan Qureshi <amaanq12@gmail.com>
2023-08-13 17:29:39 -0400

    chore(c-family): inject all preproc args again

--- queries/glsl/injections.scm
+++ queries/glsl/injections.scm
@@ -1,14 +1,4 @@
-((preproc_def
- (preproc_arg) @injection.content)
- (#lua-match? @injection.content "\n")
- (#set! injection.language "glsl"))
-
-(preproc_function_def
- (preproc_arg) @injection.content
- (#set! injection.language "glsl"))
-
-(preproc_call
- (preproc_arg) @injection.content
+((preproc_arg) @injection.content
  (#set! injection.language "glsl"))
 
 ((comment) @injection.content 
