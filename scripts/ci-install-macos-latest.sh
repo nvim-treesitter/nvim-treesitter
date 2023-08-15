0a150fc235474092758b375b4b099d8648194d48
ObserverOfTime <chronobserver@disroot.org>
2023-08-15 20:28:55 +0300

    ci: add shebangs to scripts

--- scripts/ci-install-macos-latest.sh
+++ scripts/ci-install-macos-latest.sh
@@ -1,6 +1,7 @@
-curl -L https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-macos.tar.gz | tar -xz
-sudo ln -s $(pwd)/nvim-macos/bin/nvim /usr/local/bin
-rm -rf $(pwd)/nvim-macos/lib/nvim/parser
-mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
-ln -s $(pwd) ~/.local/share/nvim/site/pack/nvim-treesitter/start
+#!/usr/bin/env bash
 
+curl -L https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-macos.tar.gz | tar -xz
+sudo ln -s "$PWD"/nvim-macos/bin/nvim /usr/local/bin
+rm -rf "$PWD"/nvim-macos/lib/nvim/parser
+mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
+ln -s "$PWD" ~/.local/share/nvim/site/pack/nvim-treesitter/start
--- scripts/ci-install-windows-2022.sh
+++ scripts/ci-install-windows-2022.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 curl -L https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-win64.zip -o nvim-win64.zip
 unzip nvim-win64
 mkdir -p ~/AppData/Local/nvim/pack/nvim-treesitter/start
 mkdir -p ~/AppData/Local/nvim-data
-cp -r $(pwd) ~/AppData/Local/nvim/pack/nvim-treesitter/start
+cp -r "$PWD" ~/AppData/Local/nvim/pack/nvim-treesitter/start
