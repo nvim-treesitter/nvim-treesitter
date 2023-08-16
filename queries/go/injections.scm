245e10df5a602d7984e7d94890375d3a16c3d3e9
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-08-16 14:07:55 +0900

    injections(go): Add back Cgo injections

--- queries/go/injections.scm
+++ queries/go/injections.scm
@@ -18,3 +18,25 @@
     ] @injection.content
     (#offset! @injection.content 0 1 0 -1)
     (#set! injection.language "regex")))
+
+(
+  (comment)+ @injection.content
+  .
+  (import_declaration
+    (import_spec path: (interpreted_string_literal) @_import_c))
+  (#eq? @_import_c "\"C\"")
+  (#lua-match? @injection.content "^/%*.*%*/$")
+  (#set! injection.language "c")
+  (#offset! @injection.content 0 2 0 -2)
+  (#set! injection.combined))
+
+(
+  (comment)+ @injection.content 
+  .
+  (import_declaration
+    (import_spec path: (interpreted_string_literal) @_import_c))
+  (#eq? @_import_c "\"C\"")
+  (#lua-match? @injection.content "^//")
+  (#set! injection.language "c")
+  (#offset! @injection.content 0 2 0 0)
+  (#set! injection.combined))
