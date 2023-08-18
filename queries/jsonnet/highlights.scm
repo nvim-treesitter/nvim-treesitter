35d372c0cb9d74ea8c135be32ef28bbf01ec140b
Duologic <jeroen@simplistic.be>
2023-08-18 19:47:27 +0200

    feat(jsonnet): update parser and queries
    We've done a few refactoring rounds, improved highlighting and added folding and locals.
    
    The queries in this PR are maintained and copied from upstream.

--- queries/jsonnet/highlights.scm
+++ queries/jsonnet/highlights.scm
@@ -1,21 +1,56 @@
+(id) @variable
+(comment) @comment
+
+; Literals
+(null) @constant.builtin
+(string) @string
+(number) @number
 [
   (true)
   (false)
 ] @boolean
 
-(comment) @comment
-(id) @variable
-(import) @include
-(null) @constant.builtin
-(number) @number
-(string) @string
+; Keywords
+"for" @repeat
+"in" @keyword.operator
+"function" @keyword.function
+[
+  "if"
+  "then"
+  "else"
+] @conditional
+[
+  (local)
+  (tailstrict)
+  "function"
+  "assert"
+  "error"
+] @keyword
 
-(fieldname (id) @field)
-(fieldaccess
-  last: (id) @field)
-(fieldaccess_super
-  (id) @field)
+[
+  (dollar)
+  (self)
+  (super)
+] @variable.builtin
+((id) @variable.builtin
+ (#eq? @variable.builtin "std"))
 
+; Operators
+[
+  (multiplicative)
+  (additive)
+  (bitshift)
+  (comparison)
+  (equality)
+  (bitand)
+  (bitxor)
+  (bitor)
+  (and)
+  (or)
+  (unaryop)
+] @operator
+
+; Punctuation
 [
   "["
   "]"
@@ -30,63 +65,100 @@
   ","
   ";"
   ":"
-  "::"
-  ":::"
 ] @punctuation.delimiter
 
-; Nested field inheritance
-("+" @punctuation.delimiter [":" "::" ":::"])
-
-"=" @operator
-(unaryop) @operator
-(binary
-  operator: (_) @operator)
-
-"for" @repeat
-
-"function" @keyword.function
-
-"in" @keyword.operator
-
 [
- (local)
- (tailstrict)
- "assert"
-] @keyword
+  "::"
+  ":::"
+] @punctuation.special
 
+(field
+  (fieldname) "+" @punctuation.special)
+
+; Imports
 [
-  "else"
-  "if"
-  "then"
-] @conditional
+  (import)
+  (importstr)
+] @include
 
-"error" @exception
+; References
 
-[
-  (dollar)
-  (self)
-  (super)
-] @variable.builtin
-((id) @variable.builtin
- (#eq? @variable.builtin "std"))
+; Make reference same color as parameter 
+; (may incur performance issues on big files)
+; Depends on locals.scm
+((id) @parameter.reference
+ (#is? @parameter.reference parameter))
 
-; Function declaration
+((id) @function.reference
+ (#is? @function.reference function))
+
+((id) @var.reference
+ (#is? @var.reference var))
+((id) @define
+ (#is? @var.reference var))
+
+; References do not apply to static field IDs
+; Workaround for `(#is-not? local)` not supported
+(fieldname (id) @field)
+(fieldname (string
+             (string_start) @text.strong
+             (string_content) @field
+             (string_end) @text.strong
+           ))
+
+; But it does apply if ID in an expression
+(fieldname
+ ("["
+  (id) @parameter.reference
+  "]"
+  (#is? @parameter.reference parameter)))
+(fieldname
+ ("["
+  (id) @define
+  "]"
+  (#is? @var.reference var)))
+
+; Functions
+(field
+  function: (fieldname (id) @function))
+(field
+  function: (fieldname
+              (string
+                (string_start) @text.strong
+                (string_content) @function
+                (string_end) @text.strong
+              )))
 (param
   identifier: (id) @parameter)
-(field
-  (fieldname (id) @function)
-  [(anonymous_function) (params)])
-(bind
-  function: (id) @function)
+
+(bind (id) @define)
+(bind function: (id) @function)
 
 ; Function call
-(functioncall (id) @function.call)
 (functioncall
   (fieldaccess
-    last: (id) @function.call))
-(functioncall
+    last: (id) @function.call
+  )?
   (fieldaccess_super
-    (id) @function.call))
-(named_argument (id) @parameter)
+    (id) @function.call
+  )?
+  (id)? @function.call
+  "("
+  (args
+    (named_argument
+      (id) @parameter
+    )
+  )?
+  ")"
+)
 
+; Emphasize implicit plus usage
+(implicit_plus
+  (_ "}"? @text.danger)
+  (object
+    "{" @text.danger
+  )
+)
+
+; ERROR
 (ERROR) @error
