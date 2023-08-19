23482601154ba11722c7d1ef3373d2c77bf457ba
ObserverOfTime <chronobserver@disroot.org>
2023-08-19 19:48:32 +0300

    feat(c-family): inherit injections

--- queries/rust/indents.scm
+++ queries/rust/indents.scm
@@ -24,6 +24,12 @@
   (macro_definition)
 ] @indent.begin
 
+(trait_item
+  body: (_) @indent.begin)
+
+(string_literal
+  (escape_sequence)) @indent.begin
+
 ; Typing in "(" inside macro definitions breaks the tree entirely
 ; Making macro_definition becoming errors
 ; Offset this by adding back one indent for start of macro rules
@@ -80,6 +86,16 @@
 
 (struct_pattern
   "}" @indent.end)
+
+; Typing in "(" inside macro definitions breaks the tree entirely
+; Making macro_definition becoming errors
+; Offset this by adding back one indent for start of macro rules
+(ERROR
+  .
+  "macro_rules!"
+  "(" @indent.begin
+  (#set! indent.immediate)
+  (#set! indent.start_at_same_line))
 
 (trait_item
   body: (declaration_list
