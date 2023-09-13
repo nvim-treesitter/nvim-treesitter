876f5e0d1bb844156c8db54b83135952de6ae913
Christoph Sax <christoph.sax@mailbox.org>
2023-09-13 20:54:04 +0200

    fix(t32): update indent queries & test
    The latest release of the grammar changes the handling of consecutive
    line breaks. This breaks the existing test for indents.
    
    This commit updates the indent queries and fixes the associated test.

--- queries/t32/indents.scm
+++ queries/t32/indents.scm
@@ -1,34 +1,20 @@
-; Subroutines & scoping
 (block
   ")" @indent.branch .) @indent.begin
 
+(if_block) @indent.begin
 
-; Control flow statements
-(
-  (if_block
-    condition: (_)
-    . (_) @_then) @indent.begin
-  (#not-has-type? @_then block)
-)
+(if_block
+  (block)) @indent.dedent
 
 (else_block) @indent.branch
 
 (else_block
-  (if_block) @indent.dedent) @indent.branch
-
-(
-  (else_block
-    (if_block
-      condition: (_)
-      . (_) @_then)) @indent.branch
-  (#not-has-type? @_then block)
-)
+  (if_block) @indent.dedent)
 
 (while_block
   (command_expression)) @indent.auto
 
 (repeat_block
   (command_expression)) @indent.auto
-
 
 (comment) @indent.auto
