5929f2588907a5c8c4bdc86d2feb77b9e0d87802
ObserverOfTime <chronobserver@disroot.org>
2023-09-20 19:34:55 +0300

    style(authzed): reformat highlights

--- queries/authzed/highlights.scm
+++ queries/authzed/highlights.scm
@@ -1,40 +1,38 @@
-; highlights.scm
 ((identifier) @keyword
  (#any-of? @keyword "definition" "permission" "relation"))
 
-((permission_literal) @variable.builtin)
+(permission_literal) @variable.builtin
 
 (permission (identifier) @type)
 (relation (identifier) @constant)
 (perm_expression (identifier) @property)
-((block_start) @punctuation.bracket)
-((block_end) @punctuation.bracket)
+
+[
+  (block_start)
+  (block_end)
+] @punctuation.bracket
 
 (block (identifier) (identifier) @constructor)
 
-((plus_literal) @operator)
-((hash_literal) @comment)
+[
+  (plus_literal)
+  (pipe_literal)
+] @operator
 
-; relations
-((relation_literal) @function)
+(relation_literal) @function
 (rel_expression (identifier) @property)
-
-
-((pipe_literal) @operator)
 
 (relation
   (rel_expression
-    (
-      (hash_literal)
-      .
-      (identifier) @constant) @comment))
-
+    ((hash_literal)
+     . (identifier) @constant) @comment))
 
 (permission
   (perm_expression
-    (
-      (stabby)
-      .
-      (identifier) @function) @operator))
+    ((stabby)
+     . (identifier) @function) @operator))
 
-((comment) @comment)
+[
+  (hash_literal)
+  (comment)
+] @comment
