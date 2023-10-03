c8f3bfbba4864125e44cc796986d5cba43cda68f
Jaehwang Jung <tomtomjhj@gmail.com>
2023-10-03 19:16:32 +0900

    feat(query): use @function.call for predicate

--- queries/query/highlights.scm
+++ queries/query/highlights.scm
@@ -2,7 +2,7 @@
 (escape_sequence) @string.escape
 (capture (identifier) @type)
 (anonymous_node (identifier) @string)
-(predicate name: (identifier) @function)
+(predicate name: (identifier) @function.call)
 (named_node name: (identifier) @variable)
 (field_definition name: (identifier) @property)
 (negated_field "!" @operator (identifier) @property)
