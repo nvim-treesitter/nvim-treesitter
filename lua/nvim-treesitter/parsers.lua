ad5e686ebc4aeae5502ad0a554ad39af72a76ac9
aMOPel <momoPFL@gmail.com>
2023-10-21 10:06:24 +0200

    feat(nim): added nim parser and queries

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -1128,6 +1128,22 @@ list.nickel = {
   },
 }
 
+list.nim = {
+  install_info = {
+    url = "https://github.com/alaviss/tree-sitter-nim",
+    files = { "src/parser.c", "src/scanner.cc" },
+  },
+  maintainers = { "@aMOPel" },
+}
+
+list.nim_format_string = {
+  install_info = {
+    url = "https://github.com/aMOPel/tree-sitter-nim-format-string",
+    files = { "src/parser.c" },
+  },
+  maintainers = { "@aMOPel" },
+}
+
 list.ninja = {
   install_info = {
     url = "https://github.com/alemuller/tree-sitter-ninja",
