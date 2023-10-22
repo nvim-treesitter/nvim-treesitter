5290dd2ea4b4364e46f9ff9f043b249a9266336b
Colin Kennedy <colinvfx@gmail.com>
2023-10-21 18:24:44 -0700

    Update queries/objdump/injections.scm
    Co-authored-by: Amaan Qureshi <amaanq12@gmail.com>

--- queries/objdump/injections.scm
+++ queries/objdump/injections.scm
@@ -7,4 +7,4 @@
 ; To be added once a compatible Assembly parser is merged into nvim-treesitter
 ;
 ; ((instruction) @injection.content
-;  (#set! injection.language "assembly"))
+;  (#set! injection.language "asm"))
