1fd012551f2d8e67132f0a381bb35cc4db1be592
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-10-28 16:47:18 -0300

    Add initial tests for Julia language indentation

---
+++ tests/indent/julia/assignment.jl
@@ -0,0 +1,11 @@
+my_variable =
+    1 + 2 - 3 *
+    4 / 5 %
+    6
+
+function test()
+    my_variable =
+        1 + 2 - 3 *
+        4 / 5 %
+        6
+end
---
+++ tests/indent/julia/parameter_list.jl
@@ -0,0 +1,15 @@
+function test(a, b, c, d, e)
+    vcat(a,
+         b,
+         c,
+         d,
+         e)
+
+    vcat(
+        a,
+        b,
+        c,
+        d,
+        e
+    )
+end
