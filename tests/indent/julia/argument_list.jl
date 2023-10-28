fc3bfd8f89a9a9ec088d686c68e45a35106df4de
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-10-28 16:47:18 -0300

    Add initial tests for Julia language indentation

---
+++ tests/indent/julia/argument_list.jl
@@ -0,0 +1,13 @@
+function test(a::Number,
+              b::Float64,
+              c::Union{Float64, Integer},
+              d::Bool)
+end
+
+function test(
+    a::Number,
+    b::Float64,
+    c::Union{Float64, Integer},
+    d::Bool
+)
+end
---
+++ tests/indent/julia_spec.lua
@@ -0,0 +1,17 @@
+local Runner = require("tests.indent.common").Runner
+local XFAIL = require("tests.indent.common").XFAIL
+
+local run = Runner:new(it, "tests/indent/julia", {
+  tabstop = 4,
+  shiftwidth = 4,
+  softtabstop = 4,
+  expandtab = true,
+})
+
+describe("indent Julia:", function()
+  describe("whole file:", function()
+    run:whole_file(".", {
+      expected_failures = {},
+    })
+  end)
+end)
