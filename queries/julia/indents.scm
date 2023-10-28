639c22c52ca2c76c6016de746836eca20145ea8c
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-10-28 10:23:50 -0300

    Improve indentation in Julia language
    This commit improves the indentation in the function declaration,
    function call, and in the assignment expressions.

--- queries/julia/indents.scm
+++ queries/julia/indents.scm
@@ -15,8 +15,8 @@
   (assignment)
   (for_binding)
 
-  (binary_expression)
   (call_expression)
+  (parenthesized_expression)
 
   (tuple_expression)
   (comprehension_expression)
@@ -48,3 +48,10 @@
   (block_comment)
 ] @indent.ignore
 
+((argument_list) @indent.align
+  (#set! indent.open_delimiter "(")
+  (#set! indent.close_delimiter ")"))
+
+((parameter_list) @indent.align
+  (#set! indent.open_delimiter "(")
+  (#set! indent.close_delimiter ")"))
