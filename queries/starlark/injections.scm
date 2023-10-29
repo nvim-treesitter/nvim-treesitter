b39121d6b8ede83c6479b29e5b6f71c238b957fa
Jongwook Choi <wookayin@gmail.com>
2023-10-28 21:46:14 -0400

    fix(starlark): injections should not inherit from python
    starlark does not have class, can't use all the queries used in python.

--- queries/starlark/injections.scm
+++ queries/starlark/injections.scm
@@ -1,1 +1,13 @@
-; inherits python
+; see queries/python/injections.scm
+
+(call
+  function: (attribute
+              object: (identifier) @_re)
+  arguments: (argument_list (string
+                              (string_content) @injection.content) @_string)
+  (#eq? @_re "re")
+  (#lua-match? @_string "^r.*")
+  (#set! injection.language "regex"))
+
+((comment) @injection.content
+ (#set! injection.language "comment"))
