d48e942084c94dc7679dd8180a35220af81be9d3
postsolar <120750161+postsolar@users.noreply.github.com>
2023-11-04 15:53:22 +0200

    Update queries/purescript/locals.scm
    More specific captures for declarations
    
    Co-authored-by: ObserverOfTime <chronobserver@disroot.org>

--- queries/purescript/locals.scm
+++ queries/purescript/locals.scm
@@ -1,4 +1,4 @@
-(signature name: (variable)) @definition
-(function name: (variable)) @definition
+(signature name: (variable)) @definition.type
+(function name: (variable)) @definition.function
 (pat_name (variable)) @definition
 (exp_name (variable)) @reference
