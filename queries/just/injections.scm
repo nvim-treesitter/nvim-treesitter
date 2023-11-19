7d63f39f797b0fdd3c97355d88144a5f2a11c688
Tomas Sandven <tomas@sandven.email>
2023-11-19 16:30:11 +0100

    Improve just injections

--- queries/just/injections.scm
+++ queries/just/injections.scm
@@ -1,28 +1,30 @@
 
 ; If no shell is explicitly set, let's default to bash for recipe bodies
 (source_file 
-  (item (recipe (body (recipe_body) @injection.content
-    (#set! injection.include-children)
-    (#set! injection.language "bash"))))) 
+  (item (recipe (body (recipe_body) @injection.content))) 
+  (#set! injection.include-children)
+  (#set! injection.language "bash"))
 
-; Sets the injection language of a recipe to the executable of the shebang line
-;
-; This works well in most cases, since it's probably "bash" or "python", but
-; will break in cases where the executable doesn't match the language name,
-; such as "python3"
-;
+; If a default shell is explicitly set, use the executable name for normal recipes
+(source_file 
+  (item (setting lang: (NAME) @_lang))
+  (item (recipe (body (recipe_body) @injection.content))) 
+  (#eq? @_lang "python3")
+  (#set! injection.language "python")
+  (#set! injection.include-children))
+(source_file 
+  (item (setting lang: (NAME) @injection.language))
+  (item (recipe (body (recipe_body) @injection.content))) 
+  (#set! injection.include-children))
+
+; For shebang recipes, use the shebang executable name as the language
+(shebang_recipe
+  (shebang interpreter: (TEXT) @_lang)
+  (shebang_body) @injection.content
+  (#eq? @_lang "python3")
+  (#set! injection.language "python")
+  (#set! injection.include-children))
 (shebang_recipe
   (shebang interpreter: (TEXT) @injection.language)
   (shebang_body) @injection.content
   (#set! injection.include-children))
-
-; If a shell is set (set shell := ["zsh"]) then use that
-;(source_file 
-;  (item (setting lang: (NAME) @injection.language))
-;  (item (recipe (body (recipe_body) @injection.content
-;    (#set! injection.include-children))))) 
-;
-; This works if the shell is set to something that matches a language name,
-; like "bash", "zsh", "python" etc. However, since it can be set to any
-; executable, including a full path, this can easily break. This needs some
-; kind of post processing to be usable.
