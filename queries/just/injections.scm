8855617c22c0a88f243af50243e0ba173b103c14
Tomas Sandven <tomas@sandven.email>
2023-11-19 16:00:38 +0100

    Improve just injections, document issues

--- queries/just/injections.scm
+++ queries/just/injections.scm
@@ -1,12 +1,28 @@
 
-(shebang_recipe 
-  (shebang_body) @injection.content (#set! injection.language "sh")) 
+; If no shell is explicitly set, let's default to bash for recipe bodies
+(source_file 
+  (item (recipe (body (recipe_body) @injection.content
+    (#set! injection.include-children)
+    (#set! injection.language "bash"))))) 
 
-;(shebang_recipe 
-;  (shebang 
-;    interpreter:(TEXT) @injection.language)
-;  (shebang_body) @injection.content) 
+; Sets the injection language of a recipe to the executable of the shebang line
 ;
+; This works well in most cases, since it's probably "bash" or "python", but
+; will break in cases where the executable doesn't match the language name,
+; such as "python3"
+;
+(shebang_recipe
+  (shebang interpreter: (TEXT) @injection.language)
+  (shebang_body) @injection.content
+  (#set! injection.include-children))
+
+; If a shell is set (set shell := ["zsh"]) then use that
 ;(source_file 
-;  (item (setting lang:(NAME) @injection.language))
-;  (item (recipe (body (recipe_body) @injection.content)))) 
+;  (item (setting lang: (NAME) @injection.language))
+;  (item (recipe (body (recipe_body) @injection.content
+;    (#set! injection.include-children))))) 
+;
+; This works if the shell is set to something that matches a language name,
+; like "bash", "zsh", "python" etc. However, since it can be set to any
+; executable, including a full path, this can easily break. This needs some
+; kind of post processing to be usable.
