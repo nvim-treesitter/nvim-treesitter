1af4464670ddbb46c215e80a777e0fcf5777f63c
Tomas Sandven <tomas@sandven.email>
2023-11-20 21:33:05 +0100

    Update queries/just/highlights.scm

--- queries/just/highlights.scm
+++ queries/just/highlights.scm
@@ -1,39 +1,102 @@
 
-(assignment (NAME) @variable)
-(alias (NAME) @variable)
-(value (NAME) @variable)
-(parameter (NAME) @variable)
-(setting (NAME) @keyword)
-(setting "shell" @keyword)
-
-(call (NAME) @function)
-(dependency (NAME) @function)
-(depcall (NAME) @function)
-(recipeheader (NAME) @function)
-
-(depcall (expression) @parameter)
-(parameter) @parameter
-(variadic_parameters) @parameter
-
-["if" "else"] @conditional
-
-(string) @string
-
-(boolean ["true" "false"]) @boolean
-
 (comment) @comment
+(string) @string
+(boolean ["true" "false"]) @boolean
+["if" "else"] @conditional
+["export" "alias" "set"] @keyword
+["@" "==" "!=" "+" ":="] @operator
+[ "(" ")" "[" "]" "{{" "}}" "{" "}"] @punctuation.bracket
 
+[
+  (assignment (NAME))
+  (alias (NAME))
+  (value (NAME))
+  (parameter (NAME))
+] @variable
+
+; Recipe definitions
+[
+  (recipeheader (NAME))
+  (dependency (NAME))
+  (dependency (depcall (NAME)))
+] @function
+(parameter) @parameter
+(depcall (expression (value (NAME) @parameter)))
+
+; Fallback highlighting for recipe bodies
 (recipe
   (body) @text.literal.block (#set! "priority" 90))
-
 (shebang_recipe
   (shebang interpreter: (TEXT) @keyword) @comment
   (shebang_body) @text.literal.block (#set! "priority" 90))
 
-["export" "alias" "set"] @keyword
+; Ref: https://just.systems/man/en/chapter_26.html
+(setting (NAME) @error)
+(setting (NAME) @constant.builtin
+  (#any-of? @constant.builtin 
+    "allow-duplicate-recipes"
+    "dotenv-filename"
+    "dotenv-load"
+    "dotenv-path"
+    "export"
+    "fallback"
+    "ignore-comments"
+    "positional-arguments"
+    "shell"
+    "tempdir"
+    "windows-powershell"
+    "windows-shell"))
+; Special handling for the shell setting
+(setting "shell" @constant.builtin)
+(setting lang: (NAME) @string.special)
 
-["@" "==" "!=" "+" ":="] @operator
-
-[ "(" ")" "[" "]" "{{" "}}" "{" "}"] @punctuation.bracket
-
-(ERROR) @error
+; Ref: https://just.systems/man/en/chapter_31.html
+(call (NAME) @error)
+(call (NAME) @function
+  (#any-of? @function 
+    "arch"
+    "num_cpus"
+    "os"
+    "os_family"
+    "env_var"
+    "env_var_or_default"
+    "env"
+    "invocation_directory"
+    "invocation_directory_native"
+    "justfile"
+    "justfile_directory"
+    "just_executable"
+    "quote"
+    "replace"
+    "replace_regex"
+    "trim"
+    "trim_end"
+    "trim_end_match"
+    "trim_end_matches"
+    "trim_start"
+    "trim_start_match"
+    "trim_start_matches"
+    "capitalize"
+    "kebabcase"
+    "lowercamelcase"
+    "lowercase"
+    "shoutykebabcase"
+    "shoutysnakecase"
+    "snakecase"
+    "titlecase"
+    "uppercamelcase"
+    "uppercase"
+    "absolute_path"
+    "extension"
+    "file_name"
+    "file_stem"
+    "parent_directory"
+    "without_extension"
+    "clean"
+    "join"
+    "path_exists"
+    "error"
+    "sha256"
+    "sha256_file"
+    "uuid"
+    "semver_matches"))
