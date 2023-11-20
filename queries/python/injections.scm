38c6b2bf25cb98ab41cd359329f83a34c18be1fc
Riley Bruins <ribru17@hotmail.com>
2023-11-19 18:56:53 -0800

    feat(python): `@string.regex` capture, injection improvements

--- queries/python/injections.scm
+++ queries/python/injections.scm
@@ -4,7 +4,6 @@
   arguments: (argument_list (string
                               (string_content) @injection.content) @_string)
   (#eq? @_re "re")
-  (#lua-match? @_string "^r.*")
   (#set! injection.language "regex"))
 
 ((comment) @injection.content
