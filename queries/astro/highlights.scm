8189d91cf82b78fd765b28c674537688bce70c10
virchau13 <virchau13@hexular.net>
2023-11-27 21:11:28 +0800

    astro: add custom component highlighting (#5728)

--- queries/astro/highlights.scm
+++ queries/astro/highlights.scm
@@ -3,3 +3,11 @@
 [ "---" ] @punctuation.delimiter
 
 [ "{" "}" ] @punctuation.special
+
+; custom components get `@type` highlighting
+((start_tag (tag_name) @type)
+ (#lua-match? @type "^[A-Z]"))
+((end_tag (tag_name) @type)
+ (#lua-match? @type "^[A-Z]"))
+((erroneous_end_tag (erroneous_end_tag_name) @type)
+ (#lua-match? @type "^[A-Z]"))
