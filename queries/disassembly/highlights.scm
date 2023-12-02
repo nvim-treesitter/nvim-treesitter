708e4048bd1105630e9c3b6bef5ebc0fc5551bf3
Colin Kennedy <colinvfx@gmail.com>
2023-12-02 13:30:08 -0800

    Added tree-sitter-disassembly

---
+++ queries/disassembly/highlights.scm
@@ -0,0 +1,18 @@
+(byte) @constant
+
+[
+  (address)
+  (hexadecimal)
+  (integer)
+] @number
+
+(identifier) @variable
+
+(bad_instruction) @text.warning
+(code_location (identifier) @function.call)
+(comment) @comment
+(instruction) @function
+(memory_dump) @string
+
+["<" ">"] @punctuation.special
+["+" ":"] @punctuation.delimiter
