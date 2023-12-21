11696561bae08598a3165bba1cb5171d27380e51
Christian Clason <c.clason@uni-graz.at>
2023-12-21 12:59:14 +0100

    vimdoc: add note, warning, deprecated

--- queries/vimdoc/highlights.scm
+++ queries/vimdoc/highlights.scm
@@ -23,3 +23,9 @@
 (argument) @parameter
 (keycode) @string.special
 (url) @text.uri
+((note) @text.note
+   (#any-of? @text.note "Note:" "NOTE:" "Notes:"))
+((note) @text.warning
+   (#any-of? @text.warning "Warning:" "WARNING:"))
+((note) @text.danger
+   (#any-of? @text.danger "Deprecated:" "DEPRECATED:"))
