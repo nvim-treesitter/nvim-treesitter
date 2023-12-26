c151ec2c0e8e8874c862e807bb782bc64ef990aa
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-12-26 21:57:22 +0900

    fix(rust): Add back 1 indent for macro rules (#5834)

--- queries/rust/indents.scm
+++ queries/rust/indents.scm
@@ -23,6 +23,43 @@
   (token_tree)
   (macro_definition)
 ] @indent.begin
+; Typing in "(" inside macro definitions breaks the tree entirely
+; Making macro_definition becoming errors
+; Offset this by adding back one indent for start of macro rules
+(ERROR
+  .
+  "macro_rules!"
+  "(" @indent.begin
+  (#set! indent.immediate)
+  (#set! indent.start_at_same_line))
+
+(block
+  "}" @indent.end)
+
+(enum_item
+  body:
+    (enum_variant_list
+      "}" @indent.end))
+
+(impl_item
+  body:
+    (declaration_list
+      "}" @indent.end))
+
+(match_expression
+  body:
+    (match_block
+      "}" @indent.end))
+
+(mod_item
+  body:
+    (declaration_list
+      "}" @indent.end))
+
+(struct_item
+  body:
+    (field_declaration_list
+      "}" @indent.end))
 
 ; Typing in "(" inside macro definitions breaks the tree entirely
 ; Making macro_definition becoming errors
