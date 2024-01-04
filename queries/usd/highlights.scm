9e02f9b98121cfaf45f070aeef4f413a85c0efc6
Pham Huy Hoang <hoangtun0810@gmail.com>
2024-01-05 03:19:54 +0900

    refactor: manual pre-cleanup
    Moving comments, adding `format-ignore` to lines that will be better
    with it

--- queries/usd/highlights.scm
+++ queries/usd/highlights.scm
@@ -39,128 +39,125 @@
 
 (attribute_type) @type
 (
- ;; Reference: https://openusd.org/release/api/sdf_page_front.html
  (attribute_type) @type.builtin
- (#any-of? @type.builtin
-  ;; Scalar types
-  "asset" "asset[]"
-  "bool" "bool[]"
-  "double" "double[]"
-  "float" "float[]"
-  "half" "half[]"
-  "int" "int[]"
-  "int64" "int64[]"
-  "string" "string[]"
-  "timecode" "timecode[]"
-  "token" "token[]"
-  "uchar" "uchar[]"
-  "uint" "uint[]"
-  "uint64" "uint64[]"
+  ;format-ignore
+  (#any-of? @type.builtin
+    ;; Reference: https://openusd.org/release/api/sdf_page_front.html
+    ;; Scalar types
+    "asset" "asset[]"
+    "bool" "bool[]"
+    "double" "double[]"
+    "float" "float[]"
+    "half" "half[]"
+    "int" "int[]"
+    "int64" "int64[]"
+    "string" "string[]"
+    "timecode" "timecode[]"
+    "token" "token[]"
+    "uchar" "uchar[]"
+    "uint" "uint[]"
+    "uint64" "uint64[]"
 
-  ;; Dimensioned Types
-  "double2" "double2[]"
-  "double3" "double3[]"
-  "double4" "double4[]"
-  "float2" "float2[]"
-  "float3" "float3[]"
-  "float4" "float4[]"
-  "half2" "half2[]"
-  "half3" "half3[]"
-  "half4" "half4[]"
-  "int2" "int2[]"
-  "int3" "int3[]"
-  "int4" "int4[]"
-  "matrix2d" "matrix2d[]"
-  "matrix3d" "matrix3d[]"
-  "matrix4d" "matrix4d[]"
-  "quatd" "quatd[]"
-  "quatf" "quatf[]"
-  "quath" "quath[]"
+    ;; Dimensioned Types
+    "double2" "double2[]"
+    "double3" "double3[]"
+    "double4" "double4[]"
+    "float2" "float2[]"
+    "float3" "float3[]"
+    "float4" "float4[]"
+    "half2" "half2[]"
+    "half3" "half3[]"
+    "half4" "half4[]"
+    "int2" "int2[]"
+    "int3" "int3[]"
+    "int4" "int4[]"
+    "matrix2d" "matrix2d[]"
+    "matrix3d" "matrix3d[]"
+    "matrix4d" "matrix4d[]"
+    "quatd" "quatd[]"
+    "quatf" "quatf[]"
+    "quath" "quath[]"
 
-  ;; Extra Types
-  "color3f" "color3f[]"
-  "normal3f" "normal3f[]"
-  "point3f" "point3f[]"
-  "texCoord2f" "texCoord2f[]"
-  "vector3d" "vector3d[]"
-  "vector3f" "vector3f[]"
-  "vector3h" "vector3h[]"
+    ;; Extra Types
+    "color3f" "color3f[]"
+    "normal3f" "normal3f[]"
+    "point3f" "point3f[]"
+    "texCoord2f" "texCoord2f[]"
+    "vector3d" "vector3d[]"
+    "vector3f" "vector3f[]"
+    "vector3h" "vector3h[]"
 
-  "dictionary"
+    "dictionary"
 
-  ;; Deprecated Types
-  "EdgeIndex" "EdgeIndex[]"
-  "FaceIndex" "FaceIndex[]"
-  "Matrix4d" "Matrix4d[]"
-  "PointIndex" "PointIndex[]"
-  "PointFloat" "PointFloat[]"
-  "Transform" "Transform[]"
-  "Vec3f" "Vec3f[]"
- )
-)
+    ;; Deprecated Types
+    "EdgeIndex" "EdgeIndex[]"
+    "FaceIndex" "FaceIndex[]"
+    "Matrix4d" "Matrix4d[]"
+    "PointIndex" "PointIndex[]"
+    "PointFloat" "PointFloat[]"
+    "Transform" "Transform[]"
+    "Vec3f" "Vec3f[]"))
 
 (
  (identifier) @keyword
- (#any-of? @keyword
+  ; format-ignore
+  (#any-of? @keyword
+    ; Reference: https://openusd.org/release/api/sdf_page_front.html
+    ; LIVRPS names
+    "inherits"
+    "payload"
+    "references"
+    "specializes"
+    "variantSets"
+    "variants"
 
-  ;; Reference: https://openusd.org/release/api/sdf_page_front.html
-  ;; LIVRPS names
-  "inherits"
-  "payload"
-  "references"
-  "specializes"
-  "variantSets"
-  "variants"
+    ; assetInfo names
+    "assetInfo"
+    "identifier"
+    "name"
+    "payloadAssetDependencies"
+    "version"
 
-  ; assetInfo names
-  "assetInfo"
-  "identifier"
-  "name"
-  "payloadAssetDependencies"
-  "version"
+    ; clips names
+    "clips"
 
-  ;; clips names
-  "clips"
+    "active"
+    "assetPaths"
+    "manifestAssetPath"
+    "primPath"
+    "templateAssetPath"
+    "templateEndTime"
+    "templateStartTime"
+    "templateStride"
+    "times"
 
-  "active"
-  "assetPaths"
-  "manifestAssetPath"
-  "primPath"
-  "templateAssetPath"
-  "templateEndTime"
-  "templateStartTime"
-  "templateStride"
-  "times"
+    ; customData names
+    "customData"
 
-  ;; customData names
-  "customData"
+    "apiSchemaAutoApplyTo"
+    "apiSchemaOverridePropertyNames"
+    "className"
+    "extraPlugInfo"
+    "isUsdShadeContainer"
+    "libraryName"
+    "providesUsdShadeConnectableAPIBehavior"
+    "requiresUsdShadeEncapsulation"
+    "skipCodeGeneration"
 
-  "apiSchemaAutoApplyTo"
-  "apiSchemaOverridePropertyNames"
-  "className"
-  "extraPlugInfo"
-  "isUsdShadeContainer"
-  "libraryName"
-  "providesUsdShadeConnectableAPIBehavior"
-  "requiresUsdShadeEncapsulation"
-  "skipCodeGeneration"
+    ; Layer metadata names
+    "colorConfiguration"
+    "colorManagementSystem"
+    "customLayerData"
+    "defaultPrim"
+    "doc"
+    "endTimeCode"
+    "framesPerSecond"
+    "owner"
+    "startTimeCode"
+    "subLayers"
 
-  ;; Layer metadata names
-  "colorConfiguration"
-  "colorManagementSystem"
-  "customLayerData"
-  "defaultPrim"
-  "doc"
-  "endTimeCode"
-  "framesPerSecond"
-  "owner"
-  "startTimeCode"
-  "subLayers"
-
-  ;; Prim metadata
-  "instanceable"
- )
-)
+    ; Prim metadata
+    "instanceable"))
 
 ;; Common attribute metadata
 (
