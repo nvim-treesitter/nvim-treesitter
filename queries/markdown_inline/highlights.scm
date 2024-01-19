82547a81e9d02923807a527e2137f5719ca68dc5
Christian Clason <c.clason@uni-graz.at>
2024-01-19 19:37:44 +0100

    fix(highlights): use `markup.raw.delimiter` for fences

--- queries/markdown_inline/highlights.scm
+++ queries/markdown_inline/highlights.scm
@@ -16,10 +16,10 @@
 ] @string.escape
 
 ; Conceal codeblock and text style markers
-((code_span_delimiter) @markup.raw
+((code_span_delimiter) @markup.raw.delimiter
   (#set! conceal ""))
 
-((emphasis_delimiter) @markup.strong
+((emphasis_delimiter) @conceal
   (#set! conceal ""))
 
 ; Conceal inline links
