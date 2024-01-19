6548bb64c11e0860a504de3012e8aa14e1d51d7e
Jędrzej Boczar <yendreij@gmail.com>
2024-01-19 16:08:23 +0100

    feat(meson): add indents.scm

---
+++ queries/meson/indents.scm
@@ -0,0 +1,25 @@
+[
+  (list)
+  (dictionaries)
+  (normal_command)
+  (if_condition)
+  (foreach_command)
+  (ternaryoperator)
+  (ERROR
+    "?") ; support partial ternary
+] @indent.begin
+
+[
+  ")"
+  "]"
+  "}"
+  (elseif_command)
+  (else_command)
+  "endif"
+  "endforeach"
+] @indent.branch @indent.end
+
+(experession_statement
+  object: (_)) @indent.begin
+
+(comment) @indent.auto
