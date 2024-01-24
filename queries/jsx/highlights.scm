bf5b25312fcb92792c08c18f1b3ed995b0b79ca4
Riley Bruins <ribru17@hotmail.com>
2024-01-24 10:47:26 -0800

    fix(jsx): proper tag and builtin tag distinction

--- queries/jsx/highlights.scm
+++ queries/jsx/highlights.scm
@@ -24,42 +24,42 @@
   (property_identifier) @tag.attribute)
 
 (jsx_opening_element
-  name: (identifier) @tag)
+  name: (identifier) @tag.builtin)
 
 (jsx_closing_element
-  name: (identifier) @tag)
+  name: (identifier) @tag.builtin)
 
 (jsx_self_closing_element
-  name: (identifier) @tag)
+  name: (identifier) @tag.builtin)
 
 (jsx_opening_element
-  ((identifier) @constructor
-    (#lua-match? @constructor "^[A-Z]")))
+  ((identifier) @tag
+    (#lua-match? @tag "^[A-Z]")))
 
 ; Handle the dot operator effectively - <My.Component>
 (jsx_opening_element
   (member_expression
-    (identifier) @tag
-    (property_identifier) @constructor))
+    (identifier) @tag.builtin
+    (property_identifier) @tag))
 
 (jsx_closing_element
-  ((identifier) @constructor
-    (#lua-match? @constructor "^[A-Z]")))
+  ((identifier) @tag
+    (#lua-match? @tag "^[A-Z]")))
 
 ; Handle the dot operator effectively - </My.Component>
 (jsx_closing_element
   (member_expression
-    (identifier) @tag
-    (property_identifier) @constructor))
+    (identifier) @tag.builtin
+    (property_identifier) @tag))
 
 (jsx_self_closing_element
-  ((identifier) @constructor
-    (#lua-match? @constructor "^[A-Z]")))
+  ((identifier) @tag
+    (#lua-match? @tag "^[A-Z]")))
 
 ; Handle the dot operator effectively - <My.Component />
 (jsx_self_closing_element
   (member_expression
-    (identifier) @tag
-    (property_identifier) @constructor))
+    (identifier) @tag.builtin
+    (property_identifier) @tag))
 
 (jsx_text) @none
