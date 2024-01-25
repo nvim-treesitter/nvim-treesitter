d2e4c67e96e02bfe4977ba3a48c54ce098c802e1
Amaan Qureshi <amaanq12@gmail.com>
2024-01-24 19:35:31 -0500

    fix: format disassembly

--- queries/disassembly/highlights.scm
+++ queries/disassembly/highlights.scm
@@ -8,11 +8,23 @@
 
 (identifier) @variable
 
-(bad_instruction) @text.warning
-(code_location (identifier) @function.call)
+(bad_instruction) @comment.warning
+
+(code_location
+  (identifier) @function.call)
+
 (comment) @comment
+
 (instruction) @function
+
 (memory_dump) @string
 
-["<" ">"] @punctuation.special
-["+" ":"] @punctuation.delimiter
+[
+  "<"
+  ">"
+] @punctuation.special
+
+[
+  "+"
+  ":"
+] @punctuation.delimiter
