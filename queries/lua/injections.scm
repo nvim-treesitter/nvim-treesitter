a7fe3ec9fa7c9e50b1bbc51630ad9243adc353f4
Amaan Qureshi <amaanq12@gmail.com>
2024-01-26 00:29:52 -0500

    fix(lua): inject luadoc into comments immediately beginning with a `|`

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -131,7 +131,7 @@
 
 (comment
   content: (_) @injection.content
-  (#lua-match? @injection.content "^[-][%s]*@")
+  (#lua-match? @injection.content "^[-][%s]*[@|]")
   (#set! injection.language "luadoc")
   (#offset! @injection.content 0 1 0 0))
 
