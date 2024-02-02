1cda98132abfde758c1778096960f9b2c0bd78c1
Ilia Ilinykh <18665585+IlyasYOY@users.noreply.github.com>
2024-02-02 22:00:03 +0300

    feat(java): inject printf in String.formatted (#6045)
    https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/String.html#formatted(java.lang.Object...)

--- queries/java/injections.scm
+++ queries/java/injections.scm
@@ -18,3 +18,11 @@
         (_) @injection.content)))
   (#any-of? @_method "format" "printf")
   (#set! injection.language "printf"))
+
+((method_invocation
+  object:
+    (string_literal
+      (string_fragment) @injection.content)
+  name: (identifier) @_method)
+  (#eq? @_method "formatted")
+  (#set! injection.language "printf"))
