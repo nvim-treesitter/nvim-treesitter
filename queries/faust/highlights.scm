4ea835933f79b3e99f35d2c19d627a2139b81d6d
Karl Hiner <karl.hiner@gmail.com>
2024-02-03 12:33:15 -0500

    feat(faust): Add back queries for documentation tags

--- queries/faust/highlights.scm
+++ queries/faust/highlights.scm
@@ -201,3 +201,24 @@
 
 ; Comments
 (comment) @comment @spell
+
+; Tags
+(documentation
+  [
+    "<mdoc>"
+    "</mdoc>"
+  ] @tag)
+
+(documentation
+  (_
+    [
+      "<metadata>"
+      "</metadata>"
+      "<equation>"
+      "</equation>"
+      "<diagram>"
+      "</diagram>"
+      "<listing"
+      "<notice"
+      "/>"
+    ] @tag))
