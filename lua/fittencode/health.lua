f9a57f7af785f7491bfdaa8eb622495ce03a5d7a
luozhiya <luozhiya@petalmail.com>
2024-02-06 10:30:21 +0800

    Improve :checkhealth

--- lua/fittencode/health.lua
+++ lua/fittencode/health.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 local M = {}
 
 function M.check()
-  vim.health.start('fittencode.nvim')
+  vim.health.start('Installation')
 
   if fn.executable('curl') == 1 then
     local handle = io.popen('curl --version')
     local result = handle:read('*a')
     handle:close()
     local version = vim.split(result, '\n')[1]
-    vim.health.ok('cURL is installed, version: ' .. (version or 'unknown'))
+    vim.health.ok('`cURL` is installed, version: ' .. (version or 'unknown'))
   else
-    vim.health.error('cURL is not installed')
+    vim.health.error('`cURL` is not installed')
   end
 end
 
