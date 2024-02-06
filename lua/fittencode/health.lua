e320ca7b4c3e67297f23b1dcddef0b66026baf2c
luozhiya <luozhiya@petalmail.com>
2024-02-06 10:21:25 +0800

    Support :checkhealth

---
+++ lua/fittencode/health.lua
@@ -0,0 +1,19 @@
+local fn = vim.fn
+
+local M = {}
+
+function M.check()
+  vim.health.start('fittencode.nvim')
+
+  if fn.executable('curl') == 1 then
+    local handle = io.popen('curl --version')
+    local result = handle:read('*a')
+    handle:close()
+    local version = vim.split(result, '\n')[1]
+    vim.health.ok('cURL is installed, version: ' .. (version or 'unknown'))
+  else
+    vim.health.error('cURL is not installed')
+  end
+end
+
+return M
