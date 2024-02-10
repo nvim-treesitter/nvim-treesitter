467090f504b83da94717037a3e631f6e0d8a3131
dundargoc <gocdundar@gmail.com>
2024-02-10 15:33:57 +0100

    ci: fix invalid workflow
    Context variables aren't always available, so we can't rely on them for
    e.g. environment variables.
    
    Also merge all install scripts into `script/ci-install.sh`, that way it
    can be called the same way regardless of system.

--- scripts/ci-install-windows-2022.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-curl -L https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-win64.zip -o nvim-win64.zip
-unzip nvim-win64
-mkdir -p ~/AppData/Local/nvim/pack/nvim-treesitter/start
-mkdir -p ~/AppData/Local/nvim-data
-cp -r "$PWD" ~/AppData/Local/nvim/pack/nvim-treesitter/start
