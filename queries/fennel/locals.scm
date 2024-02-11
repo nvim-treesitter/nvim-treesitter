3009fef955b314a5e68d51ca30bd981864b6ba8f
Alexei Mozaidze <lexerpexer@proton.me>
2024-02-11 17:15:37 +0400

    feat!: change Fennel parser
    This commit changes the Fennel parser to a simpler and more consistent one, along with updating all the queries.

--- queries/fennel/locals.scm
+++ queries/fennel/locals.scm
@@ -1,32 +1,26 @@
-[
-  (program)
-  (fn)
-  (lambda)
-  (let)
-  (each)
-  (for)
-  (match)
-] @local.scope
+; TODO: Add tests
+; TODO: Improve queries
+(program) @local.scope
 
 ((list
   .
-  (symbol) @_special) @local.scope
-  (#any-of? @_special "while" "if" "when" "do" "collect" "icollect" "accumulate"))
-
-(fn
-  name: (symbol) @local.definition.function
-  (#set! definition.function.scope "parent"))
-
-(lambda
-  name: (symbol) @local.definition.function
-  (#set! definition.function.scope "parent"))
-
-; TODO: use @local.definition.parameter for parameters
-(binding
-  (symbol) @local.definition.var)
-
-(for_clause
-  .
-  (symbol) @local.definition.var)
+  (symbol) @_call) @local.scope
+  (#any-of? @_call "let" "fn" "lambda" "λ" "while" "each" "for" "if" "when" "do" "collect" "icollect" "accumulate" "case" "match"))
 
 (symbol) @local.reference
+
+(list
+  .
+  (symbol) @_fn
+  (#any-of? @_fn "fn" "lambda" "λ")
+  .
+  [
+    (symbol) @local.definition.function
+    (multi_symbol
+      member: (symbol_fragment) @local.definition.function .)
+  ]
+  (#set! definition.function.scope "parent")
+  .
+  (sequence
+    (symbol)* @local.definition.parameter
+    (#not-contains? @local.definition.parameter "&")))
