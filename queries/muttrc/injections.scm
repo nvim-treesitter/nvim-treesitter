bf5d3cb2b2aa1efdf622055dd981797e8d71fcf3
Wu Zhenyu <wuzhenyu@ustc.edu>
2024-02-14 10:15:10 +0800

    feat: add the muttrc parser

---
+++ queries/muttrc/injections.scm
@@ -0,0 +1,8 @@
+((regex) @injection.content
+  (#set! injection.language "regex"))
+
+((shell) @injection.content
+  (#set! injection.language "bash"))
+
+((comment) @injection.content
+  (#set! injection.language "comment"))
