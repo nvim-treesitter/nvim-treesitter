16fbfcf178e07801f8ad1ea2807056b760f357c3
luozhiya <luozhiya@petalmail.com>
2024-02-16 15:47:33 +0800

    Refactor: Base type annotations

--- lua/fittencode/base.lua
+++ lua/fittencode/base.lua
@@ -4,6 +4,9 @@ local uv = vim.uv
 
 local M = {}
 
+---@param name string
+---@param func function
+---@param opts table|nil
 function M.command(name, func, opts)
   opts = opts or {}
   if type(opts) == 'string' then
@@ -12,6 +15,10 @@ function M.command(name, func, opts)
   api.nvim_create_user_command(name, func, opts)
 end
 
+---@param mode string
+---@param lhs string
+---@param rhs string|function
+---@param opts table|nil
 function M.map(mode, lhs, rhs, opts)
   opts = opts or {}
   if type(opts) == 'string' then
@@ -23,21 +30,26 @@ function M.map(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
+---@param chars string
 function M.feedkeys(chars)
   local keys = api.nvim_replace_termcodes(chars, true, false, true)
   api.nvim_feedkeys(keys, 'in', true)
 end
 
+---@param name string
 function M.augroup(name)
   return api.nvim_create_augroup('Fittencode_' .. name, { clear = true })
 end
 
+---@param name string
+---@param hi table
 function M.set_hi(name, hi)
   hi.force = true
   hi.cterm = hi.cterm or {}
   vim.api.nvim_set_hl(0, name, hi)
 end
 
+---@return integer, integer
 function M.get_cursor()
   local cursor = api.nvim_win_get_cursor(0)
   local row = cursor[1] - 1
@@ -45,7 +57,11 @@ function M.get_cursor()
   return row, col
 end
 
-function M.debounce(timer, callback, wait)
+---@param timer uv_timer_t|nil
+---@param callback function
+---@param wait number
+---@param on_error function|nil
+function M.debounce(timer, callback, wait, on_error)
   local function destroy_timer()
     if timer then
       if timer:has_ref() then
@@ -59,6 +75,12 @@ function M.debounce(timer, callback, wait)
   end
   if not timer then
     timer = uv.new_timer()
+    if timer == nil then
+      if on_error then
+        on_error()
+      end
+      return
+    end
     timer:start(
       wait,
       0,
@@ -72,6 +94,9 @@ function M.debounce(timer, callback, wait)
   end
 end
 
+---@param data string
+---@param path string
+---@param callback function|nil
 function M.write(data, path, callback)
   uv.fs_open(path, 'w', 438, function(_, fd)
     if fd ~= nil then
@@ -87,16 +112,31 @@ function M.write(data, path, callback)
   end)
 end
 
+---@param data string
+---@param dir string
+---@param path string
+---@param callback function|nil
 function M.write_mkdir(data, dir, path, callback)
   uv.fs_mkdir(dir, 448, function(_, _)
     M.write(data, path, callback)
   end)
 end
 
-function M.write_temp_file(data, callback)
-  M.write(data, fn.tempname(), callback)
+---@param data string
+---@param callback function|nil
+function M.write_temp_file(data, callback, on_error)
+  local path = fn.tempname()
+  if path then
+    M.write(data, path, callback)
+  else
+    if on_error then
+      on_error()
+    end
+  end
 end
 
+---@param path string
+---@param callback function|nil
 function M.read(path, callback)
   uv.fs_open(path, 'r', 438, function(_, fd)
     if fd ~= nil then
@@ -116,53 +156,88 @@ function M.read(path, callback)
   end)
 end
 
+---@param file string
+---@return boolean
 function M.exists(file)
   return uv.fs_stat(file) ~= nil
 end
 
+---@return string
 function M.nt_sep()
   return '\\'
 end
 
+---@return string
 function M.kernel_sep()
   return '/'
 end
 
+---@return boolean
 function M.is_windows()
   return uv.os_uname().sysname == 'Windows_NT'
 end
 
+---@return boolean
 function M.is_kernel()
   return uv.os_uname().sysname == 'Linux'
 end
 
+---@param s string
+---@return string
 function M.to_nt(s)
-  return s:gsub(M.kernel_sep(), M.nt_sep())
+  local nt, _ = s:gsub(M.kernel_sep(), M.nt_sep())
+  return nt
 end
 
+---@param s string
+---@return string
 function M.to_kernel(s)
-  return s:gsub(M.nt_sep(), M.kernel_sep())
+  local kernel, _ = s:gsub(M.nt_sep(), M.kernel_sep())
+  return kernel
 end
 
+---@param s string
+---@return string
 function M.to_native(s)
   return M.is_windows() and M.to_nt(s) or M.to_kernel(s)
 end
 
+---@param char string
+---@return boolean
 function M.is_alpha(char)
+  ---@type number
   local byte = char:byte()
   return (byte >= 65 and byte <= 90) or (byte >= 97 and byte <= 122)
 end
 
+---@param char string
+---@return boolean
 function M.is_space(char)
   local byte = string.byte(char)
   return byte == 32 or byte == 9
 end
 
--- NVIM v0.10.0-dev-2315+g32b49448b
--- Build type: RelWithDebInfo
--- LuaJIT 2.1.1707061634
+---@class NeovimVersion
+---@field nvim string
+---@field buildtype string
+---@field luajit string
+
+-- Original `version` output:
+--   NVIM v0.10.0-dev-2315+g32b49448b
+--   Build type: RelWithDebInfo
+--   LuaJIT 2.1.1707061634
+-- Strucutred as:
+--   {
+--     nvim = '0.10.0-dev-2315+g32b49448b',
+--     buildtype = 'RelWithDebInfo',
+--     luajit = '2.1.1707061634',
+--   }
+---@return NeovimVersion|nil
 function M.get_version()
   local version = fn.execute('version')
+  if not version then
+    return nil
+  end
 
   local function find_part(offset, part)
     local start = version:find(part, offset)
