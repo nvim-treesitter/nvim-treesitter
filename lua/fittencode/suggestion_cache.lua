453475a00392d324d0cf3c697bc869dfe0a3a88c
luozhiya <luozhiya@petalmail.com>
2024-02-16 15:49:06 +0800

    Refactor: SuggestionCache type annotations

--- lua/fittencode/suggestion_cache.lua
+++ lua/fittencode/suggestion_cache.lua
@@ -10,7 +10,7 @@ local Log = require('fittencode.log')
 ---@field count? integer
 local SuggestionCache = {}
 
-function SuggestionCache.new(view)
+function SuggestionCache.new()
   local self = setmetatable({}, { __index = SuggestionCache })
   self.lines = {}
   self.pos = {}
@@ -23,20 +23,25 @@ function SuggestionCache:flush()
   self:update_pos()
 end
 
+---@param lines string[]|nil
 function SuggestionCache:update_lines(lines)
   lines = lines or {}
   -- Log.debug("SuggestionCache:update_lines; lines: {}", lines)
-
   self.lines = lines
   self.count = vim.tbl_count(lines)
 end
 
+---@param row integer|nil
+---@param col integer|nil
 function SuggestionCache:update_pos(row, col)
   -- Log.debug("SuggestionCache:update_pos; row: {}, col: {}", row, col)
   self.pos.row = row
   self.pos.col = col
 end
 
+---@param row integer
+---@param col integer
+---@return boolean
 function SuggestionCache:equal_pos(row, col)
   return self.pos.row == row and self.pos.col == col
 end
