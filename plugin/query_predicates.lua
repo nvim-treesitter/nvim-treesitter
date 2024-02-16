9c6428bf5b172b95be426045ce07081038584ea3
Gregory Anders <greg@gpanders.com>
2024-02-15 19:48:48 -0600

    fix: update add_predicate and add_directive calls for upstream
    Update custom predicates and directives to handle multiple nodes per
    capture ID per changes upstream.
    
    Ref: https://github.com/neovim/neovim/pull/24738

--- plugin/query_predicates.lua
+++ plugin/query_predicates.lua
@@ -1,19 +1,59 @@
 local query = vim.treesitter.query
 
+local predicates = {
+  ---@param nodes TSNode[]
+  ---@param pred string[]
+  ---@param any boolean
+  ---@return boolean
+  ['has-type'] = function(nodes, pred, any)
+    if not nodes or #nodes == 0 then
+      return true
+    end
+
+    local types = { unpack(pred, 3) }
+    for _, node in ipairs(nodes) do
+      local match = vim.list_contains(types, node:type())
+      if any and match then
+        return true
+      elseif not any and not match then
+        return false
+      end
+    end
+    return not any
+  end,
+}
+
 -- register custom predicates (overwrite existing; needed for CI)
 
----@param match (TSNode|nil)[]
+---@param match TSMatch
 ---@param pred string[]
 ---@return boolean|nil
 query.add_predicate('has-type?', function(match, _, _, pred)
-  local node = match[pred[2]]
-  if not node then
-    return true
+  local nodes = match[pred[2]]
+  if type(nodes) ~= 'table' then
+    -- Check for old versions of Nvim that map capture IDs to a single node
+    nodes = { nodes }
   end
+  return predicates['has-type'](nodes, pred, false)
+end, {
+  force = true,
+  correct = true,
+})
 
-  local types = { unpack(pred, 3) }
-  return vim.list_contains(types, node:type())
-end, true)
+---@param match TSMatch
+---@param pred string[]
+---@return boolean|nil
+query.add_predicate('any-has-type?', function(match, _, _, pred)
+  local nodes = match[pred[2]]
+  if type(nodes) ~= 'table' then
+    -- Check for old versions of Nvim that map capture IDs to a single node
+    nodes = { nodes }
+  end
+  return predicates['has-type'](nodes, pred, true)
+end, {
+  force = true,
+  correct = true,
+})
 
 -- register custom directives
 
@@ -31,11 +71,17 @@ local mimetype_aliases = {
 ---@return boolean|nil
 query.add_directive('set-lang-from-mimetype!', function(match, _, bufnr, pred, metadata)
   local id = pred[2]
-  local node = match[id]
-  if not node then
+  local nodes = match[id]
+  if type(nodes) ~= 'table' then
+    nodes = { nodes }
+  end
+
+  if not nodes or #nodes == 0 then
     return
   end
 
+  assert(#nodes == 1, '#set-lang-from-mimetype! does not support captures on multiple nodes')
+  local node = nodes[1]
   local type_attr_value = vim.treesitter.get_node_text(node, bufnr, { metadata = metadata[id] })
   local configured = mimetype_aliases[type_attr_value]
   if configured then
@@ -44,4 +90,7 @@ query.add_directive('set-lang-from-mimetype!', function(match, _, bufnr, pred, m
     local parts = vim.split(type_attr_value, '/', {})
     metadata['injection.language'] = parts[#parts]
   end
-end, true)
+end, {
+  force = true,
+  correct = true,
+})
