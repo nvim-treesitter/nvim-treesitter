4c6c458ee63dc76f3dc83ed4b302b174d7ec652a
luozhiya <luozhiya@petalmail.com>
2024-02-16 15:48:39 +0800

    Refactor: LSP type annotations

--- lua/fittencode/lsp.lua
+++ lua/fittencode/lsp.lua
@@ -1,12 +1,13 @@
 local M = {}
 
-local Log = require('fittencode.log')
-
+---@return boolean
 function M.is_active()
+  ---@type boolean, any
   local ok, cmp = pcall(require, 'cmp')
   if not ok then
     return false
   end
+  ---@type table
   local entries = cmp.get_entries()
   return vim.tbl_count(entries) > 0
 end
