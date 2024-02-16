2d708bc580a9b299784d30b14e21c061380be638
luozhiya <luozhiya@petalmail.com>
2024-02-16 15:48:17 +0800

    Refactor: Health type annotations

--- lua/fittencode/health.lua
+++ lua/fittencode/health.lua
@@ -7,10 +7,14 @@ function M.check()
 
   if fn.executable('curl') == 1 then
     local handle = io.popen('curl --version')
-    local result = handle:read('*a')
-    handle:close()
-    local version = vim.split(result, '\n')[1]
-    vim.health.ok('`cURL` is installed, version: ' .. (version or 'unknown'))
+    if handle then
+      local result = handle:read('*a')
+      handle:close()
+      local version = vim.split(result, '\n')[1]
+      vim.health.ok('`cURL` is installed, version: ' .. (version or 'unknown'))
+    else
+      vim.health.error('`cURL` is installed but cannot be executed')
+    end
   else
     vim.health.error('`cURL` is not installed')
   end
