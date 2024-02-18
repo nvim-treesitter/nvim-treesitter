d597cf044ef1256e112f5e87afb7c9ad05071198
luozhiya <luozhiya@petalmail.com>
2024-02-18 23:05:36 +0800

    Add SuggestionCache comment

--- lua/fittencode/suggestion_cache.lua
+++ lua/fittencode/suggestion_cache.lua
@@ -3,9 +3,9 @@
 ---@field col? integer
 
 ---@class SuggestionCache
----@field lines? string[]
----@field pos? SuggestionPos
----@field count? integer
+---@field lines? string[] @suggestion lines
+---@field pos? SuggestionPos @suggestion position
+---@field count? integer @suggestion count
 local SuggestionCache = {}
 
 function SuggestionCache.new()
@@ -16,27 +16,31 @@ function SuggestionCache.new()
   return self
 end
 
+-- Flush cache
 function SuggestionCache:flush()
   self:update_lines()
   self:update_pos()
 end
 
----@param lines string[]|nil
+-- Update suggestion lines
+---@param lines string[]|nil @suggestion lines
 function SuggestionCache:update_lines(lines)
   lines = lines or {}
   self.lines = lines
   self.count = vim.tbl_count(lines)
 end
 
----@param row integer|nil
----@param col integer|nil
+-- Update suggestion position
+---@param row integer|nil @suggestion row
+---@param col integer|nil @suggestion col
 function SuggestionCache:update_pos(row, col)
   self.pos.row = row
   self.pos.col = col
 end
 
----@param row integer
----@param col integer
+-- Check if the position is equal to the cached position
+---@param row integer @suggestion row
+---@param col integer @suggestion col
 ---@return boolean
 function SuggestionCache:equal_pos(row, col)
   return self.pos.row == row and self.pos.col == col
