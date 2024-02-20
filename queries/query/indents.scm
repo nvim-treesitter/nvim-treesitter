2cdc2d018b331fcc1e206a8080689d9faac5c4de
Riley Bruins <ribru17@hotmail.com>
2024-02-20 10:36:42 -0800

    feat(query): indents that reflect the formatter

--- queries/query/indents.scm
+++ queries/query/indents.scm
@@ -1,8 +1,18 @@
-(list) @indent.begin
-
 [
+  (list)
+  (named_node)
+  (grouping)
   "["
-  "]"
 ] @indent.begin
 
 "]" @indent.branch
+
+")" @indent.end
+
+((list
+  (capture) @indent.dedent) @_list
+  (#not-has-parent? @_list grouping))
+
+(program
+  (named_node
+    (capture) @indent.dedent .))
