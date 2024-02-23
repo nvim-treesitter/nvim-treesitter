dd19be86ab7a4456f1687c24b984cd1acdde6df4
Alexei Mozaidze <lexerpexer@proton.me>
2024-02-23 18:27:21 +0400

    feat(fennel): in-depth form support and fixes
    Changes:
    1. Added proper function highlighting (it was semi-broken before)
    2. Added `call:` field to lists's first value
    3. Added `:` operator
    4. Highlighted `macro` and `macrodebug` as @keyword
    5. Added compiler-scoped forms
    6. Added proper destructuring support for variable forms (`let`, `local`) and
       table comprehension forms (`icollect`, `accumulate`)
    7. Added support for `comment` form

--- queries/fennel/highlights.scm
+++ queries/fennel/highlights.scm
@@ -29,13 +29,12 @@
   member: (symbol_fragment) @variable.member)
 
 (list
-  .
-  (symbol) @function.call)
+  call: (symbol) @function.call)
 
 (list
-  .
-  (multi_symbol
-    member: (symbol_fragment) @function.call .))
+  call:
+    (multi_symbol
+      member: (symbol_fragment) @function.call .))
 
 (multi_symbol_method
   ":" @punctuation.delimiter
@@ -61,7 +60,7 @@
     ; comparison
     ">" "<" ">=" "<=" "=" "~="
     ; other
-    "#" "." "?." ".."))
+    "#" ":" "." "?." ".."))
 
 ((symbol) @keyword.operator
   (#any-of? @keyword.operator
@@ -84,12 +83,12 @@
   (#any-of? @keyword.repeat "for" "each" "while"))
 
 ((symbol) @keyword.conditional
-  (#any-of? @keyword.conditional "if" "when" "match" "case"))
+  (#any-of? @keyword.conditional "if" "when" "case" "case-try" "match" "match-try"))
 
 ((symbol) @keyword
   (#any-of? @keyword
-    "global" "local" "let" "set" "var" "comment" "do" "doc" "eval-compiler" "lua" "macros" "unquote"
-    "quote" "tset" "values" "tail!"))
+    "global" "local" "let" "set" "var" "comment" "do" "doc" "eval-compiler" "lua" "macro" "macros"
+    "macrodebug" "unquote" "quote" "tset" "values" "tail!"))
 
 ((symbol) @keyword.import
   (#any-of? @keyword.import "require" "require-macros" "import-macros" "include"))
@@ -97,7 +96,10 @@
 ((symbol) @function.macro
   (#any-of? @function.macro
     "collect" "icollect" "fcollect" "accumulate" "faccumulate" "->" "->>" "-?>" "-?>>" "?." "doto"
-    "macro" "macrodebug" "partial" "pick-args" "pick-values" "with-open"))
+    "partial" "pick-args" "pick-values" "with-open"
+    ; compiler scope
+    "list" "sym" "gensym" "list?" "sym?" "table?" "sequence?" "varg?" "multi-sym?" "comment?" "view"
+    "get-scope" "assert-compile" "in-scope?" "macroexpand"))
 
 ; TODO: Highlight builtin methods (`table.unpack`, etc) as @function.builtin
 ([
@@ -130,28 +132,355 @@
 
 (table
   (table_pair
+    key: (symbol) @punctuation.special
+    (#eq? @punctuation.special ":")))
+
+(table
+  (table_pair
     key: (symbol) @keyword.directive
     (#eq? @keyword.directive "&as")))
 
+; List destructuring support for `let`
 (list
+  call: (symbol) @keyword
+  (#eq? @keyword "let")
   .
-  (symbol) @keyword.function
+  (comment)*
+  .
+  (sequence
+    .
+    ((comment)*
+      .
+      item:
+        [
+          (_)
+          (list
+            call: (symbol) @variable)
+        ].
+      (comment)*
+      .
+      item: (_).
+      (comment)*)*))
+
+; List destructuring support for "variable forms"
+(list
+  call: (symbol) @keyword
+  (#any-of? @keyword "local" "var" "set" "global")
+  .
+  (comment)*
+  .
+  (list
+    call: (symbol) @variable))
+
+(list
+  call: (symbol) @keyword.import
+  (#eq? @keyword.import "import-macros")
+  .
+  (comment)*
+  .
+  (table
+    (table_pair
+      value: (symbol) @function.macro)))
+
+(list
+  call: (symbol) @keyword
+  (#eq? @keyword "macro")
+  .
+  (comment)*
+  .
+  (symbol) @function.macro
+  .
+  (comment)*
+  .
+  [
+    (sequence
+      !item)
+    (sequence
+      [
+        (symbol) @variable.parameter
+        ((symbol) @punctuation.special
+          (#eq? @punctuation.special "&"))
+        ((symbol) @variable.builtin
+          (#eq? @variable.builtin "..."))
+      ])
+  ])
+
+(list
+  call: (symbol) @keyword
+  (#eq? @keyword "macros")
+  .
+  (comment)*
+  .
+  (table
+    (table_pair
+      key:
+        (string
+          ":"? @function.macro
+          (string_content) @function.macro))))
+
+; Function definition (without metadata/docstring)
+(list
+  call: (symbol) @keyword.function
   (#any-of? @keyword.function "fn" "lambda" "λ")
+  .
+  (comment)*
   .
   [
     (symbol) @function
     (multi_symbol
       (symbol_fragment) @function .)
+  ]?
+  .
+  (comment)*
+  .
+  [
+    (sequence
+      !item)
+    (sequence
+      [
+        (symbol) @variable.parameter
+        ((symbol) @punctuation.special
+          (#eq? @punctuation.special "&"))
+        ((symbol) @variable.builtin
+          (#eq? @variable.builtin "..."))
+      ])
+  ])
+
+; Function definition with docstring
+(list
+  call: (symbol) @keyword.function
+  (#any-of? @keyword.function "fn" "lambda" "λ")
+  .
+  (comment)*
+  .
+  [
+    (symbol)
+    (multi_symbol
+      (symbol_fragment) .)
+  ]?
+  .
+  (comment)*
+  .
+  [
+    (sequence
+      !item)
+    (sequence
+      (symbol))
   ]
   .
+  (comment)*
+  .
+  ((string) @string.documentation
+    .
+    (comment)*
+    .
+    item: (_))?)
+
+; Function definition with table metadata
+(list
+  call: (symbol) @keyword.function
+  (#any-of? @keyword.function "fn" "lambda" "λ")
+  .
+  (comment)*
+  .
+  [
+    (symbol)
+    (multi_symbol
+      (symbol_fragment) .)
+  ]?
+  .
+  (comment)*
+  .
+  [
+    (sequence
+      !item)
+    (sequence
+      (symbol))
+  ]
+  .
+  (comment)*
+  .
+  ((string)?
+    .
+    (comment)*
+    .
+    (table
+      (table_pair
+        key:
+          (string
+            (string_content) @string
+            (#eq? @string "fnl/docstring"))
+        value: (string) @string.documentation))
+    .
+    (comment)*
+    .
+    item: (_))?)
+
+; Function definition with table arglist metadata
+(list
+  call: (symbol) @keyword.function
+  (#any-of? @keyword.function "fn" "lambda" "λ")
+  .
+  (comment)*
+  .
+  [
+    (symbol)
+    (multi_symbol
+      (symbol_fragment) .)
+  ]?
+  .
+  (comment)*
+  .
+  [
+    (sequence
+      !item)
+    (sequence
+      (symbol))
+  ]
+  .
+  (comment)*
+  .
+  ((string)?
+    .
+    (comment)*
+    .
+    (table
+      (table_pair
+        key:
+          (string
+            (string_content) @string
+            (#eq? @string "fnl/arglist"))
+        value:
+          [
+            (sequence
+              !item)
+            (sequence
+              [
+                (symbol) @variable.parameter
+                ((symbol) @punctuation.special
+                  (#eq? @punctuation.special "&"))
+                ((symbol) @variable.builtin
+                  (#eq? @variable.builtin "..."))
+              ])
+          ]))
+    .
+    (comment)*
+    .
+    item: (_))?)
+
+; Table comprehension macros destructuring support
+(list
+  call:
+    [
+      ((symbol) @function.macro
+        (#any-of? @function.macro "collect" "icollect"))
+      ((symbol) @keyword.repeat
+        (#eq? @keyword.repeat "each"))
+    ]
+  .
+  (comment)*
+  .
   (sequence
-    ((symbol) @variable.parameter
-      (#not-any-of? @variable.parameter "&" "..."))))
+    .
+    (comment)*
+    .
+    [
+      (comment)
+      (list
+        call: (_) @variable)
+      (sequence)
+      (table)
+      ((symbol) @variable
+        (#lua-match? @variable "^[^&]"))
+    ]*
+    .
+    [
+      (list)
+      (sequence)
+      (table)
+      ((symbol) @_variable
+        (#lua-match? @_variable "^[^&]"))
+    ]
+    .
+    (comment)*
+    .
+    [
+      (string)
+      (symbol)
+    ]? @_option
+    (#lua-match? @_option "^[&:]")))
 
 (list
+  call: (symbol) @function.macro
+  (#eq? @function.macro "accumulate")
   .
-  (symbol) @function.macro
+  (comment)*
+  .
+  (sequence
+    .
+    (comment)*
+    .
+    (symbol)
+    .
+    (comment)*
+    .
+    ; HACK: Just (_) won't work since it will also capture (comment)
+    ; TODO: Remove `item:` once tree-sitter/tree-sitter#1454 is resolved
+    item: (_)
+    .
+    (comment)*
+    .
+    [
+      (comment)
+      (list
+        call: (_) @variable)
+      (sequence)
+      (table)
+      ((symbol) @variable
+        (#lua-match? @variable "^[^&]"))
+    ]*
+    .
+    [
+      (list)
+      (sequence)
+      (table)
+      ((symbol) @_variable
+        (#lua-match? @_variable "^[^&]"))
+    ]
+    .
+    (comment)*
+    .
+    [
+      (string)
+      (symbol)
+    ]? @_option
+    (#lua-match? @_option "^[&:]")))
+
+; Iterator &until option
+(list
+  call:
+    [
+      ((symbol) @function.macro
+        (#any-of? @function.macro "collect" "icollect" "fcollect" "accumulate" "faccumulate"))
+      ((symbol) @keyword.repeat
+        (#any-of? @keyword.repeat "for" "each"))
+    ]
+  .
+  (comment)*
+  .
+  (sequence
+    [
+      (symbol)
+      (string)
+    ] @keyword.directive
+    (#any-of? @keyword.directive "&until" ":until")))
+
+; Iterator &into option
+(list
+  call: (symbol) @function.macro
   (#any-of? @function.macro "collect" "icollect" "fcollect")
+  .
+  (comment)*
   .
   (sequence
     [
@@ -161,34 +490,119 @@
     (#any-of? @keyword.directive "&into" ":into")))
 
 (list
+  call: (symbol) @keyword.conditional
+  (#any-of? @keyword.conditional "case" "match")
   .
-  (symbol) @function.macro
-  (#any-of? @function.macro "for" "each" "collect" "icollect" "fcollect" "accumulate" "faccumulate")
+  (comment)*
   .
-  (sequence
-    [
-      (symbol)
-      (string)
-    ] @keyword.directive
-    (#any-of? @keyword.directive "&until" ":until")))
-
-(list
+  item: (_)
   .
-  (symbol) @keyword.conditional
-  (#any-of? @keyword.conditional "match" "case")
-  .
-  (_)
-  .
-  ((list
-    .
-    (symbol) @keyword
-    (#eq? @keyword "where"))
-    .
-    (_))*
+  [
+    (comment)
+    ((list
+      call:
+        [
+          (symbol) @variable
+          ((symbol) @keyword
+            (#eq? @keyword "where"))
+        ])
+      .
+      (comment)*
+      .
+      item: (_))
+  ]*
   .
   (list
-    .
-    (symbol) @keyword
-    (#eq? @keyword "where"))
+    call:
+      [
+        (symbol) @variable
+        ((symbol) @keyword
+          (#eq? @keyword "where"))
+      ]))
+
+; Same as above, but for `-try` variants
+(list
+  call: (symbol) @keyword.conditional
+  (#any-of? @keyword.conditional "case-try" "match-try")
   .
-  (_)? .)
+  (comment)*
+  .
+  item: (_)
+  .
+  [
+    (comment)
+    ((list
+      call:
+        [
+          (symbol) @variable
+          ((symbol) @keyword
+            (#eq? @keyword "where"))
+        ])
+      .
+      (comment)*
+      .
+      item: (_))
+  ]*
+  .
+  (list
+    call:
+      [
+        (symbol) @variable
+        ((symbol) @keyword
+          (#eq? @keyword "where"))
+      ]))
+
+; Catch block of `case-try` and `match-try`
+(list
+  call: (symbol) @keyword.conditional
+  (#any-of? @keyword.conditional "case-try" "match-try")
+  .
+  (comment)*
+  .
+  item: (_)
+  ; NOTE: I am not sure wether `catch` can appear anywhere or just at the end
+  (list
+    call: (symbol) @keyword @_catch
+    (#eq? @_catch "catch")
+    .
+    [
+      (comment)
+      ((list
+        call:
+          [
+            (symbol) @variable
+            ((symbol) @_where
+              (#eq? @_where "where"))
+          ])
+        .
+        (comment)*
+        .
+        item: (_))
+    ]*
+    .
+    (list
+      call:
+        [
+          (symbol) @variable
+          ((symbol) @keyword @_where
+            (#eq? @_where "where"))
+        ])?)
+  .
+  (comment)* .)
+
+((list
+  call: (symbol) @keyword
+  (#eq? @keyword "comment")) @comment
+  ; BUG: Offset for an unclosed list errors on Nightly *and* Stable:
+  ; Invalid 'end_col': out of range
+  ;
+  ; With offset, parentheses are colored properly, without it, they're part
+  ; of the comment (which is incorrect, but ok for now)
+  ;
+  ; (#offset! @comment 0 1 0 -1)
+  (#set! "priority" 101))
+
+(list
+  call: (symbol) @keyword
+  (#eq? @keyword "comment")
+  (#set! "priority" 102))
