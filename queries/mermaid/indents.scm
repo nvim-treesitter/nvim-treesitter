37760e8a54538b8f79a389665f04357522f0f7ab
Avi Areman <avi.areman@gmail.com>
2024-02-23 14:29:29 -0500

    Add missing chart types and format/lint

--- queries/mermaid/indents.scm
+++ queries/mermaid/indents.scm
@@ -1,7 +1,14 @@
-[
+([
+  (diagram_sequence)
+  (diagram_class)
+  (diagram_state)
+  (diagram_gantt)
+  (diagram_pie)
+  (diagram_er)
   (diagram_flow)
   (flow_stmt_subgraph)
 ] @indent.begin
+(#set! indent.immediate 1))
 
 [
   "end"
