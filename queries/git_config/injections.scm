36f40197ffd71760416c77e1150d8881e242641e
Wu Zhenyu <wuzhenyu@ustc.edu>
2024-02-29 23:17:19 +0800

    feat: add some injections to gitconfig

--- queries/git_config/injections.scm
+++ queries/git_config/injections.scm
@@ -4,7 +4,46 @@
 ((variable
   (name) @_name
   value: (string) @injection.content)
-  (#any-of? @_name "cmd" "command" "textconv")
+  (#any-of? @_name "cmd" "command" "textconv" "sendmailCmd")
+  (#set! injection.language "bash"))
+
+(section
+  (variable
+    (name) @_name
+    value: (string) @injection.content)
+  (#eq? @_name "tool")
+  (#set! injection.language "bash"))
+
+(section
+  (section_header
+    (section_name) @_pager)
+  (variable
+    value: (string) @injection.content)
+  (#eq? @_pager "pager")
+  (#set! injection.language "bash"))
+
+(section
+  (section_header
+    (section_name) @_interactive)
+  (variable
+    (name) @_name
+    value: (string) @injection.content)
+  (#eq? @_interactive "interactive")
+  (#eq? @_name "diffFilter")
+  (#set! injection.language "bash"))
+
+; https://github.com/git-lfs/git-lfs
+; git lfs install
+(section
+  (section_header
+    (section_name) @_filter
+    (subsection_name) @_lfs)
+  (variable
+    (name) @_name
+    value: (string) @injection.content)
+  (#eq? @_filter "filter")
+  (#eq? @_lfs "lfs")
+  (#any-of? @_name "smudge" "process" "clean")
   (#set! injection.language "bash"))
 
 (section
