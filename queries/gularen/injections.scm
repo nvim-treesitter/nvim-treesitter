6f805eb95677c6e8cc9bffd21ae63fa5f66008a5
Noor Wachid <wachid@fisip.net>
2024-03-08 19:19:14 +0700

    rerun format-queries

--- queries/gularen/injections.scm
+++ queries/gularen/injections.scm
@@ -10,14 +10,17 @@
 (code_block_lang
   (code_lang) @_language
   (#any-of? @_language "js")
-  (code_content) @injection.content (#set! injection.language "javascript"))
+  (code_content) @injection.content
+  (#set! injection.language "javascript"))
 
 (code_block_lang
   (code_lang) @_language
   (#any-of? @_language "py")
-  (code_content) @injection.content (#set! injection.language "python"))
+  (code_content) @injection.content
+  (#set! injection.language "python"))
 
 (code_block_lang
   (code_lang) @_language
   (#any-of? @_language "sh" "zsh")
-  (code_content) @injection.content (#set! injection.language "bash"))
+  (code_content) @injection.content
+  (#set! injection.language "bash"))
