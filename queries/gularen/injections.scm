b140ce5a3e55909b88e9a1c788d8dcada030b31e
Noor Wachid <wachid@fisip.net>
2024-03-08 19:14:42 +0700

    update deprecated injection.content method

--- queries/gularen/injections.scm
+++ queries/gularen/injections.scm
@@ -10,14 +10,14 @@
 (code_block_lang
   (code_lang) @_language
   (#any-of? @_language "js")
-  (code_content) @javascript)
+  (code_content) @injection.content (#set! injection.language "javascript"))
 
 (code_block_lang
   (code_lang) @_language
   (#any-of? @_language "py")
-  (code_content) @python)
+  (code_content) @injection.content (#set! injection.language "python"))
 
 (code_block_lang
   (code_lang) @_language
   (#any-of? @_language "sh" "zsh")
-  (code_content) @bash)
+  (code_content) @injection.content (#set! injection.language "bash"))
