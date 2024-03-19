7ac7fbc94dfce308dd075f0a7d3d0b59c6de60aa
datwaft <datwaftx@gmail.com>
2024-03-19 15:20:30 -0600

    feat(lua): add injections for `vim.filetype.add`

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -177,3 +177,21 @@
 (comment
   content: (_) @injection.content
   (#set! injection.language "comment"))
+
+; vim.filetype.add({ pattern = { ["some lua pattern here"] = "filetype" } })
+((function_call
+  name: (_) @_filetypeadd_identifier
+  arguments:
+    (arguments
+      (table_constructor
+        (field
+          name: (_) @_pattern_key
+          value:
+            (table_constructor
+              (field
+                name:
+                  (string
+                    content: _ @injection.content)))))))
+  (#set! injection.language "luap")
+  (#eq? @_filetypeadd_identifier "vim.filetype.add")
+  (#eq? @_pattern_key "pattern"))
