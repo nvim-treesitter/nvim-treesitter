d0dcb623e5ac05cce56b61852c971da756381e55
Alexei Mozaidze <lexerpexer@proton.me>
2024-03-19 12:57:36 +0400

    feat(fennel)!: update parser and queries

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -547,7 +547,7 @@ list.faust = {
 list.fennel = {
   install_info = {
     url = "https://github.com/alexmozaidze/tree-sitter-fennel",
-    files = { "src/parser.c" },
+    files = { "src/parser.c", "src/scanner.c" },
   },
   maintainers = { "@alexmozaidze" },
 }
