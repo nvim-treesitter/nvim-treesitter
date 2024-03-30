7c7c6966d44d7ddadc4350a27db98d25a11199cd
Oliver Thomas <thomaso@ie-freiburg.mpg.de>
2024-03-30 12:11:51 +0100

    feat(snakemake): add indentation queries

---
+++ queries/snakemake/indents.scm
@@ -0,0 +1,16 @@
+; inherits: python
+
+((rule_definition) @indent.begin
+  (#set! indent.immediate 1))
+
+((checkpoint_definition) @indent.begin
+  (#set! indent.immediate 1))
+
+((rule_inheritance) @indent.begin
+  (#set! indent.immediate 1))
+
+((module_definition) @indent.begin
+  (#set! indent.immediate 1))
+
+((directive) @indent.begin
+  (#set! indent.immediate 1))
