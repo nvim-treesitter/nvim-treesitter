282be08f39873bbafbdf9ddb05bd64344ab958ba
Jonas Hietala <mail@jonashietala.se>
2024-03-31 18:19:44 +0200

    Update djot queries
    - Adjust some highlights to fit context better
    - Conceal inline content
    - Add locals
    - Add folds

---
+++ queries/djot/locals.scm
@@ -0,0 +1,17 @@
+(link_reference_definition
+  (link_label) @local.definition)
+
+(footnote
+  (reference_label) @local.definition)
+
+(collapsed_reference_link
+  (link_text) @local.reference)
+
+(full_reference_link
+  (link_label) @local.reference)
+
+(full_reference_image
+  (link_label) @local.reference)
+
+(footnote_reference
+  (reference_label) @local.reference)
