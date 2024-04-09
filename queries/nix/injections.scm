8ef456cfc6fbfc06c2b16ddaf6857f5290a9c6a5
Riley Bruins <ribru17@hotmail.com>
2024-04-08 21:40:50 -0700

    feat(nix): arbitrary injections using `#` comments

--- queries/nix/injections.scm
+++ queries/nix/injections.scm
@@ -12,6 +12,18 @@
   (#gsub! @injection.language "/%*%s*([%w%p]+)%s*%*/" "%1")
   (#set! injection.combined))
 
+; #-style Comments
+((comment) @injection.language
+  . ; this is to make sure only adjacent comments are accounted for the injections
+  [
+    (string_expression
+      (string_fragment) @injection.content)
+    (indented_string_expression
+      (string_fragment) @injection.content)
+  ]
+  (#gsub! @injection.language "#%s*([%w%p]+)%s*" "%1")
+  (#set! injection.combined))
+
 (apply_expression
   function: (_) @_func
   argument: [
