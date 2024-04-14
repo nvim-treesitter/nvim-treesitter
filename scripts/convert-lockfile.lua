763f4e7e23f7c135d0dbf27a2a92fee104823df0
Christian Clason <c.clason@uni-graz.at>
2024-04-14 16:25:28 +0200

    feat!: track parser revision in Lua
    Problem: Tracking parser revision in lockfile and allowing override
    through the parsers module complicates the code. In addition, only
    revision changes are handled robustly, not changes to other installation
    info.
    
    Solution: Track parser revision in the parsers module directly. Reload
    parser table on every install or update call. Support modifying parser
    table in a `User TSUpdate` autocommand.

---
+++ scripts/convert-lockfile.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S nvim -l
+vim.opt.runtimepath:append('.')
+local util = require('nvim-treesitter.util')
+local parsers = require('nvim-treesitter.parsers')
+
+local filename = require('nvim-treesitter.install').get_package_path('lockfile.json')
+local lockfile = vim.json.decode(util.read_file(filename)) --[[@as table<string,{revision:string}>]]
+
+for k, p in pairs(parsers) do
+  if p.install_info then
+    p.install_info.revision = lockfile[k].revision
+  end
+end
+
+-- write new parser file
+local header = '---@type nvim-ts.parsers\nreturn '
+local parser_file = header .. vim.inspect(parsers)
+if vim.fn.executable('stylua') == 1 then
+  parser_file = vim.system({ 'stylua', '-' }, { stdin = parser_file }):wait().stdout --[[@as string]]
+end
+util.write_file('lua/nvim-treesitter/parsers.lua', parser_file)
