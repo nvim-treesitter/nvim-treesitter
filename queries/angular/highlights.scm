446e99b9782f7b7ee5c8f769a1cd0f70f11832d3
Dennis van den Berg <dennis.vandenberg@nedap.com>
2024-04-22 11:35:48 +0200

    feat(angular): Add support for ICU expressions.

--- queries/angular/highlights.scm
+++ queries/angular/highlights.scm
@@ -101,6 +101,12 @@
   (defer_end_expression)
 ] @punctuation.bracket
 
+(two_way_binding
+  [
+    "[("
+    ")]"
+  ] @punctuation.bracket)
+
 [
   "{{"
   "}}"
@@ -118,6 +124,10 @@
 
 (concatenation_expression
   "+" @operator)
+
+(icu_clause) @keyword.operator
+
+(icu_category) @keyword.conditional
 
 (binary_expression
   [
