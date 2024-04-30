1a4274c9e1226fb4c2009f4c0c3521b1931b0c5c
Lucas Lopes <lucaslquintino@gmail.com>
2024-04-30 12:10:19 -0300

    feat(svelte): add javascript injection (#6540)

--- queries/svelte/injections.scm
+++ queries/svelte/injections.scm
@@ -25,6 +25,17 @@
   (#any-of? @_lang "ts" "typescript")
   (#set! injection.language "typescript"))
 
+((script_element
+  (start_tag
+    (attribute
+      (attribute_name) @_attr
+      (quoted_attribute_value
+        (attribute_value) @_lang)))
+  (raw_text) @injection.content)
+  (#eq? @_attr "lang")
+  (#any-of? @_lang "js" "javascript")
+  (#set! injection.language "javascript"))
+
 ((element
   (start_tag
     (attribute
