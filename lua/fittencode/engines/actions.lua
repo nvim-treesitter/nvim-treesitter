14cb0aa928a253a4c1e4bc570cb5b5108466e384
luozhiya <luozhiya@petalmail.com>
2024-05-09 23:14:42 +0800

    Implement `chian_action`

--- lua/fittencode/engines/actions.lua
+++ lua/fittencode/engines/actions.lua
@@ -6,6 +6,8 @@ local Log = require('fittencode.log')
 local Promise = require('fittencode.concurrency.promise')
 local Sessions = require('fittencode.sessions')
 
+local schedule = Base.schedule
+
 local ActionsEngine = {}
 
 local Actions = {
@@ -29,14 +31,31 @@ local function get_action_type(action)
   return 'FittenCodePrompt/Actions/' .. get_action_name(action)
 end
 
-local function linear_suggestions(suggestions)
+local function make_solved_prefix(prefix, suggestions)
+  if not suggestions then
+    return
+  end
   suggestions = vim.tbl_filter(function(s) return #s > 0 end, suggestions)
   if #suggestions ~= 0 then
-    return table.concat(suggestions, '\n') .. '\n'
+    return prefix .. table.concat(suggestions, '\n') .. '\n'
   end
 end
 
-local function chian_action(promise, resolve_promise)
+local function chian_action(action, solved_prefix, on_error)
+  Sessions.request_generate_one_stage(0, {
+    prompt_ty = get_action_type(action),
+    solved_prefix = solved_prefix,
+  }, function(_, prompt, suggestions)
+    Log.debug('Suggestions for Actions: {}', suggestions)
+    local new_solved_prefix = make_solved_prefix(prompt.prefix, suggestions)
+    if not new_solved_prefix then
+      schedule(on_error)
+    else
+      chian_action(action, new_solved_prefix, on_error)
+    end
+  end, function()
+    schedule(on_error)
+  end)
 end
 
 ---@param action number
@@ -50,6 +69,10 @@ function ActionsEngine.start_action(action, opts)
   local window = api.nvim_get_current_win()
   local buffer = api.nvim_win_get_buf(window)
 
+  local on_error = function()
+    Log.debug('Action: No more suggestions')
+  end
+
   Promise:new(function(resolve, reject)
     Sessions.request_generate_one_stage(0, {
       window = window,
@@ -61,26 +84,19 @@ function ActionsEngine.start_action(action, opts)
       prompt = opts and opts.prompt,
     }, function(_, prompt, suggestions)
       Log.debug('Suggestions for Actions: {}', suggestions)
-      suggestions = linear_suggestions(suggestions)
-      if not suggestions then
+      local solved_prefix = make_solved_prefix(prompt.prefix, suggestions)
+      if not solved_prefix then
         reject()
       else
-        resolve(prompt.prefix .. suggestions)
+        resolve(solved_prefix)
       end
     end, function()
-      -- nothings to do here
+      reject()
     end)
   end):forward(function(solved_prefix)
-    return Promise:new(function(resolve, reject)
-      Sessions.request_generate_one_stage(0, {
-        prompt_ty = get_action_type(action),
-        solved_prefix = solved_prefix
-      }, function(_, prompt, suggestions)
-        Log.debug('Suggestions for Actions 2: {}', suggestions)
-      end, function()
-        -- nothings to do here
-      end)
-    end)
+    chian_action(action, solved_prefix, on_error)
+  end, function()
+    schedule(on_error)
   end
   )
 end
