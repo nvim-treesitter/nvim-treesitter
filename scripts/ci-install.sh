4d9fb7d8b0621db203959e8bc7ecc68afdf5f3d9
Christian Clason <c.clason@uni-graz.at>
2024-05-17 08:50:23 +0200

    ci: Nvim 0.10 macos artifact name

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -11,9 +11,10 @@ if [[ $os == Linux ]]; then
   mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
   ln -s "$PWD" ~/.local/share/nvim/site/pack/nvim-treesitter/start
 elif [[ $os == Darwin ]]; then
-  curl -L https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-macos.tar.gz | tar -xz
-  sudo ln -s "$PWD"/nvim-macos/bin/nvim /usr/local/bin
-  rm -rf "$PWD"/nvim-macos/lib/nvim/parser
+  RELEASE_NAME="nvim-macos-$(uname -m)"
+  curl -L "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/$RELEASE_NAME.tar.gz" | tar -xz
+  sudo ln -s "$PWD/$RELEASE_NAME/bin/nvim" /usr/local/bin
+  rm -rf "$PWD/$RELEASE_NAME/lib/nvim/parser"
   mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
   ln -s "$PWD" ~/.local/share/nvim/site/pack/nvim-treesitter/start
 else
