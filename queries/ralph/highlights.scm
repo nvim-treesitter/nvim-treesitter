f0d57c891532823d733e0cf6e5cca96ff9d56a73
Thomas Droxler <thomas.droxler@gmail.com>
2024-05-23 15:48:11 +0200

    Fix ralph highlights

--- queries/ralph/highlights.scm
+++ queries/ralph/highlights.scm
@@ -1,91 +1,135 @@
 (type_identifier) @type
-(primitive_type) @type.builtin
-(alph_token_id) @type.builtin
 
-(var_declaration) @variable
-(struct_field) @variable
-(arg_def (identifier) @variable)
-(struct_constructor_field (identifier) @variable)
+[
+  (primitive_type)
+  (alph_token_id)
+] @type.builtin
 
-(hex_num) @number
-(integer) @number
-(typed_num) @number
+[
+  (var_declaration)
+  (struct_field)
+] @variable
 
-(bool)  @constant.builtin
-(bytes) @constant.builtin
-(address) @constant.builtin
+(arg_def
+  (identifier) @variable)
+
+(struct_constructor_field
+  (identifier) @variable)
+
+[
+  (hex_num)
+  (integer)
+  (typed_num)
+] @number
+
+[
+  (bool)
+  (bytes)
+  (address)
+] @constant.builtin
 
 (func_id) @function
 
-(mutable) @keyword
-(annotation)  @keyword
-(annotation (identifier) @keyword)
-(annotation_field (identifier) @variable)
-(annotation_field (identifier) @variable)
-(map_def (identifier) @variable)
+[
+  (mutable)
+  (annotation)
+] @keyword
 
-"Abstract" @keyword
-"Contract" @keyword
-"TxScript" @keyword
-"Interface" @keyword
-"AssetScript" @keyword
-"struct" @keyword
-"fn" @keyword
-"pub" @keyword
-"let" @keyword
-"while" @keyword
-"for" @keyword
-"if" @keyword
-"else" @keyword
-"emit" @keyword
-"enum" @keyword
-"extends" @keyword
-"embeds" @keyword
-"implements" @keyword
-"return" @keyword
-"const" @keyword
-"event" @keyword
-"mapping" @keyword
+(annotation
+  (identifier) @keyword)
 
-"(" @punctuation.bracket
-")" @punctuation.bracket
-"[" @punctuation.bracket
-"]" @punctuation.bracket
-"{" @punctuation.bracket
-"}" @punctuation.bracket
+(annotation_field
+  (identifier) @variable)
 
-"." @punctuation.delimiter
-"," @punctuation.delimiter
-":" @punctuation.delimiter
-";" @punctuation.delimiter
-"=" @punctuation.delimiter
-"->" @punctuation.delimiter
+(annotation_field
+  (identifier) @variable)
 
-(op_byte_vec_add) @operator
-(op_add) @operator
-(op_sub) @operator
-(op_mul) @operator
-(op_exp) @operator
-(op_mod_exp) @operator
-(op_div) @operator
-(op_mod) @operator
-(op_mod_add) @operator
-(op_mod_sub) @operator
-(op_mod_mul) @operator
-(op_shl) @operator
-(op_shr) @operator
-(op_bit_and) @operator
-(op_xor) @operator
-(op_bit_or) @operator
-(op_eq) @operator
-(op_ne) @operator
-(op_lt) @operator
-(op_le) @operator
-(op_gt) @operator
-(op_ge) @operator
-(op_and) @operator
-(op_or) @operator
-(op_not) @operator
+(map_def
+  (identifier) @variable)
 
-(line_comment) @comment
+"fn" @keyword.function
 
+[
+  "Contract"
+  "TxScript"
+  "Interface"
+  "AssetScript"
+  "struct"
+  "enum"
+  "event"
+  "mapping"
+] @keyword.type
+
+[
+  "const"
+  "pub"
+  "Abstract"
+] @keyword.modifier
+
+[
+  "while"
+  "for"
+] @keyword.repeat
+
+"return" @keyword.return
+
+[
+  "if"
+  "else"
+] @keyword.conditional
+
+[
+  "let"
+  "emit"
+  "extends"
+  "embeds"
+  "implements"
+] @keyword
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+[
+  "."
+  ","
+  ":"
+  ";"
+  "="
+  "->"
+] @punctuation.delimiter
+
+[
+  (op_byte_vec_add)
+  (op_add)
+  (op_sub)
+  (op_mul)
+  (op_exp)
+  (op_mod_exp)
+  (op_div)
+  (op_mod)
+  (op_mod_add)
+  (op_mod_sub)
+  (op_mod_mul)
+  (op_shl)
+  (op_shr)
+  (op_bit_and)
+  (op_xor)
+  (op_bit_or)
+  (op_eq)
+  (op_ne)
+  (op_lt)
+  (op_le)
+  (op_gt)
+  (op_ge)
+  (op_and)
+  (op_or)
+  (op_not)
+] @operator
+
+(line_comment) @comment @spell
