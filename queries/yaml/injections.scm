37ff3cc68dd070754178daed3082cadfef860705
Magnus Larsen <mzla@topsoe.com>
2024-05-29 14:12:05 +0200

    [injections/yaml]: bash injections in ansible files

--- queries/yaml/injections.scm
+++ queries/yaml/injections.scm
@@ -78,3 +78,56 @@
           (block_scalar) @injection.content
           (#set! injection.language "promql")
           (#offset! @injection.content 0 1 0 0))))))
+
+; Ansible ("command" / "shell")
+(block_sequence
+  (block_sequence_item
+    (_
+      (_
+        (block_mapping_pair
+          key: (flow_node
+            (plain_scalar
+              (string_scalar) @_command
+              (#any-of? @_command "command" "shell" "ansible.builtin.command"
+                "ansible.builtin.shell")))
+          value: (flow_node
+            (plain_scalar
+              (string_scalar) @injection.content
+              (#set! injection.language "bash"))))))))
+
+(block_sequence
+  (block_sequence_item
+    (_
+      (_
+        (block_mapping_pair
+          key: (flow_node
+            (plain_scalar
+              (string_scalar) @_command
+              (#any-of? @_command "command" "shell" "ansible.builtin.command"
+                "ansible.builtin.shell")))
+          value: (block_node
+            (block_scalar) @injection.content
+            (#set! injection.language "bash")
+            (#offset! @injection.content 0 1 0 0)))))))
+
+(block_sequence
+  (block_sequence_item
+    (_
+      (_
+        (block_mapping_pair
+          key: (flow_node
+            (plain_scalar
+              (string_scalar) @_command
+              (#any-of? @_command "command" "shell" "ansible.builtin.command"
+                "ansible.builtin.shell")))
+          value: (block_node
+            (block_mapping
+              (block_mapping_pair
+                key: (flow_node
+                  (plain_scalar
+                    (string_scalar) @_cmd
+                    (#eq? @_cmd "cmd")))
+                value: (flow_node
+                  (plain_scalar
+                    (string_scalar) @injection.content
+                    (#set! injection.language "bash")))))))))))
