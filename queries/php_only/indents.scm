3d8f3e069c54d2016f6d97161022b0e2d54d39a4
Caleb White <cdwhite3@pm.me>
2024-06-07 12:43:02 -0500

    feat(php): indent inside property hooks

--- queries/php_only/indents.scm
+++ queries/php_only/indents.scm
@@ -13,6 +13,7 @@
   (match_block)
   (case_statement)
   (default_statement)
+  (property_hook_list)
 ] @indent.begin
 
 (return_statement
@@ -45,6 +46,9 @@
 
 (return_statement
   ";" @indent.end)
+
+(property_hook_list
+  "}" @indent.end)
 
 (ERROR
   "(" @indent.align
