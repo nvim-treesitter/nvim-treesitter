f0dbe180004d3566a994b1093e3a37893cc141c1
Riley Bruins <ribru17@hotmail.com>
2024-07-04 16:42:11 -0700

    feat(jsdoc): add missing highlights

--- queries/jsdoc/highlights.scm
+++ queries/jsdoc/highlights.scm
@@ -1,3 +1,40 @@
 (tag_name) @keyword @nospell
 
 (type) @type @nospell
+
+[
+  "{"
+  "}"
+  "["
+  "]"
+] @punctuation.bracket
+
+[
+  ":"
+  "/"
+  "."
+  "#"
+  "~"
+] @punctuation.delimiter
+
+(identifier) @variable
+
+(tag
+  (tag_name) @_name
+  (identifier) @function
+  (#any-of? @_name "@callback" "@function" "@func" "@method"))
+
+(tag
+  (tag_name) @_name
+  (identifier) @variable.parameter
+  (#any-of? @_name "@param" "@arg" "@argument"))
+
+(tag
+  (tag_name) @_name
+  (identifier) @property
+  (#any-of? @_name "@prop" "@property"))
+
+(tag
+  (tag_name) @_name
+  (identifier) @type
+  (#eq? @_name "@typedef"))
