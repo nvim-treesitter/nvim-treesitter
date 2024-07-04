5d1646893a01397f2b4abea6ad546c4aa6c6efed
Omar Valdez <80359343+ValdezFOmar@users.noreply.github.com>
2024-07-04 10:23:44 -0700

    feat: editorconfig parser and queries

---
+++ queries/editorconfig/highlights.scm
@@ -0,0 +1,56 @@
+(comment) @comment @spell
+
+; consistency with toml and ini
+(section
+  (section_name) @type)
+
+(character_choice
+  (character) @constant)
+
+(character_range
+  start: (character) @constant
+  end: (character) @constant)
+
+[
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+[
+  ","
+  ".."
+  (path_separator)
+] @punctuation.delimiter
+
+[
+  "-"
+  "="
+  (negation)
+] @operator
+
+[
+  (wildcard_characters)
+  (wildcard_any_characters)
+  (wildcard_single_character)
+] @character.special
+
+(escaped_character) @string.escape
+
+(pair
+  key: (identifier) @property
+  value: (_) @string)
+
+(boolean) @boolean
+
+(integer) @number
+
+(unset) @constant.builtin
+
+[
+  (spelling_language)
+  (indent_style)
+  (end_of_line)
+  (charset)
+] @string.special
