64f6f0ab4e3f613aa682eb5fe29c5025db500ddd
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2024-07-07 18:37:02 +0200

    fix(hurl): multiline injection query needs injection.combined
    fixes: #6882, https://github.com/pfeiferj/tree-sitter-hurl/issues/10

--- queries/hurl/injections.scm
+++ queries/hurl/injections.scm
@@ -11,4 +11,5 @@
 (multiline_string
   (multiline_string_type) @_lang
   (multiline_string_content) @injection.content
-  (#set-lang-from-info-string! @_lang))
+  (#set-lang-from-info-string! @_lang)
+  (#set! injection.combined))
