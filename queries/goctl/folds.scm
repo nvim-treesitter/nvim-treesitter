c576099f099e67a8a1faede8df22abe1dc8dfc66
jayce.zhao <326747337@qq.com>
2024-07-07 10:08:25 +0800

    feat: modify format

--- queries/goctl/folds.scm
+++ queries/goctl/folds.scm
@@ -1,19 +1,15 @@
-; Fold types
 (typeGroupSpec) @fold
 
 (typeSingleSpec) @fold
 
 (typeStruct) @fold
 
-; Fold services
 (serviceSpec) @fold
 
 (serviceBody) @fold
 
-; Fold import groups
 (importStatement) @fold
 
-; Fold service info struct
 [
   (serviceServerSpec)
   (infoStatement)
