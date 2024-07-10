f29560d110c9a361ca85022d47a550f4730df231
jayce.zhao <326747337@qq.com>
2024-07-10 18:47:49 +0800

    feat: add indents.scm for goctl.

---
+++ queries/goctl/indents.scm
@@ -0,0 +1,33 @@
+[
+  (importGroup)
+  (typeGroupSpec)
+  (structType)
+  (infoStatement)
+  (serviceServerSpec)
+  (serviceSpec)
+] @indent.begin
+
+(importGroup
+  ")" @indent.branch)
+
+(structType
+  "}" @indent.branch)
+
+(typeGroupSpec
+  ")" @indent.branch)
+
+(infoStatement
+  ")" @indent.branch)
+
+(serviceServerSpec
+  ")" @indent.branch)
+
+(serviceSpec
+  "}" @indent.branch)
+
+[
+  "}"
+  ")"
+] @indent.end
+
+(comment) @indent.ignore
