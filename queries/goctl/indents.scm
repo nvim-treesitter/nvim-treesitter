656ac242338d3d78a5e64d2b3b7f06892aeabfea
zhaown <51357674+chaozwn@users.noreply.github.com>
2024-07-11 00:36:01 +0800

    feat(goctl): add indents

---
+++ queries/goctl/indents.scm
@@ -0,0 +1,15 @@
+[
+  (importGroup)
+  (typeGroupSpec)
+  (structType)
+  (infoStatement)
+  (serviceServerSpec)
+  (serviceSpec)
+] @indent.begin
+
+[
+  ")"
+  "}"
+] @indent.branch @indent.end
+
+(comment) @indent.ignore
