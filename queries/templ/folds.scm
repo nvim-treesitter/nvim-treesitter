1b2d114763911351957512fd4fefe799d3332231
Vincent Rischmann <vincent@rischmann.fr>
2024-07-15 22:36:31 +0200

    templ: add folds queries

---
+++ queries/templ/folds.scm
@@ -0,0 +1,15 @@
+[
+  (component_block)
+  (css_declaration)
+  (script_declaration)
+  (element)
+  (style_element)
+  (script_element)
+  (component_switch_statement)
+  (field_declaration_list)
+  (block)
+  (interface_type)
+  (comment)
+  (const_declaration)
+  (var_declaration)
+] @fold
