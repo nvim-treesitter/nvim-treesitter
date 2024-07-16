0d5e1214a5c386a168dc8e19d7da0ceb0e0bd6f2
Dennis van den Berg <dennis.vandenberg@nedap.com>
2024-07-16 20:29:19 +0200

    feat: add indentation queries for angular

--- queries/angular/indents.scm
+++ queries/angular/indents.scm
@@ -1,1 +1,16 @@
 ; inherits: html_tags
+
+[
+  (statement_block)
+  (switch_statement)
+] @indent.begin
+
+(statement_block
+  "{" @indent.branch)
+
+(statement_block
+  "}" @indent.end)
+
+"}" @indent.branch
+
+"}" @indent.end
