a0732ca9f0b2549d795aafadd42b357dffb231d0
Dheepak Krishnamurthy <noreply@kdheepak.com>
2024-07-23 20:26:57 -0400

    feat(julia): Add syntax highlighting for markdown and bash prefixed_string_literals

--- queries/julia/injections.scm
+++ queries/julia/injections.scm
@@ -13,14 +13,27 @@
   (#set! injection.language "markdown")
   (#offset! @injection.content 0 3 0 -3))
 
+; Inject comments
 ([
   (line_comment)
   (block_comment)
 ] @injection.content
   (#set! injection.language "comment"))
 
+; Inject regex in r"hello\bworld"
 ((prefixed_string_literal
   prefix: (identifier) @_prefix) @injection.content
   (#eq? @_prefix "r")
   (#set! injection.language "regex")
   (#offset! @injection.content 0 2 0 -1))
+
+; Inject markdown in md"**Bold** and _Italics_"
+((prefixed_string_literal
+  prefix: (identifier) @_prefix) @injection.content
+  (#eq? @_prefix "md")
+  (#set! injection.language "markdown")
+  (#offset! @injection.content 0 3 0 -1))
+
+; Inject bash in `git add --help`
+((command_literal) @injection.content
+  (#set! injection.language "bash"))
