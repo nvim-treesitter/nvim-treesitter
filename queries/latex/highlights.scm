3a646ccad4a14017cbebf433879e2606246c9baf
Jonas Dujava <jonas.dujava@gmail.com>
2024-07-27 23:18:55 +0200

    feat(latex): capture left/right math delimiters
    Capture math delimiters provided by commands such as
    `\left` and `\right`.
    
    Signed-off-by: Jonas Dujava <jonas.dujava@gmail.com>

--- queries/latex/highlights.scm
+++ queries/latex/highlights.scm
@@ -36,6 +36,12 @@
 
 (delimiter) @punctuation.delimiter
 
+(math_delimiter
+  left_command: _ @punctuation.delimiter
+  left_delimiter: _ @punctuation.delimiter
+  right_command: _ @punctuation.delimiter
+  right_delimiter: _ @punctuation.delimiter)
+
 [
   "["
   "]"
