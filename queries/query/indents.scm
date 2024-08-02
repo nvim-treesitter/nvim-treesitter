b1377202577fd8a2eebc32ea526d529ffaa9ab80
Riley Bruins <ribru17@hotmail.com>
2024-08-02 06:05:25 -0700

    fix(query): proper indentation after quantifiers

--- queries/query/indents.scm
+++ queries/query/indents.scm
@@ -12,7 +12,12 @@
 
 "]" @indent.branch @indent.end
 
-")" @indent.end
+[
+  ")"
+  "?"
+  "*"
+  "+"
+] @indent.end
 
 ; Captures always mark the end of a node
 ; Because of that, mark it as indent.end
