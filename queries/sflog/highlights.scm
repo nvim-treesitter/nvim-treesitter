b32be28f6a73929acff230c75bc4c903d0c84681
xi xiao <tdxiaoxi2@gmail.com>
2024-08-06 07:15:00 +0300

    fix: linting issue in highlights.scm

--- queries/sflog/highlights.scm
+++ queries/sflog/highlights.scm
@@ -1,23 +1,44 @@
-;; highlights.scm
-
+; highlights.scm
 (number) @number
+
 (identifier) @variable
+
 (version) @number.readonly
-(namespace) @namespace
+
+(namespace) @variable
 
 (limit) @property
-(limit (identifier) @string)
-(limit (number) @number)
 
-(duration (number) @number.readonly) @string
-(location (number) @number.readonly) @string
+(limit
+  (identifier) @string)
 
-(event_detail (event_detail_value) @string)
+(limit
+  (number) @number)
+
+(duration
+  (number) @number.readonly) @string
+
+(location
+  (number) @number.readonly) @string
+
+(event_detail
+  (event_detail_value) @string)
 
 (log_level_settings) @comment
-(log_level_setting (component) @type)
-(log_level_setting (log_level) @constant)
 
-(log_entry (timestamp (time) @function) @number)
-(log_entry (location) @number)
-(log_entry (event_identifier (identifier) @type) @keyword)
+(log_level_setting
+  (component) @type)
+
+(log_level_setting
+  (log_level) @constant)
+
+(log_entry
+  (timestamp
+    (time) @function) @number)
+
+(log_entry
+  (location) @number)
+
+(log_entry
+  (event_identifier
+    (identifier) @type) @keyword)
