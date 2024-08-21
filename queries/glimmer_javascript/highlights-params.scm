d7b0205a1d51356d6260ca30b0ad4ea4114e0907
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-08-20 22:03:22 -0400

    We can extend from ecma

--- queries/glimmer_javascript/highlights-params.scm
+++ queries/glimmer_javascript/highlights-params.scm
@@ -1,12 +1,3 @@
-(formal_parameters
-  [
-    (identifier) @variable.parameter
-    (array_pattern
-      (identifier) @variable.parameter)
-    (object_pattern
-      [
-        (pair_pattern
-          value: (identifier) @variable.parameter)
-        (shorthand_property_identifier_pattern) @variable.parameter
-      ])
-  ])
+; inherits: javascript
+
+; extends
--- queries/glimmer_javascript/highlights.scm
+++ queries/glimmer_javascript/highlights.scm
@@ -1,226 +1,65 @@
+; inherits: ecma
+
 (glimmer_opening_tag) @tag.builtin
 
 (glimmer_closing_tag) @tag.builtin
 
 ;
 ;
-; Copied from tree-sitter-javascript @ 0.21.4
+; Copied from the javascript queries
 ;
 ;
-; Variables
-;----------
-(identifier) @variable
+; Parameters
+(formal_parameters
+  (identifier) @variable.parameter)
 
-; Properties
-;-----------
-(property_identifier) @property
+(formal_parameters
+  (rest_pattern
+    (identifier) @variable.parameter))
 
-; Function and method definitions
-;--------------------------------
-(function_expression
-  name: (identifier) @function)
+; ({ a }) => null
+(formal_parameters
+  (object_pattern
+    (shorthand_property_identifier_pattern) @variable.parameter))
 
-(function_declaration
-  name: (identifier) @function)
+; ({ a = b }) => null
+(formal_parameters
+  (object_pattern
+    (object_assignment_pattern
+      (shorthand_property_identifier_pattern) @variable.parameter)))
 
-(method_definition
-  name: (property_identifier) @function.method)
+; ({ a: b }) => null
+(formal_parameters
+  (object_pattern
+    (pair_pattern
+      value: (identifier) @variable.parameter)))
 
-(pair
-  key: (property_identifier) @function.method
-  value: [
-    (function_expression)
-    (arrow_function)
-  ])
+; ([ a ]) => null
+(formal_parameters
+  (array_pattern
+    (identifier) @variable.parameter))
 
-(assignment_expression
-  left: (member_expression
-    property: (property_identifier) @function.method)
-  right: [
-    (function_expression)
-    (arrow_function)
-  ])
+; ({ a } = { a }) => null
+(formal_parameters
+  (assignment_pattern
+    (object_pattern
+      (shorthand_property_identifier_pattern) @variable.parameter)))
 
-(variable_declarator
-  name: (identifier) @function
-  value: [
-    (function_expression)
-    (arrow_function)
-  ])
+; ({ a = b } = { a }) => null
+(formal_parameters
+  (assignment_pattern
+    (object_pattern
+      (object_assignment_pattern
+        (shorthand_property_identifier_pattern) @variable.parameter))))
 
-(assignment_expression
-  left: (identifier) @function
-  right: [
-    (function_expression)
-    (arrow_function)
-  ])
+; a => null
+(arrow_function
+  parameter: (identifier) @variable.parameter)
 
-; Function and method calls
-;--------------------------
-(call_expression
-  function: (identifier) @function)
+; optional parameters
+(formal_parameters
+  (assignment_pattern
+    left: (identifier) @variable.parameter))
 
-(call_expression
-  function: (member_expression
-    property: (property_identifier) @function.method))
-
-; Special identifiers
-;--------------------
-(glimmer_opening_tag) @tag.builtin
-
-(glimmer_closing_tag) @tag.builtin
-
-((identifier) @constructor
-  (#match? @constructor "^[A-Z]"))
-
-([
-  (identifier)
-  (shorthand_property_identifier)
-  (shorthand_property_identifier_pattern)
-] @constant
-  (#match? @constant "^[A-Z_][A-Z\\d_]+$"))
-
-((identifier) @variable.builtin
-  (#match? @variable.builtin "^(arguments|module|console|window|document)$")
-  (#is-not? local))
-
-((identifier) @function.builtin
-  (#eq? @function.builtin "require")
-  (#is-not? local))
-
-; Literals
-;---------
-(this) @variable.builtin
-
-(super) @variable.builtin
-
-[
-  (true)
-  (false)
-  (null)
-  (undefined)
-] @constant.builtin
-
-(comment) @comment
-
-[
-  (string)
-  (template_string)
-] @string
-
-(regex) @string.special
-
-(number) @number
-
-; Tokens
-;-------
-[
-  ";"
-  (optional_chain)
-  "."
-  ","
-] @punctuation.delimiter
-
-[
-  "-"
-  "--"
-  "-="
-  "+"
-  "++"
-  "+="
-  "*"
-  "*="
-  "**"
-  "**="
-  "/"
-  "/="
-  "%"
-  "%="
-  "<"
-  "<="
-  "<<"
-  "<<="
-  "="
-  "=="
-  "==="
-  "!"
-  "!="
-  "!=="
-  "=>"
-  ">"
-  ">="
-  ">>"
-  ">>="
-  ">>>"
-  ">>>="
-  "~"
-  "^"
-  "&"
-  "|"
-  "^="
-  "&="
-  "|="
-  "&&"
-  "||"
-  "??"
-  "&&="
-  "||="
-  "??="
-] @operator
-
-[
-  "("
-  ")"
-  "["
-  "]"
-  "{"
-  "}"
-] @punctuation.bracket
-
-(template_substitution
-  "${" @punctuation.bracket
-  "}" @punctuation.bracket) @punctuation.bracket
-
-[
-  "as"
-  "async"
-  "await"
-  "break"
-  "case"
-  "catch"
-  "class"
-  "const"
-  "continue"
-  "debugger"
-  "default"
-  "delete"
-  "do"
-  "else"
-  "export"
-  "extends"
-  "finally"
-  "for"
-  "from"
-  "function"
-  "get"
-  "if"
-  "import"
-  "in"
-  "instanceof"
-  "let"
-  "new"
-  "of"
-  "return"
-  "set"
-  "static"
-  "switch"
-  "target"
-  "throw"
-  "try"
-  "typeof"
-  "var"
-  "void"
-  "while"
-  "with"
-  "yield"
-] @keyword
+; punctuation
+(optional_chain) @punctuation.delimiter
