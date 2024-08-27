d8d061a1e7657aee7da27b02fa0bec6e61eecb18
Christian Clason <c.clason@uni-graz.at>
2024-08-27 11:02:34 +0200

    feat(install): allow specifying max jobs

--- scripts/install-parsers.lua
+++ scripts/install-parsers.lua
@@ -1,10 +1,13 @@
 #!/usr/bin/env -S nvim -l
 
 local generate = false
+local max_jobs = nil ---@as integer
 local parsers = {}
 for i = 1, #_G.arg do
   if _G.arg[i] == '--generate' then
     generate = true
+  elseif _G.arg[i]:find('^%-%-max%-jobs') then
+    max_jobs = _G.arg[i]:match('=(%d+)')
   else
     parsers[#parsers + 1] = _G.arg[i]
   end
@@ -18,7 +21,7 @@ vim.fn.mkdir(vim.fn.stdpath('cache'), 'p')
 local done = false
 require('nvim-treesitter.install').install(
   #parsers > 0 and parsers or 'all',
-  { force = true, generate = generate },
+  { force = true, generate = generate, max_jobs = max_jobs },
   function()
     done = true
   end
