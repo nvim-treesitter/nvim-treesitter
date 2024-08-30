51c49e1b4385aab149e3f6b2a8c7f156e2914a87
Maxim Onciul <onciul@cs.fau.de>
2024-08-30 21:00:54 +0200

    feat(edl): highlights for EDL
    EDL is intel's enclave definition language to define SGX enclaves. These
    files probably never need anything more than highlights.

---
+++ queries/edl/highlights.scm
@@ -0,0 +1,59 @@
+(parameter (type_specifier) @type)
+
+(parameter (identifier) @variable.parameter)
+
+(ecall_definition (identifier) @function)
+(ocall_definition (identifier) @function)
+
+(ecall_definition (retval) @type)
+(ocall_definition (retval) @type)
+
+(type_qualifier) @keyword.modifier
+
+(comments) @comment
+
+(string) @string
+(header_name) @string
+
+[";" ","] @punctuation.delimiter
+
+["(" ")" "[" "]" "{" "}"] @punctuation.bracket
+
+[
+ "enclave"
+ "trusted"
+ "untrusted"
+ "allow"
+ ] @keyword
+
+(untrusted_suffix_allow (identifier) @variable.parameter)
+
+(size_attribute (number) @number)
+(size_attribute (identifier) @variable.parameter)
+
+(trusted_prefix_public) @keyword.modifier
+
+[
+ "import"
+ "from"
+ ] @keyword.import
+
+[
+ "*"
+ "="
+ ] @operator
+
+[
+ "in"
+ "out"
+ "user_check"
+ "string"
+ "wstring"
+ "isary"
+ "isptr"
+ "readonly"
+ "size"
+ "count"
+ ] @storageclass
+
+(size_attribute (number)) @number
