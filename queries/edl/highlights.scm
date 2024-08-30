52114e3d3793abcdcb7d9e6e4e4083851c8da513
Maxim Onciul <onciul@cs.fau.de>
2024-08-30 21:16:52 +0200

    fix(edl): scheme format

--- queries/edl/highlights.scm
+++ queries/edl/highlights.scm
@@ -1,59 +1,83 @@
-(parameter (type_specifier) @type)
+(parameter
+  (type_specifier) @type)
 
-(parameter (identifier) @variable.parameter)
+(parameter
+  (identifier) @variable.parameter)
 
-(ecall_definition (identifier) @function)
-(ocall_definition (identifier) @function)
+(ecall_definition
+  (identifier) @function)
 
-(ecall_definition (retval) @type)
-(ocall_definition (retval) @type)
+(ocall_definition
+  (identifier) @function)
+
+(ecall_definition
+  (retval) @type)
+
+(ocall_definition
+  (retval) @type)
 
 (type_qualifier) @keyword.modifier
 
 (comments) @comment
 
 (string) @string
+
 (header_name) @string
 
-[";" ","] @punctuation.delimiter
-
-["(" ")" "[" "]" "{" "}"] @punctuation.bracket
+[
+  ";"
+  ","
+] @punctuation.delimiter
 
 [
- "enclave"
- "trusted"
- "untrusted"
- "allow"
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+[
+  "enclave"
+  "trusted"
+  "untrusted"
+  "allow"
  ] @keyword
 
-(untrusted_suffix_allow (identifier) @variable.parameter)
+(untrusted_suffix_allow
+  (identifier) @variable.parameter)
 
-(size_attribute (number) @number)
-(size_attribute (identifier) @variable.parameter)
+(size_attribute
+  (number) @number)
+
+(size_attribute
+  (identifier) @variable.parameter)
 
 (trusted_prefix_public) @keyword.modifier
 
 [
- "import"
- "from"
- ] @keyword.import
+  "import"
+  "from"
+] @keyword.import
 
 [
- "*"
- "="
- ] @operator
+  "*"
+  "="
+] @operator
 
 [
- "in"
- "out"
- "user_check"
- "string"
- "wstring"
- "isary"
- "isptr"
- "readonly"
- "size"
- "count"
- ] @storageclass
+  "in"
+  "out"
+  "user_check"
+  "string"
+  "wstring"
+  "isary"
+  "isptr"
+  "readonly"
+  "size"
+  "count"
+] @storageclass
 
-(size_attribute (number)) @number
+(size_attribute
+  (number)) @number
