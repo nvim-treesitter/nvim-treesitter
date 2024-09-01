3775bae1578ab77a85a5a4a2bd834010a5593d77
reiniscirpons <rc234@st-andrews.ac.uk>
2024-09-01 15:53:25 +0100

    fix(gap): Specify correct branch for tree-sitter-gaptst

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -646,6 +646,7 @@ list.gaptst = {
   install_info = {
     url = "https://github.com/gap-system/tree-sitter-gaptst",
     files = { "src/parser.c", "src/scanner.c" },
+    branch = "main",
   },
   maintainers = { "@reiniscirpons" },
   readme_name = "GAP system test files",
