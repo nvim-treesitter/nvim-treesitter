e090c5b22d0d061e749f8ce33cb9b9a7e6b8ab55
Riley Bruins <ribru17@hotmail.com>
2024-08-31 18:28:56 -0700

    feat(wing): add many missing highlights
    ...as well as some fold and injection queries.

--- queries/wing/injections.scm
+++ queries/wing/injections.scm
@@ -1,2 +1,15 @@
 ((comment) @injection.content
   (#set! injection.language "comment"))
+
+(call
+  (reference
+    (nested_identifier
+      object: (reference) @_ref
+      property: (member_identifier) @_ident))
+  (argument_list
+    (positional_argument
+      (string) @injection.content))
+  (#eq? @_ref "regex")
+  (#eq? @_ident "compile")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "regex"))
