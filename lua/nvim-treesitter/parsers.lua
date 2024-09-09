50549361f6b9dbc44ed3f751d834725bda92d712
Tim Culverhouse <tim@timculverhouse.com>
2024-09-09 09:50:54 -0500

    feat(superhtml): add superhtml parser and queries
    SuperHTML is a templating language used by the zine static site
    generator.

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -2113,6 +2113,18 @@ list.supercollider = {
   maintainers = { "@madskjeldgaard" },
 }
 
+list.superhtml = {
+  install_info = {
+    url = "https://github.com/kristoff-it/superhtml",
+    files = {
+      "src/parser.c",
+      "src/scanner.c",
+    },
+    location = "tree-sitter-superhtml",
+  },
+  maintainers = { "@rockorager" },
+}
+
 list.surface = {
   install_info = {
     url = "https://github.com/connorlay/tree-sitter-surface",
