7a876d56dc44d32bc387b2d922003b59a7991091
Yuri Zolkin <zolkin.u.a@gmail.com>
2024-11-05 10:48:49 +0200

    feat(yaml): add language injection from comments

--- queries/yaml/injections.scm
+++ queries/yaml/injections.scm
@@ -78,3 +78,25 @@
           (block_scalar) @injection.content
           (#set! injection.language "promql")
           (#offset! @injection.content 0 1 0 0))))))
+
+; Support Idea-style injection: # language=yaml
+; Inject language at toplevel yaml node
+((comment) @injection.language
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  .
+  (block_mapping_pair
+    value: (block_node
+      (block_scalar) @injection.content
+      (#offset! @injection.content 0 1 0 0))))
+
+; Inject language in yaml body
+(block_mapping_pair
+  (comment) @injection.language
+  (#gsub! @injection.language "#%s*language=%s*([%w%p]+)%s*" "%1")
+  value: (block_node
+    (block_mapping
+      .
+      (block_mapping_pair
+        value: (block_node
+          (block_scalar) @injection.content
+          (#offset! @injection.content 0 1 0 0))))))
