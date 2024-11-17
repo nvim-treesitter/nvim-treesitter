37427012d1c77c544356bfff0c9acc88fd3256bc
PrestonKnopp <prestonknopp@gmail.com>
2024-11-17 15:23:02 -0800

    feat(gdscript): add new export annotations

--- queries/gdscript/highlights.scm
+++ queries/gdscript/highlights.scm
@@ -254,12 +254,13 @@
   (identifier) @attribute)
   (#any-of? @attribute
     ; from modules/gdscript/doc_classes/@GDScript.xml
-    "export" "export_category" "export_color_no_alpha" "export_dir" "export_enum"
+    "export" "export_category" "export_color_no_alpha" "export_custom" "export_dir" "export_enum"
     "export_exp_easing" "export_file" "export_flags" "export_flags_2d_navigation"
     "export_flags_2d_physics" "export_flags_2d_render" "export_flags_3d_navigation"
     "export_flags_3d_physics" "export_flags_3d_render" "export_flags_avoidance" "export_global_dir"
     "export_global_file" "export_group" "export_multiline" "export_node_path" "export_placeholder"
-    "export_range" "export_subgroup" "icon" "onready" "rpc" "static_unload" "tool" "warning_ignore"))
+    "export_range" "export_storage" "export_subgroup" "icon" "onready" "rpc" "static_unload" "tool"
+    "warning_ignore"))
 
 ; Builtin Types
 ((identifier) @type.builtin
