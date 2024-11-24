47fd0f5d255ddc3f93f5293ed05cdeec18facf38
AtomToast <reg-2@t-online.de>
2024-11-24 14:42:28 +0100

    feat(zig): add locals queries

--- queries/zig/locals.scm
+++ queries/zig/locals.scm
@@ -1,1 +1,64 @@
+(FnProto
+  function: (IDENTIFIER) @local.definition.function)
 
+(ParamDecl
+  parameter: (IDENTIFIER) @local.definition.parameter
+  (ParamType
+    (ErrorUnionExpr
+      (SuffixExpr
+        variable_type_function: (IDENTIFIER) @local.reference
+        (#set! reference.kind "type")))))
+
+(VarDecl
+  variable_type_function: (IDENTIFIER) @local.definition.var)
+
+(VarDecl
+  variable_type_function: (IDENTIFIER) @local.definition.type
+  (ErrorUnionExpr
+    (SuffixExpr
+      (ContainerDecl))))
+
+(ContainerField
+  (ErrorUnionExpr
+    (SuffixExpr
+      variable_type_function: (IDENTIFIER) @local.definition.var)))
+
+(ContainerField
+  field_member: (IDENTIFIER) @local.definition.field)
+
+
+(FieldInit
+  field_member: (IDENTIFIER) @local.definition.field)
+
+(FieldOrFnCall
+  function_call: (IDENTIFIER) @local.reference
+  (#set! reference.kind "function"))
+
+(FieldOrFnCall
+  field_access: (IDENTIFIER) @local.reference
+  (#set! reference.kind "field"))
+
+(SuffixExpr
+  field_constant: (IDENTIFIER) @local.reference
+  (#set! reference.kind "field"))
+
+(SuffixExpr
+  variable_type_function: (IDENTIFIER) @local.reference)
+
+
+(LabeledStatement
+  (BlockLabel
+    (IDENTIFIER) @local.definition))
+
+(BreakLabel
+  (IDENTIFIER) @local.reference)
+
+[
+  (ForStatement)
+  (IfStatement)
+  (WhileStatement)
+  (FnProto)
+  (Block)
+  (source_file)
+  (ContainerDecl )
+] @local.scope
