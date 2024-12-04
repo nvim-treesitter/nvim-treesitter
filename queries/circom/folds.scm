21ab3327a2da4bc0070d0dc81b53ccef0350290b
Alexandr Martirosyan <alexandr.martirosyan2000@gmail.com>
2024-12-04 15:35:53 +0400

    add injections and folds

---
+++ queries/circom/folds.scm
@@ -0,0 +1,12 @@
+[
+  (template_body)
+  (block_statement)
+  (if_statement)
+  (for_statement)
+  (while_statement)
+  (function_body)
+  (call_expression)
+  (array_expression)
+  (tuple_expression)
+  (comment)
+] @fold
