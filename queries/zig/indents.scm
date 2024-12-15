bce11f05723654942cf23f832f3f1d0f9b002f73
Vincent Rischmann <vincent@rischmann.fr>
2024-12-15 20:56:34 +0100

    improve indent queries for zig

--- queries/zig/indents.scm
+++ queries/zig/indents.scm
@@ -1,7 +1,12 @@
 [
   (block)
   (struct_declaration)
+  (enum_declaration)
+  (union_declaration)
   (switch_expression)
+  (if_expression)
+  (while_expression)
+  (for_expression)
   (initializer_list)
 ] @indent.begin
 
