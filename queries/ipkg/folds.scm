2be7be19e8e17c1b58371704734393a6dfc2d865
Serhii Khoma <srghma@gmail.com>
2024-12-23 10:38:12 +0700

    feat(ipkg): ribru17 proposal -> implement

--- queries/ipkg/folds.scm
+++ queries/ipkg/folds.scm
@@ -2,37 +2,12 @@
   (source_file)
   (dependency_list)
   (module_list)
+  (dependency_declaration)
+  (module_declaration)
+  (module_declaration)
+  (package_declaration)
+  (main_declaration)
+  (executable_declaration)
+  (version_declaration)
+  (langversion_declaration)
 ] @fold
-
-(dependency_declaration
-  "depends"
-  "="
-  (dependency_list)) @fold
-
-(module_declaration
-  "modules"
-  "="
-  (module_list)) @fold
-
-(package_declaration
-  "package"
-  (package_name)) @fold
-
-(main_declaration
-  "main"
-  "="
-  (module_name)) @fold
-
-(executable_declaration
-  "executable"
-  "="
-  (package_name)) @fold
-
-(version_declaration
-  "version"
-  "="
-  (version_number)) @fold
-
-(langversion_declaration
-  "langversion"
-  (version_range)) @fold
