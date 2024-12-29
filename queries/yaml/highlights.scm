65ef4655f7541281fbbac8da07a95d6ff8211e0b
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-12-29 16:35:24 +0100

    feat(yaml): highlighting to deal with yaml's Norway problem
    see for example: https://www.bram.us/2022/01/11/yaml-the-norway-problem/
    values based on: https://yamllint.readthedocs.io/en/stable/rules.html#module-yamllint.rules.truthy

--- queries/yaml/highlights.scm
+++ queries/yaml/highlights.scm
@@ -80,3 +80,9 @@
   "---"
   "..."
 ] @punctuation.special
+
+; help deal with for yaml's norway problem https://www.bram.us/2022/01/11/yaml-the-norway-problem/
+; values based on https://yamllint.readthedocs.io/en/stable/rules.html#module-yamllint.rules.truthy
+((string_scalar) @boolean
+  (#any-of? @boolean
+    "yes" "no" "YES" "NO" "Yes" "No" "on" "off" "ON" "OFF" "On" "Off" "TRUE" "FALSE" "True" "False"))
