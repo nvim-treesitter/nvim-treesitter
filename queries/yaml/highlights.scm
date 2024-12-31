230d9b6741bab5846cdf5d71a241d1c64c6b298c
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-12-31 12:58:19 +0100

    fix: only highlight values, not keys

--- queries/yaml/highlights.scm
+++ queries/yaml/highlights.scm
@@ -83,6 +83,21 @@
 
 ; help deal with for yaml's norway problem https://www.bram.us/2022/01/11/yaml-the-norway-problem/
 ; values based on https://yamllint.readthedocs.io/en/stable/rules.html#module-yamllint.rules.truthy
-((string_scalar) @boolean
-  (#any-of? @boolean
-    "yes" "no" "YES" "NO" "Yes" "No" "on" "off" "ON" "OFF" "On" "Off" "TRUE" "FALSE" "True" "False"))
+(block_mapping_pair
+  value: (block_node
+    (block_sequence
+      (block_sequence_item
+        (flow_node
+          (plain_scalar
+            (string_scalar) @boolean
+            (#any-of? @boolean
+              "yes" "no" "YES" "NO" "Yes" "No" "on" "off" "ON" "OFF" "On" "Off" "TRUE" "FALSE"
+              "True" "False")))))))
+
+(block_mapping_pair
+  value: (flow_node
+    (plain_scalar
+      (string_scalar) @boolean
+      (#any-of? @boolean
+        "yes" "no" "YES" "NO" "Yes" "No" "on" "off" "ON" "OFF" "On" "Off" "TRUE" "FALSE" "True"
+        "False"))))
