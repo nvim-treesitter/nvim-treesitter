6935286b4ee3d938954e446d657eebee71b4d07a
Robert Horvath <rob@nyar.eu>
2025-01-03 15:07:57 -0300

    fix(leo): intentation fix of parenthesis

--- queries/leo/indents.scm
+++ queries/leo/indents.scm
@@ -21,6 +21,19 @@
   (#set! indent.open_delimiter "(")
   (#set! indent.close_delimiter ")"))
 
+((function_arguments) @indent.align
+  (#set! indent.open_delimiter "(")
+  (#set! indent.close_delimiter ")"))
+
+(parenthesized_expression
+  ")" @indent.branch)
+
+(function_parameters
+  ")" @indent.branch)
+
+(function_arguments
+  ")" @indent.branch)
+
 (record_declaration
   "}" @indent.branch)
 
@@ -31,10 +44,10 @@
   "}" @indent.branch)
 
 (array_expression
-  "}" @indent.branch)
+  "]" @indent.branch)
 
 (tuple_expression
-  "}" @indent.branch)
+  ")" @indent.branch)
 
 (items_block
   "}" @indent.branch)
