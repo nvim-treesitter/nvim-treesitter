645d14f40f6c83da255591e16de1c0dfe5bf8ed4
Omar Valdez <omarantoniovaldezf2@gmail.com>
2025-01-19 01:57:38 -0800

    feat(hyprlang): bash injections for exec keybindings

--- queries/hyprlang/injections.scm
+++ queries/hyprlang/injections.scm
@@ -4,3 +4,18 @@
 (exec
   (string) @injection.content
   (#set! injection.language "bash"))
+
+((keyword
+  (name) @_bind
+  (params
+    .
+    (_)
+    .
+    (_)
+    .
+    (string) @_exec
+    .
+    (string) @injection.content))
+  (#lua-match? @_bind "^bind")
+  (#lua-match? @_exec "^%s*exec%s*$")
+  (#set! injection.language "bash"))
