1c67567464b0b01d2f8ccd4237d9ecbb3eb2dff1
Théo Reichel <390145+theoo@users.noreply.github.com>
2025-01-21 09:25:08 +0100

    feat(slim): add parser and queries (#7425)

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -2042,6 +2042,14 @@ list.slang = {
   experimental = true,
 }
 
+list.slim = {
+  install_info = {
+    url = "https://github.com/theoo/tree-sitter-slim",
+    files = { "src/parser.c", "src/scanner.c" },
+  },
+  maintainers = { "@theoo" },
+}
+
 list.slint = {
   install_info = {
     url = "https://github.com/slint-ui/tree-sitter-slint",
