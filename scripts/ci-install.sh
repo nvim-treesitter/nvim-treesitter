188bbf7a098915511b899cdb7324450b35ab3ce7
Christian Clason <c.clason@uni-graz.at>
2025-01-27 16:53:58 +0100

    fix(ci): update to changed neovim release name

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -6,10 +6,10 @@ NVIM_TAG=${NVIM_TAG-nightly}
 
 os=$(uname -s)
 if [[ $os == Linux ]]; then
-  wget https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-linux-x86_64.tar.gz
+  wget "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-linux-x86_64.tar.gz"
   tar -zxf nvim-linux-x86_64.tar.gz
   sudo ln -s "$PWD"/nvim-linux-x86_64/bin/nvim /usr/local/bin
-  rm -rf "$PWD"/nvim-linux-x86_64/lib/nvim/parser
+  rm -rf "$PWD"/nvim-linu-x86_x64/lib/nvim/parser
   mkdir -p ~/.local/share/nvim/site/pack/nvim-treesitter/start
   ln -s "$PWD" ~/.local/share/nvim/site/pack/nvim-treesitter/start
 elif [[ $os == Darwin ]]; then
