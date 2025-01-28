c9d90fd53d9a34b6d25ec66b5e04cb38ef0d293f
tris203 <admin@snappeh.com>
2025-01-28 20:14:12 +0000

    refactor(razor): inject and inherit

--- queries/razor/injections.scm
+++ queries/razor/injections.scm
@@ -1,2 +1,13 @@
+((html_comment) @injection.content
+  (#set! injection.language "comment"))
+
+((razor_comment) @injection.content
+  (#set! injection.language "comment"))
+
 ((comment) @injection.content
   (#set! injection.language "comment"))
+
+((element) @injection.content
+  (#set! injection.language "html")
+  (#set! injection.include-children)
+  (#set! injection.combined))
