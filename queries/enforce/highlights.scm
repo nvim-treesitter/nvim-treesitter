3589fb2e1e69ff459c5a782a02a24f8e10fdfb9a
simonvic <simonvic.dev@gmail.com>
2025-02-07 16:36:45 +0100

    style(query): format highlights

--- queries/enforce/highlights.scm
+++ queries/enforce/highlights.scm
@@ -1,111 +1,178 @@
 [
- (comment_line)
- (comment_block)
- ] @comment @spell
+  (comment_line)
+  (comment_block)
+] @comment @spell
 
 [
- (doc_line)
- (doc_block)
- ] @comment.documentation @spell
+  (doc_line)
+  (doc_block)
+] @comment.documentation @spell
 
 (literal_bool) @boolean
+
 (literal_int) @number
+
 (literal_float) @number.float
+
 (literal_string) @string
+
 (escape_sequence) @string.escape
 
 (identifier) @variable
 
-((identifier) @constant (#lua-match? @constant "^[A-Z_][A-Z%d_]+$"))
+((identifier) @constant
+  (#lua-match? @constant "^[A-Z_][A-Z%d_]+$"))
 
 ; Preprocessor directives
 [
- (include)
- (define)
- (ifdef)
- (ifndef)
- (else)
- (endif)
- ] @keyword.directive
+  (include)
+  (define)
+  (ifdef)
+  (ifndef)
+  (else)
+  (endif)
+] @keyword.directive
 
 (preproc_const) @constant.macro
 
 ; Constant fields
 (decl_field
-  ((field_modifier) @_modifier (#eq? @_modifier "const"))
+  ((field_modifier) @_modifier
+    (#eq? @_modifier "const"))
   type: (_)
-  name: (identifier) @constant
-  )
+  name: (identifier) @constant)
 
-(enum_member name: (identifier) @constant)
-
-[
- "+" "-" "*" "/" "%" "^"
- "++" "--"
- "=" "+=" "-=" "*=" "/=" "&=" "^=" "|=" "<<=" ">>="
- "<" "<=" ">=" ">"
- "==" "!="
- "!" "&&" "||" ">>" "<<" "&" "|" "^" "~"
- ] @operator
+(enum_member
+  name: (identifier) @constant)
 
 [
- "(" ")"
- "[" "]"
- "{" "}"
- ] @punctuation.bracket
+  "+"
+  "-"
+  "*"
+  "/"
+  "%"
+  "^"
+  "++"
+  "--"
+  "="
+  "+="
+  "-="
+  "*="
+  "/="
+  "&="
+  "^="
+  "|="
+  "<<="
+  ">>="
+  "<"
+  "<="
+  ">="
+  ">"
+  "=="
+  "!="
+  "!"
+  "&&"
+  "||"
+  ">>"
+  "<<"
+  "&"
+  "|"
+  "^"
+  "~"
+] @operator
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
 
 ; TODO: <> in decl_class
-(types [ "<" ">" ] @punctuation.bracket)
+(types
+  [
+    "<"
+    ">"
+  ] @punctuation.bracket)
 
 [
- "," "." ":" ";"
- ] @punctuation.delimiter
+  ","
+  "."
+  ":"
+  ";"
+] @punctuation.delimiter
 
-(literal_string ["\"" "\""] @punctuation.delimiter)
+(literal_string
+  [
+    "\""
+    "\""
+  ] @punctuation.delimiter)
 
 [
- "continue" "break"
- "switch" "case"
- "typedef"
- "delete"
- "default"
- "extends"
- "new" ; TODO: is it operator?
- "auto" ; TODO: is it type?
- ] @keyword
+  "continue"
+  "break"
+  "switch"
+  "case"
+  "typedef"
+  "delete"
+  "default"
+  "extends"
+  "new" ; TODO: is it operator?
+  "auto" ; TODO: is it type?
+] @keyword
 
-["return"] @keyword.return
-["if" "else"] @keyword.conditional
-["while" "for" "foreach"] @keyword.repeat
-["enum" "class"] @keyword.type
+"return" @keyword.return
+
 [
- (variable_modifier)
- (method_modifier)
- (class_modifier)
- (field_modifier)
- (formal_parameter_modifier)
- ] @keyword.modifier
+  "if"
+  "else"
+] @keyword.conditional
 
-["ref"] @type
-(decl_class typename: (identifier) @type)
-(decl_enum typename: (identifier) @type)
-(type_identifier (identifier) @type)
+[
+  "while"
+  "for"
+  "foreach"
+] @keyword.repeat
+
+[
+  "enum"
+  "class"
+] @keyword.type
+
+[
+  (variable_modifier)
+  (method_modifier)
+  (class_modifier)
+  (field_modifier)
+  (formal_parameter_modifier)
+] @keyword.modifier
+
+"ref" @type
+
+(decl_class
+  typename: (identifier) @type)
+
+(decl_enum
+  typename: (identifier) @type)
+
+(type_identifier
+  (identifier) @type)
 
 (type_primitive) @type.builtin
 
 [
- (super)
- (this)
- (literal_null)
- ] @variable.builtin
+  (super)
+  (this)
+  (literal_null)
+] @variable.builtin
 
 (decl_method
-  name: (identifier) @function.method
- )
+  name: (identifier) @function.method)
 
 (invokation
-  invoked: (identifier) @function.method.call
-  )
+  invoked: (identifier) @function.method.call)
 
 ; Constructor and deconstructor (function with same name of the class)
 (decl_class
@@ -113,7 +180,4 @@
   body: (class_body
     (decl_method
       name: (identifier) @constructor
-      (#eq? @constructor @_classname)
-      )
-    )
-  )
+      (#eq? @constructor @_classname))))
