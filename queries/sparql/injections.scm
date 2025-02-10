6af3db99ebd0079256fdd6af7ce307221a1da55b
ginnyTheCat <ginnythecat@lelux.net>
2025-02-10 03:35:57 +0100

    feat(sparql): add regex injection

--- queries/sparql/injections.scm
+++ queries/sparql/injections.scm
@@ -1,2 +1,8 @@
 ((comment) @injection.content
   (#set! injection.language "comment"))
+
+(regex_expression
+  pattern: (rdf_literal
+    value: (string) @injection.content)
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "regex"))
