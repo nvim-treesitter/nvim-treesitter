ee4387a404595a1f7d6163d5d11c31b1e234465b
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-02-19 18:33:34 +0100

    fix: target yaml schema 1.2 only

--- queries/yaml/highlights.scm
+++ queries/yaml/highlights.scm
@@ -82,7 +82,7 @@
 ] @punctuation.special
 
 ; help deal with for yaml's norway problem https://www.bram.us/2022/01/11/yaml-the-norway-problem/
-; values based on https://yamllint.readthedocs.io/en/stable/rules.html#module-yamllint.rules.truthy
+; only using `true` and `false`, since Treesitter parser targets YAML spec 1.2 https://github.com/nvim-treesitter/nvim-treesitter/pull/7512#issuecomment-2565397302
 (block_mapping_pair
   value: (block_node
     (block_sequence
@@ -90,14 +90,10 @@
         (flow_node
           (plain_scalar
             (string_scalar) @boolean
-            (#any-of? @boolean
-              "yes" "no" "YES" "NO" "Yes" "No" "on" "off" "ON" "OFF" "On" "Off" "TRUE" "FALSE"
-              "True" "False")))))))
+            (#any-of? @boolean "TRUE" "FALSE" "True" "False")))))))
 
 (block_mapping_pair
   value: (flow_node
     (plain_scalar
       (string_scalar) @boolean
-      (#any-of? @boolean
-        "yes" "no" "YES" "NO" "Yes" "No" "on" "off" "ON" "OFF" "On" "Off" "TRUE" "FALSE" "True"
-        "False"))))
+      (#any-of? @boolean "TRUE" "FALSE" "True" "False"))))
