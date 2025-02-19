c32b59a8fde33ced5b30ea97750336a6fcff402f
Michael Parque <michael.t.parque@outlook.com>
2025-02-19 18:12:46 +0100

    fix(css): do not fold top-level blocks (#7562)
    Using (block) makes the fold behavior only work on the line that the
    block starts not the start line of the statement, which is unideal when
    the preceding statement spans multiple lines. instead, use all top-level
    statements explicitly from https://github.com/tree-sitter/tree-sitter-css/blob/master/grammar.js

--- queries/css/folds.scm
+++ queries/css/folds.scm
@@ -1,4 +1,10 @@
 [
+  ; top-level block statements from https://github.com/tree-sitter/tree-sitter-css/blob/master/grammar.js
+  ; note: (block) is not used due to unideal behavior when node before block node spans multiple lines
   (rule_set)
+  (at_rule)
+  (supports_statement)
+  (media_statement)
+  (keyframe_block)
   (import_statement)+
 ] @fold
