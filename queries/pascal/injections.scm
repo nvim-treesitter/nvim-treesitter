e4fcbafa508d6f3cb7c52592d43a596585daa8db
Philip Zander <philip.zander@gmail.com>
2025-02-20 18:00:06 +0100

    feat(pascal): Add asm language injection for Pascal

--- queries/pascal/injections.scm
+++ queries/pascal/injections.scm
@@ -1,7 +1,5 @@
 ((comment) @injection.content
   (#set! injection.language "comment"))
 
-; There is no parser for assembly language yet. Add an injection here when we
-; have a parser.
-; ((asmBody) @injection.content
-;  (#set! injection.language "asm"))
+((asmBody) @injection.content
+  (#set! injection.language "asm"))
