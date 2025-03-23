58b1092115ccf585efa3818a7c580d83a93ad289
Omar Valdez <omarantoniovaldezf2@gmail.com>
2025-03-23 01:14:22 -0700

    feat(bash): highlight variable argument for `printf -v`

--- runtime/queries/bash/highlights.scm
+++ runtime/queries/bash/highlights.scm
@@ -247,6 +247,17 @@
     "READLINE_ARGUMENT" "READLINE_LINE" "READLINE_MARK" "READLINE_POINT" "REPLY" "SECONDS" "SHELL"
     "SHELLOPTS" "SHLVL" "SRANDOM" "TIMEFORMAT" "TMOUT" "TMPDIR" "UID"))
 
+((command
+  name: (command_name
+    (word) @_printf)
+  .
+  argument: (word) @_v
+  .
+  argument: (word) @variable)
+  (#eq? @_printf "printf")
+  (#eq? @_v "-v")
+  (#lua-match? @variable "^[a-zA-Z_][a-zA-Z0-9_]*$"))
+
 (case_item
   value: (word) @variable.parameter)
 
