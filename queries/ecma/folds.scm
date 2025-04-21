e0d861b23ace1702eb3d0c2e49ee11ef8771c82d
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-04-20 20:20:34 -0700

    fix(folds.ecma): separate folds for if/else/else if
    With large if/else/else if blocks, it's difficult to notice that folding
    an if statement also folds the else and else if statements along with
    them. By allowing the folds of the if, else, and else if statements to
    be separate, the folds are easier to distinguish and are more granular.

--- queries/ecma/folds.scm
+++ queries/ecma/folds.scm
@@ -22,3 +22,11 @@
   (generator_function)
   (generator_function_declaration)
 ] @fold
+
+(if_statement
+  consequence: (statement_block) @fold
+  (#offset! @fold 0 0 -1 0))
+
+(if_statement
+  alternative: (else_clause) @fold
+  (#offset! @fold 0 0 -1 0))
