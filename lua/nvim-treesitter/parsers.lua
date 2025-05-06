55d16aa9daf771bfcfd9e0c4011018327307a87e
Christian Clason <c.clason@uni-graz.at>
2025-05-06 10:35:58 +0200

    feat(parsers): update vim, gren, comment, purescript, elm, matlab, javadoc, swift, godot_resource, query, vimdoc, ledger

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -230,7 +230,7 @@ return {
   },
   comment = {
     install_info = {
-      revision = '85eae6644d25bcbdd19f4a35c9946abc71c2d6fe',
+      revision = '689be73775bd2dd57b938b8e12bf50fec35a6ca3',
       url = 'https://github.com/stsewd/tree-sitter-comment',
     },
     maintainers = { '@stsewd' },
@@ -473,7 +473,7 @@ return {
   },
   elm = {
     install_info = {
-      revision = '3b373a3ccb48f31aa4b7ddc9092e2f5e24ab7aed',
+      revision = '6e3c6d51f13168f9d7794c8e8add7dfdd07d20b8',
       url = 'https://github.com/elm-tooling/tree-sitter-elm',
     },
     maintainers = { '@zweimach' },
@@ -778,7 +778,7 @@ return {
   },
   godot_resource = {
     install_info = {
-      revision = '91c55fdf325a832659e59cdf4a02bfe8a423f14c',
+      revision = '99417ca9f4eccbf49e4ce5a69d89228811593bf5',
       url = 'https://github.com/PrestonKnopp/tree-sitter-godot-resource',
     },
     maintainers = { '@pierpo' },
@@ -835,7 +835,7 @@ return {
   },
   gren = {
     install_info = {
-      revision = '06389ece5bc7344ed3931dc516bb609d8864dd2a',
+      revision = 'c36aac51a915fdfcaf178128ba1e9c2205b25930',
       url = 'https://github.com/MaeBrooks/tree-sitter-gren',
     },
     maintainers = { '@MaeBrooks' },
@@ -1068,7 +1068,7 @@ return {
   },
   javadoc = {
     install_info = {
-      revision = '5ae22c17a9a60ffbf674f00d0080d4304fde2231',
+      revision = 'e49c8f0e26dc79afc999f377121ecceb30ffcc74',
       url = 'https://github.com/rmuir/tree-sitter-javadoc',
     },
     maintainers = { '@rmuir' },
@@ -1241,7 +1241,7 @@ return {
   },
   ledger = {
     install_info = {
-      revision = '4d828c87e8bf3f7cef9927417b47ce1126c1ca29',
+      revision = '96c92d4908a836bf8f661166721c98439f8afb80',
       url = 'https://github.com/cbarrete/tree-sitter-ledger',
     },
     maintainers = { '@cbarrete' },
@@ -1360,7 +1360,7 @@ return {
   },
   matlab = {
     install_info = {
-      revision = 'bbf1b3f0bd7417c1efb8958fe95be3d0d540207a',
+      revision = '88ccb5ee90711e9961582d421c42bc8c6bd53f59',
       url = 'https://github.com/acristoffers/tree-sitter-matlab',
     },
     maintainers = { '@acristoffers' },
@@ -1740,7 +1740,7 @@ return {
   },
   purescript = {
     install_info = {
-      revision = 'daf9b3e2be18b0b2996a1281f7783e0d041d8b80',
+      revision = '4cacfdaa75090bb790965674fe387bb215bab64b',
       url = 'https://github.com/postsolar/tree-sitter-purescript',
     },
     maintainers = { '@postsolar' },
@@ -1790,7 +1790,7 @@ return {
   },
   query = {
     install_info = {
-      revision = '930202c2a80965a7a9ca018b5b2a08b25dfa7f12',
+      revision = 'dbab9b69f954167fb88785450dab32e5283b758d',
       url = 'https://github.com/tree-sitter-grammars/tree-sitter-query',
     },
     maintainers = { '@steelsojka' },
@@ -2501,7 +2501,7 @@ return {
   },
   vim = {
     install_info = {
-      revision = '054c4beaf7b95b52cb20b138bfd8b6fd6ba561a7',
+      revision = 'ccc312e878aa84f32d180b8528a3585c7b86a545',
       url = 'https://github.com/tree-sitter-grammars/tree-sitter-vim',
     },
     maintainers = { '@clason' },
@@ -2509,7 +2509,7 @@ return {
   },
   vimdoc = {
     install_info = {
-      revision = '2694c3d27e2ca98a0ccde72f33887394300d524e',
+      revision = '6dab95ab84a006e8adfd6966c5fd9dde48224725',
       url = 'https://github.com/neovim/tree-sitter-vimdoc',
     },
     maintainers = { '@clason' },
