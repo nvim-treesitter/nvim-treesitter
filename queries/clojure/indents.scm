55a008083b26380489d84f8a37d2b2186a7025d9
Garrett Hopper <garrett@garretthopper.com>
2025-05-07 12:11:25 -0500

    Add Clojure indents.scm using Tonsky's simplified formatting

---
+++ queries/clojure/indents.scm
@@ -0,0 +1,21 @@
+; https://tonsky.me/blog/clojurefmt/
+
+; Align lines within forms
+((list_lit)    @indent.align (#set! indent.open_delimiter "(") (#set! indent.close_delimiter ")"))
+((vec_lit)     @indent.align (#set! indent.open_delimiter "[") (#set! indent.close_delimiter "]"))
+((map_lit)     @indent.align (#set! indent.open_delimiter "{") (#set! indent.close_delimiter "}"))
+((set_lit)     @indent.align (#set! indent.open_delimiter "{") (#set! indent.close_delimiter "}"))
+((anon_fn_lit) @indent.align (#set! indent.open_delimiter "(") (#set! indent.close_delimiter ")"))
+
+([(map_lit) (set_lit) (vec_lit) (list_lit) (anon_fn_lit)]
+ @indent.align
+ ; Align lines even when parent open_delimiter is hanging
+ (#set! indent.align_hanging 1)
+ ; Allow parent nodes on same line to indent (e.g. '(' and '[' on single line)
+ (#set! indent.propagate 1))
+
+; Indent lists starting with a symbol by 2 spaces
+([(list_lit    . (sym_lit))
+  (anon_fn_lit . (sym_lit))]
+ @indent.align 
+ (#set! indent.increment 2))
