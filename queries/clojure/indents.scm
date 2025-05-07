fba13d610ede2f38a1d1f3bcbc52f2436a03d6fc
Garrett Hopper <garrett@garretthopper.com>
2025-05-07 15:55:57 -0500

    Clojure indent, add {,splicing_}read_cond_lit support

--- queries/clojure/indents.scm
+++ queries/clojure/indents.scm
@@ -1,13 +1,15 @@
 ; https://tonsky.me/blog/clojurefmt/
 
 ; Align lines within forms
-((list_lit)    @indent.align (#set! indent.open_delimiter "(") (#set! indent.close_delimiter ")"))
-((vec_lit)     @indent.align (#set! indent.open_delimiter "[") (#set! indent.close_delimiter "]"))
-((map_lit)     @indent.align (#set! indent.open_delimiter "{") (#set! indent.close_delimiter "}"))
-((set_lit)     @indent.align (#set! indent.open_delimiter "{") (#set! indent.close_delimiter "}"))
-((anon_fn_lit) @indent.align (#set! indent.open_delimiter "(") (#set! indent.close_delimiter ")"))
+((list_lit)               @indent.align (#set! indent.open_delimiter "(") (#set! indent.close_delimiter ")"))
+((vec_lit)                @indent.align (#set! indent.open_delimiter "[") (#set! indent.close_delimiter "]"))
+((map_lit)                @indent.align (#set! indent.open_delimiter "{") (#set! indent.close_delimiter "}"))
+((set_lit)                @indent.align (#set! indent.open_delimiter "{") (#set! indent.close_delimiter "}"))
+((anon_fn_lit)            @indent.align (#set! indent.open_delimiter "(") (#set! indent.close_delimiter ")"))
+((read_cond_lit)          @indent.align (#set! indent.open_delimiter "(") (#set! indent.close_delimiter ")"))
+((splicing_read_cond_lit) @indent.align (#set! indent.open_delimiter "(") (#set! indent.close_delimiter ")"))
 
-([(map_lit) (set_lit) (vec_lit) (list_lit) (anon_fn_lit)]
+([(map_lit) (set_lit) (vec_lit) (list_lit) (anon_fn_lit) (read_cond_lit) (splicing_read_cond_lit)]
  @indent.align
  ; Align lines even when parent open_delimiter is hanging
  (#set! indent.align_hanging 1)
