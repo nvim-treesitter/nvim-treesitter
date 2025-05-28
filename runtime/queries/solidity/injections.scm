8c899d6f0f898a184114320bb8425c4f9d504654
Christian Clason <c.clason@uni-graz.at>
2025-05-28 09:41:11 +0200

    fix(queries): fix rebase errors on main

--- runtime/queries/solidity/injections.scm
+++ runtime/queries/solidity/injections.scm
@@ -1,2 +1,14 @@
 ((comment) @injection.content
   (#set! injection.language "comment"))
+
+((comment) @injection.content
+  (#lua-match? @injection.content "^///[^/]")
+  (#set! injection.language "doxygen"))
+
+((comment) @injection.content
+  (#lua-match? @injection.content "^///$")
+  (#set! injection.language "doxygen"))
+
+((comment) @injection.content
+  (#lua-match? @injection.content "^/[*][*][^*].*[*]/$")
+  (#set! injection.language "doxygen"))
