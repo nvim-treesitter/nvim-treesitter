024e6c5e46f8ec4237695b9e3020ecb601d817df
Christian Clason <c.clason@uni-graz.at>
2025-05-28 09:41:11 +0200

    fix(queries): fix rebase errors on main

--- runtime/queries/agda/injections.scm
+++ runtime/queries/agda/injections.scm
@@ -1,14 +1,2 @@
 ((comment) @injection.content
   (#set! injection.language "comment"))
-
-((comment) @injection.content
-  (#lua-match? @injection.content "^///[^/]")
-  (#set! injection.language "doxygen"))
-
-((comment) @injection.content
-  (#lua-match? @injection.content "^///$")
-  (#set! injection.language "doxygen"))
-
-((comment) @injection.content
-  (#lua-match? @injection.content "^/[*][*][^*].*[*]/$")
-  (#set! injection.language "doxygen"))
