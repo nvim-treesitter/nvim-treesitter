68847fac6bbf435202ea9554476da094533397ea
Oliver Thomas <thomaso@ie-freiburg.mpg.de>
2025-09-06 13:57:49 +0200

    chore: run make formatlua

--- tests/indent/snakemake_spec.lua
+++ tests/indent/snakemake_spec.lua
@@ -26,10 +26,22 @@ describe('indent Snakemake:', function()
     run:new_line('blocks.smk', { on_line = 15, text = 'pass', indent = 4 })
     run:new_line('directive_parameters.smk', { on_line = 4, text = 'before_a = 0,', indent = 8 })
     run:new_line('directive_parameters.smk', { on_line = 5, text = 'after_a = 1.1,', indent = 8 })
-    run:new_line('directive_parameters.smk', { on_line = 7, text = '"""dedent_after_last_param"""', indent = 4 })
-    run:new_line('directive_parameters.smk', { on_line = 14, text = 'b = "indent_after_param_with_comma"', indent = 8 })
-    run:new_line('directive_parameters.smk', { on_line = 15, text = 'b = "indent_after_param_with_comma"', indent = 8 })
-    run:new_line('directive_parameters.smk', { on_line = 17, text = 'b = "indent_after_opening"', indent = 8 })
+    run:new_line(
+      'directive_parameters.smk',
+      { on_line = 7, text = '"""dedent_after_last_param"""', indent = 4 }
+    )
+    run:new_line(
+      'directive_parameters.smk',
+      { on_line = 14, text = 'b = "indent_after_param_with_comma"', indent = 8 }
+    )
+    run:new_line(
+      'directive_parameters.smk',
+      { on_line = 15, text = 'b = "indent_after_param_with_comma"', indent = 8 }
+    )
+    run:new_line(
+      'directive_parameters.smk',
+      { on_line = 17, text = 'b = "indent_after_opening"', indent = 8 }
+    )
     for _, line in ipairs({ 22, 24, 26, 28, 30, 32, 34, 36 }) do
       run:new_line('directive_parameters.smk', { on_line = line, text = '"doc"', indent = 4 })
     end
