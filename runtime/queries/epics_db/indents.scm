dd96bc61ea0e1f6319c1fb327750b480f00699c7
Minijackson <minijackson@riseup.net>
2025-09-18 11:10:23 +0200

    feat(epics_db): add parser and queries

---
+++ runtime/queries/epics_db/indents.scm
@@ -0,0 +1,35 @@
+[
+  (menu_definition)
+  (record_type_definition)
+  (device_support_declaration)
+  (driver_declaration)
+  (registrar_declaration)
+  (variable_declaration)
+  (function_declaration)
+  (breakpoint_table)
+  (record_instance)
+  (alias_statement)
+  (menu_choice)
+  (field_definition)
+  (field_descriptor)
+  (alias)
+  (field)
+  (info)
+] @indent.begin
+
+[
+  "{"
+  "}"
+  "("
+  ")"
+] @indent.branch
+
+[
+  "}"
+  ")"
+] @indent.end
+
+[
+  (comment)
+  (string)
+] @indent.auto
