20166dbb1f5048123a5ea4ab184a4952b4a8c4f6
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2025-10-19 15:07:14 +0200

    test(python): update regex highlights test to test comments around regex strings

--- tests/query/highlights/python/regex.py
+++ tests/query/highlights/python/regex.py
@@ -3,8 +3,14 @@ import re
 re_test = re.compile(r"^(?P<year>\d{4}) (?P<day>\d) \w\s{,3}$")
 #                                    ^ @string.regexp
 re_test = re.compile(
-    r"^(?P<year>\d{4}) "
+    # comment
+    # ^ @comment
+    r"^(?P<year>\d{4}){1}"
     # ^ @string.regexp
+    # comment
+    # ^ @comment
     r"(?P<day>\d) \w\s{,3}"
     # ^ @string.regexp
+    # comment
+    # ^ @comment
 )
