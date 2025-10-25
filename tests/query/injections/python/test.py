c113dc6d7206658c214d80eac777f0aa6c291bdf
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2025-10-25 19:21:36 +0200

    ci(python): add injections test and update highlights test

--- tests/query/injections/python/test.py
+++ tests/query/injections/python/test.py
@@ -1,19 +1,18 @@
 import re
 
 re_test = re.compile(r"^(?P<year>\d{4}) (?P<day>\d) \w\s{,3}$")
-#                                    ^ @string.regexp
+#                                    ^ @regex
 re_test = re.compile(
     # comment
     # ^ @comment
-    r"^(?P<year>\d{4}){1}"
-    # ^ @string.regexp
+    r"^(?P<year>\d{4}) "
     # comment
     # ^ @comment
-    r"(?P<day>\d) \w\s{,3}"
-    # ^ @string.regexp
+    r"(?P<day>\d) \w\s{,3}$"
+    # ^ @regex
+    # comment
     # comment
     # ^ @comment
 )
-# interpolation
 print("foo %s bar %d" % ("arg1", 2))
-#          ^ @character
+#          ^ @printf
