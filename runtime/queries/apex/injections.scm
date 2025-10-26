738d9ced4ce5b4538c3a0e23ceca12c34c8d2e74
imawaki <imawaki.2015@gmail.com>
2025-10-27 03:07:19 +0900

    feat(apex): javadoc injections (#8232)
    Apex codebases commonly use Javadoc-style comments, similar to Java (Apex is Salesforce's object-oriented language).
    
    This updates the injection query to capture javadoc nodes instead of the generic comment for better highlighting and parsing accuracy.

--- runtime/queries/apex/injections.scm
+++ runtime/queries/apex/injections.scm
@@ -3,3 +3,12 @@
   (block_comment)
 ] @injection.content
   (#set! injection.language "comment"))
+
+((block_comment) @injection.content
+  (#lua-match? @injection.content "/[*][*][%s]")
+  (#set! injection.language "javadoc"))
+
+; markdown-style javadocs https://openjdk.org/jeps/467
+((line_comment) @injection.content
+  (#lua-match? @injection.content "^///%s")
+  (#set! injection.language "javadoc"))
