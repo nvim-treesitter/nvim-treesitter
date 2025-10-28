1f70b19e858cd7fd8e01b952c622d21236657579
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-28 09:38:01 +0100

    feat(diff): add language injection for changes

--- runtime/queries/diff/injections.scm
+++ runtime/queries/diff/injections.scm
@@ -1,2 +1,10 @@
 ((comment) @injection.content
   (#set! injection.language "comment"))
+
+(block
+  (new_file
+    (filename) @injection.filename)
+  (hunks
+    (hunk
+      changes: (changes) @injection.content
+      (#set! injection.include-children))))
