d8f4593bab4deb810db0d81b871bcc746fb836d9
Manfred Egger <github@tmk-stgeorgen.at>
2025-12-15 15:47:03 +0100

    Update runtime/queries/typoscript/injections.scm
    Co-authored-by: Christian Clason <ch.clason+github@icloud.com>

--- runtime/queries/typoscript/injections.scm
+++ runtime/queries/typoscript/injections.scm
@@ -6,6 +6,5 @@
   (#lua-match? @_id "_CSS_DEFAULT_STYLE$")
   (#set! injection.language "css"))
 
-; (#set! injection.overlay "constant"))
 ((comment) @injection.content
   (#set! injection.language "comment"))
