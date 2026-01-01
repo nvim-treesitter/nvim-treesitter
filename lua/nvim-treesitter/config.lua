e973e9dfa8b0fb57b34fbea9a4fb90dea2607d51
Christian Clason <c.clason@uni-graz.at>
2026-01-01 20:52:04 +0100

    feat(install): warn on unsupported parsers

--- lua/nvim-treesitter/config.lua
+++ lua/nvim-treesitter/config.lua
@@ -140,8 +140,12 @@ function M.norm_languages(languages, skip)
   languages = vim.tbl_filter(
     --- @param v string
     function(v)
-      -- TODO(lewis6991): warn of any unknown parsers?
-      return parsers[v] ~= nil
+      if parsers[v] ~= nil then
+        return true
+      else
+        require('nvim-treesitter.log').warn('skipping unsupported language: ' .. v)
+        return false
+      end
     end,
     languages
   )
