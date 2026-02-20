decca28b3557e253fcb7b307a931f1c5f654d978
Stefan VanBuren <svanburen@buf.build>
2026-02-20 16:23:44 -0500

    CEL
    https://cel.dev
    
    `.cel` (supported on nvim nightly: https://github.com/neovim/neovim/pull/37834)
    
    CEL is an expression-based language that's embeddable, so sorry for the
    short example. (Many more on https://celbyexample.com.)
    
    <details>
    <summary>Representative code sample</summary>
    ```
    // From cel.dev homepage
    // Simple predicates
    'tacocat'.startsWith('taco')
    ```
    </details>
    
    https://github.com/bufbuild/tree-sitter-cel
    
    <details>
    <summary>Parsed tree for code sample</summary>
    ```
    (expr ; [0, 0] - [3, 0]
      (comment) ; [0, 0] - [0, 24]
      (comment) ; [1, 0] - [1, 20]
      (member_call_expression ; [2, 0] - [2, 28]
        operand: (string_literal ; [2, 0] - [2, 9]
          (single_quoted_string_literal)) ; [2, 0] - [2, 9]
        function: (identifier) ; [2, 10] - [2, 20]
        arguments: (arguments ; [2, 20] - [2, 28]
          (string_literal ; [2, 21] - [2, 27]
            (single_quoted_string_literal))))) ; [2, 21] - [2, 27]
    
    ```
    </details>
    
    Source of queries: written from scratch; cobbled together from other
    tree-sitter repositories.
    
    <details>
    <summary>Screenshots of code sample</summary>
    <!-- paste screenshot of code sample using provided queries here -->
    </details>
    
    <!--
    CHECKLIST: _Before_ submitting, make sure
    
    * `./scripts/install-parsers.lua <language>` works without warnings
    * `./scripts/install-parsers.lua --generate <language>` works without warnings
    * `make query` works without warning
    * `make docs` is run
    -->

---
+++ runtime/queries/cel/folds.scm
@@ -0,0 +1,10 @@
+; Code folding queries for CEL (Common Expression Language).
+; Marks collection literals as foldable regions.
+; Fold map expressions
+(map_expression) @fold
+
+; Fold list expressions
+(list_expression) @fold
+
+; Fold struct expressions
+(struct_expression) @fold
