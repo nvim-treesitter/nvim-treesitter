78bebef150057eff7f26044bb177fbbf82ff047d
Christian Clason <c.clason@uni-graz.at>
2026-07-15 12:08:54 +0200

    fix(hlsplaylist,muttrc,tmux,zathurarc)!: drop support
    Upstream removed all generated files from the main branch, so these
    parsers are no longer installable.

--- runtime/queries/zathurarc/injections.scm
+++
@@ -1,9 +0,0 @@
-(set_directive
-  (option) @_option
-  (string) @injection.content
-  (#eq? @_option "synctex-editor-command")
-  (#offset! @injection.content 0 1 0 -1)
-  (#set! injection.language "bash"))
-
-((comment) @injection.content
-  (#set! injection.language "comment"))
