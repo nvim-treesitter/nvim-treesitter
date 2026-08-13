3d71e1b3995c9680d3d301bc796f14d6900795d4
Christian Clason <c.clason@uni-graz.at>
2026-08-13 10:02:34 +0200

    feat(teal)!: update parser and queries
    Breaking change: `(function_name)` node replaced by
    `(table_function_name)`.

--- lua/nvim-treesitter/parsers.lua
+++ lua/nvim-treesitter/parsers.lua
@@ -2264,7 +2264,7 @@ return {
   teal = {
     install_info = {
       generate = true,
-      revision = '05d276e737055e6f77a21335b7573c9d3c091e2f',
+      revision = 'ac6b42ffb23d5d52b582c02508dd3524ac0ba3df',
       url = 'https://github.com/euclidianAce/tree-sitter-teal',
     },
     maintainers = { '@euclidianAce' },
